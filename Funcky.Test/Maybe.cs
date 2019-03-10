5b34d71932abe29674efb2ad8f01ca0b3883b21a
Thomas Bruderer <apophis@apophis.ch>
2019-03-10 13:02:46 +0100

    Improve Tests and add Test for TryGetValue

--- Funcky.Test/Maybe.cs
+++ Funcky.Test/Maybe.cs
@@ -1,15 +1,30 @@
-using System.Net.Sockets;
+using System.Collections.Generic;
+using Funcky.Extensions;
 using Xunit;
-using System.Linq.Expressions;
 
 namespace Funcky.Test
 {
     public class Maybe
     {
-        [Fact]
-        public void MaybeMonadMatchesNothing()
+
+
+        [Theory]
+        [InlineData(-12, "-12")]
+        [InlineData(0, "0")]
+        [InlineData(-953542, "-953542")]
+        [InlineData(1337, "1337")]
+        public void ParseIntViaMaybeMonadWhereStringIsNumber(int parsed, string stringToParse)
         {
-            var maybe = TryParseHelper.TryParseInt("no number");
+            var maybe = stringToParse.TryParseInt();
+
+            Assert.True(maybe.Match(false, m => true));
+            Assert.Equal(parsed, maybe.Match(0, m => m));
+        }
+
+        [Fact]
+        public void ParseIntViaMaybeMonadWhereStringIsNoNumber()
+        {
+            var maybe = "no number".TryParseInt();
 
             Maybe<bool> isLeet = maybe.Select(m => m == 1337);
 
@@ -17,11 +32,24 @@ namespace Funcky.Test
         }
 
         [Fact]
-        public void MaybeMonadJust1337()
+        public void GetValueFromDictionaryViaMaybeMonad()
         {
-            var maybe = TryParseHelper.TryParseInt("1337");
+            var dictionary = new Dictionary<string, string> { { "some", "value" } };
 
-            Assert.Equal(1337, maybe.Match(0, m => m));
+            var maybe = dictionary.TryGetValue("some");
+
+            Assert.True(maybe.Match(false, m => true));
+            Assert.Equal("value", maybe.Match("", m => m));
+        }
+
+        [Fact]
+        public void GetValueFromDictionaryViaMaybeMonadWhereKeyDoesNotExist()
+        {
+            var dictionary = new Dictionary<string, string> { { "some", "value" } };
+
+            var maybe = dictionary.TryGetValue("none");
+
+            Assert.False(maybe.Match(false, m => true));
         }
     }
 }
