150de05d7d50a1a1a8954f4acf2ff31c1aa608b9
Thomas Bruderer <apophis@apophis.ch>
2019-03-12 16:55:36 +0100

    Implement Either Monad with Tests

---
+++ Funcky.Test/EitherTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Text;
+using Funcky.Monads;
+using Xunit;
+
+namespace Funcky.Test
+{
+    public class EitherTest
+    {
+        public class IntegerSums : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] { Either<string, int>.Right(5), Either<string, int>.Right(10), Either<string, int>.Right(15), 30 };
+                yield return new object[] { Either<string, int>.Right(1337), Either<string, int>.Right(42), Either<string, int>.Right(99), 1478 };
+                yield return new object[] { Either<string, int>.Right(45856), Either<string, int>.Right(58788), Either<string, int>.Right(699554), 804198 };
+                yield return new object[] { Either<string, int>.Right(5), Either<string, int>.Right(10), Either<string, int>.Left("Last"), "Last" };
+                yield return new object[] { Either<string, int>.Right(5), Either<string, int>.Left("Middle"), Either<string, int>.Left("Last"), "Middle" };
+                yield return new object[] { Either<string, int>.Left("First"), Either<string, int>.Left("Middle"), Either<string, int>.Left("Last"), "First" };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        [Fact]
+        public void CreateEitherLeftAndMatchCorrectly()
+        {
+            var value = Either<string, int>.Left("Error: not cool!");
+
+            var hasLeft = value.Match(
+                left: l => true,
+                right: r => false);
+
+            Assert.True(hasLeft);
+        }
+
+        [Fact]
+        public void CreateEitherRightAndMatchCorrectly()
+        {
+            var value = Either<string, int>.Right(1337);
+
+            var hasRight = value.Match(
+                left: l => false,
+                right: r => true);
+
+            Assert.True(hasRight);
+        }
+
+        [Theory]
+        [ClassData(typeof(IntegerSums))]
+        public void TheSumsOverEitherTypesShouldBeValid(Either<string, int> firstValue, Either<string, int> secondValue, Either<string, int> thirdValue, object reference)
+        {
+            var result =
+                from first in firstValue
+                from second in secondValue
+                from third in thirdValue
+                select first + second + third;
+
+            var temp = result.Match(
+                right: r => reference is int number && number == r,
+                left: l => reference is string word && word == l);
+
+            Assert.True(temp);
+        }
+
+
+    }
+}
---
+++ Funcky/Monads/Either.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Funcky.Monads
+{
+    public class Either<TLeft, TRight>
+    {
+        private readonly TLeft _left;
+        private readonly TRight _right;
+        private readonly bool _isRight;
+
+        private Either(TLeft left)
+        {
+            _left = left;
+            _isRight = false;
+        }
+
+        private Either(TRight right)
+        {
+            _right = right;
+            _isRight = true;
+        }
+
+        public static Either<TLeft, TRight> Left(TLeft left)
+        {
+            return new Either<TLeft, TRight>(left);
+        }
+
+        public static Either<TLeft, TRight> Right(TRight right)
+        {
+            return new Either<TLeft, TRight>(right);
+        }
+
+        public Either<TLeft, TResult> Select<TResult>(Func<TRight, TResult> selector)
+        {
+            if (selector == null)
+            {
+                throw new ArgumentNullException(nameof(selector));
+            }
+
+
+            return _isRight
+                ? Either<TLeft, TResult>.Right(selector(_right))
+                : Either<TLeft, TResult>.Left(_left);
+        }
+
+        public Either<TLeft, TResult> SelectMany<TEither, TResult>(Func<TRight, Either<TLeft, TEither>> eitherSelector, Func<TRight, TEither, TResult> resultSelector)
+        {
+            if (eitherSelector == null)
+            {
+                throw new ArgumentNullException(nameof(eitherSelector));
+            }
+
+            if (resultSelector == null)
+            {
+                throw new ArgumentNullException(nameof(resultSelector));
+            }
+
+            Either<TLeft, TEither> selectedEither = eitherSelector(_right);
+            if (_isRight)
+            {
+                return selectedEither._isRight
+                    ? Either<TLeft, TResult>.Right(resultSelector(_right, selectedEither._right))
+                    : Either<TLeft, TResult>.Left(selectedEither._left);
+            }
+
+            return Either<TLeft, TResult>.Left(_left);
+        }
+
+
+        public TMatchResult Match<TMatchResult>(Func<TLeft, TMatchResult> left, Func<TRight, TMatchResult> right)
+        {
+            return _isRight
+                ? right(_right)
+                : left(_left);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Either<TLeft, TLeft> other
+                && Equals(_isRight, other._isRight)
+                && Equals(_right, other._right)
+                && Equals(_left, other._left);
+        }
+
+        public override int GetHashCode()
+        {
+            return _isRight
+                ? _right.GetHashCode()
+                : _left.GetHashCode();
+        }
+    }
+}
