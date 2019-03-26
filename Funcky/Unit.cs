c464061228dc7859ad393ad213858a28ba185c29
Thomas Bruderer <apophis@apophis.ch>
2019-03-26 15:31:19 +0100

    Add a Unit Type (a real void Type)

---
+++ Funcky/Unit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Funcky
+{
+    public struct Unit : IEquatable<Unit>, IComparable<Unit>
+    {
+        public bool Equals(Unit other) => true;
+
+        public int CompareTo(Unit other) => 0;
+    }
+}
