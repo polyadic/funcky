588ec0a41e87e397d3d8c9134ba7e683a143778d
Thomas Bruderer <apophis@apophis.ch>
2019-10-24 14:04:55 +0200

    Add Extensions making Lazy<T> to a monad

---
+++ Funcky/Monads/LazyExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Funcky.Monads
+{
+    public static class LazyExtensions
+    {
+        public static Lazy<TResult> Select<T, TResult>(this Lazy<T> lazy, Func<T, TResult> selector)
+        {
+            return new Lazy<TResult>(() => selector(lazy.Value));
+        }
+
+        public static Lazy<TResult> SelectMany<T, TResult>(this Lazy<T> lazy, Func<T, Lazy<TResult>> selector)
+        {
+            return SelectMany(lazy, selector, (a, b) => b);
+        }
+
+        public static Lazy<TResult> SelectMany<T, TA, TResult>(this Lazy<T> lazy, Func<T, Lazy<TA>> selector, Func<T, TA, TResult> resultSelector)
+        {
+            return new Lazy<TResult>(() =>
+            {
+                var first = lazy.Value;
+                var second = selector(first).Value;
+                return resultSelector(first, second);
+            });
+        }
+    }
+}
