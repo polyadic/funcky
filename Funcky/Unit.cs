1486657855ea2915fc091a9a9695da2282aa2c6f
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2020-02-08 11:16:46 +0100

    Implement equality operators on Unit

--- Funcky/Unit.cs
+++ Funcky/Unit.cs
@@ -6,6 +6,14 @@ namespace Funcky
     {
         public bool Equals(Unit other) => true;
 
+        public override bool Equals(object obj) => obj is Unit other && Equals(other);
+
+        public override int GetHashCode() => 0;
+
         public int CompareTo(Unit other) => 0;
+
+        public static bool operator ==(Unit lhs, Unit rhs) => true;
+
+        public static bool operator !=(Unit lhs, Unit rhs) => false;
     }
 }
