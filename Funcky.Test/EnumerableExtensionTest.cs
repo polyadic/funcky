5f25be4981a2c054615973cf38ab283a6297f701
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2020-05-26 10:10:31 +0200

    Add WhereSelect extension function for IEnumerable

--- Funcky.Test/EnumerableExtensionTest.cs
+++ Funcky.Test/EnumerableExtensionTest.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Funcky.Extensions;
+using Funcky.Monads;
 using Xunit;
 
 namespace Funcky.Test
@@ -30,6 +32,20 @@ namespace Funcky.Test
 
             Assert.Equal(numbers.Count, sideEffect);
         }
+
+        [Fact]
+        public void WhereSelectFiltersEmptyValues()
+        {
+            var input = Enumerable.Range(0, 10);
+            var expectedResult = new[] { 0, 4, 16, 36, 64 };
+            var result = input.WhereSelect(SquareEvenNumbers);
+            Assert.Equal(expectedResult, result);
+        }
+
+        private static Option<int> SquareEvenNumbers(int number)
+            => IsEven(number) ? Option.Some(number * number) : Option<int>.None();
+
+        private static bool IsEven(int number) => number % 2 == 0;
 
         private void AcceptIntegers(IEnumerable<int> values)
         {
