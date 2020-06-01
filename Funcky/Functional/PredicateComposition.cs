28b429c8727894f3b504b2290140590260adde7b
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2020-06-01 11:09:19 +0200

    Use LINQ methods instead of custom aggregate

--- Funcky/Functional/PredicateComposition.cs
+++ Funcky/Functional/PredicateComposition.cs
@@ -9,29 +9,19 @@ namespace Funcky
         /// Returns a new predicate that is true when all given predicates are true.
         /// </summary>
         public static Predicate<T> All<T>(params Predicate<T>[] predicates)
-            => predicates.Any() ? AllInternal(predicates) : True;
+            => value => predicates.All(predicate => predicate(value));
 
         /// <summary>
         /// Returns a new predicate that is true when any of the given predicates are true.
         /// </summary>
         public static Predicate<T> Any<T>(params Predicate<T>[] predicates)
-            => predicates.Any() ? AnyInternal(predicates) : False;
+            => value => predicates.Any(predicate => predicate(value));
 
         /// <summary>
         /// Returns a new predicate that inverts the given predicate.
         /// </summary>
         public static Predicate<T> Not<T>(Predicate<T> predicate)
             => value => !predicate(value);
-
-        private static Predicate<T> AllInternal<T>(Predicate<T>[] predicates)
-            => value => predicates.Aggregate(
-                true,
-                (result, predicate) => result && predicate(value));
-
-        private static Predicate<T> AnyInternal<T>(Predicate<T>[] predicates)
-            => value => predicates.Aggregate(
-                false,
-                (result, predicate) => result || predicate(value));
 
         private static bool True<T>(T value) => true;
 
