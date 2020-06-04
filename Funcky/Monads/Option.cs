4bb8a4d834395507a53f1201d96395b1d404565a
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2020-06-04 14:35:52 +0200

    Add factory function for creating an option from a nullable value

--- Funcky/Monads/Option.cs
+++ Funcky/Monads/Option.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Funcky.Constraints;
 
 namespace Funcky.Monads
 {
@@ -206,5 +207,22 @@ namespace Funcky.Monads
         {
             return item;
         }
+
+        /// <summary>
+        /// Creates an <see cref="Option{T}"/> from a nullable value.
+        /// </summary>
+        public static Option<T> From<T>(T? item, RequireClass<T>? ω = null)
+            where T : class
+            => item is { } value ? Some(value) : Option<T>.None();
+
+        /// <inheritdoc cref="From{T}(T, RequireClass{T})"/>
+        public static Option<T> From<T>(T item, RequireStruct<T>? ω = null)
+            where T : struct
+            => Some(item);
+
+        /// <inheritdoc cref="From{T}(T, RequireClass{T})"/>
+        public static Option<T> From<T>(T? item)
+            where T : struct
+            => item.HasValue ? Some(item.Value) : Option<T>.None();
     }
 }
