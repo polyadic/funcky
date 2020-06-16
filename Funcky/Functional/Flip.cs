c6693007d1ba76f6b291673157292a2dcd90cdf2
Thomas Bruderer <apophis@apophis.ch>
2020-06-16 12:27:42 +0200

    Add flip overloads

---
+++ Funcky/Functional/Flip.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Diagnostics.Contracts;
+
+namespace Funcky
+{
+    public static partial class Functional
+    {
+        /// <summary>
+        /// Flips the first to arguments of the function.
+        /// </summary>
+        [Pure]
+        public static Func<T2, T1, TResult> Flip<T1, T2, TResult>(Func<T1, T2, TResult> function) =>
+            (p1, p2) => function(p2, p1);
+
+        /// <summary>
+        /// Flips the first to arguments of the function.
+        /// </summary>
+        [Pure]
+        public static Func<T2, T1, T3, TResult> Flip<T1, T2, T3, TResult>(Func<T1, T2, T3, TResult> function) =>
+            (p1, p2, p3) => function(p2, p1, p3);
+
+        /// <summary>
+        /// Flips the first to arguments of the function.
+        /// </summary>
+        [Pure]
+        public static Func<T2, T1, T3, T4, TResult> Flip<T1, T2, T3, T4, TResult>(Func<T1, T2, T3, T4, TResult> function) =>
+            (p1, p2, p3, p4) => function(p2, p1, p3, p4);
+
+        /// <summary>
+        /// Flips the first to arguments of the function.
+        /// </summary>
+        [Pure]
+        public static Func<T2, T1, T3, T4, T5, TResult> Flip<T1, T2, T3, T4, T5, TResult>(Func<T1, T2, T3, T4, T5, TResult> function) =>
+            (p1, p2, p3, p4, p5) => function(p2, p1, p3, p4, p5);
+
+        /// <summary>
+        /// Flips the first to arguments of the function.
+        /// </summary>
+        [Pure]
+        public static Func<T2, T1, T3, T4, T5, T6, TResult> Flip<T1, T2, T3, T4, T5, T6, TResult>(Func<T1, T2, T3, T4, T5, T6, TResult> function) =>
+            (p1, p2, p3, p4, p5, p6) => function(p2, p1, p3, p4, p5, p6);
+
+        /// <summary>
+        /// Flips the first to arguments of the function.
+        /// </summary>
+        [Pure]
+        public static Func<T2, T1, T3, T4, T5, T6, T7, TResult> Flip<T1, T2, T3, T4, T5, T6, T7, TResult>(Func<T1, T2, T3, T4, T5, T6, T7, TResult> function) =>
+            (p1, p2, p3, p4, p5, p6, p7) => function(p2, p1, p3, p4, p5, p6, p7);
+
+        /// <summary>
+        /// Flips the first to arguments of the function.
+        /// </summary>
+        [Pure]
+        public static Func<T2, T1, T3, T4, T5, T6, T7, T8, TResult> Flip<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> function) =>
+            (p1, p2, p3, p4, p5, p6, p7, p8) => function(p2, p1, p3, p4, p5, p6, p7, p8);
+    }
+}
