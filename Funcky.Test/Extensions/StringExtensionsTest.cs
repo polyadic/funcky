97fa40e13ddde494776525b66031f2798b3a1ee2
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2020-06-16 16:44:32 +0200

    Improve tests

--- Funcky.Test/Extensions/StringExtensionsTest.cs
+++ Funcky.Test/Extensions/StringExtensionsTest.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Reflection;
 using Funcky.Extensions;
 using Funcky.Monads;
 using Xunit;
@@ -14,72 +16,99 @@ namespace Funcky.Test.Extensions
         private const char ExistingNeedleChar = 'y';
         private const int NeedlePosition = 2;
 
-        [Fact]
-        public void ReturnsNoneIfNeedleIsNotFound()
+        [Theory]
+        [MemberData(nameof(InvalidIndexes))]
+        public void ReturnsNoneIfNeedleIsNotFound(Option<int> index)
         {
-            AssertIsNone(Haystack.IndexOfOrNone(NonExistingNeedleChar));
-            AssertIsNone(Haystack.IndexOfOrNone(NonExistingNeedleChar, startIndex: 0));
-            AssertIsNone(Haystack.IndexOfOrNone(NonExistingNeedleChar, StringComparison.InvariantCulture));
-            AssertIsNone(Haystack.IndexOfOrNone(NonExistingNeedleChar, startIndex: 0, count: 1));
-            AssertIsNone(Haystack.IndexOfOrNone(NonExistingNeedle));
-            AssertIsNone(Haystack.IndexOfOrNone(NonExistingNeedle, startIndex: 0));
-            AssertIsNone(Haystack.IndexOfOrNone(NonExistingNeedle, startIndex: 0, count: 1));
-            AssertIsNone(Haystack.IndexOfOrNone(NonExistingNeedle, StringComparison.InvariantCulture));
-            AssertIsNone(Haystack.IndexOfOrNone(NonExistingNeedle, startIndex: 0, StringComparison.InvariantCulture));
-            AssertIsNone(Haystack.IndexOfOrNone(NonExistingNeedle, startIndex: 0, count: 1, StringComparison.InvariantCulture));
-            AssertIsNone(Haystack.IndexOfAnyOrNone(new[] { NonExistingNeedleChar }));
-            AssertIsNone(Haystack.IndexOfAnyOrNone(new[] { NonExistingNeedleChar }, startIndex: 0));
-            AssertIsNone(Haystack.IndexOfAnyOrNone(new[] { NonExistingNeedleChar }, startIndex: 0, count: 1));
-            AssertIsNone(Haystack.LastIndexOfOrNone(NonExistingNeedleChar));
-            AssertIsNone(Haystack.LastIndexOfOrNone(NonExistingNeedleChar, startIndex: 0));
-            AssertIsNone(Haystack.LastIndexOfOrNone(NonExistingNeedleChar, startIndex: 0, count: 1));
-            AssertIsNone(Haystack.LastIndexOfOrNone(NonExistingNeedle));
-            AssertIsNone(Haystack.LastIndexOfOrNone(NonExistingNeedle, startIndex: 0));
-            AssertIsNone(Haystack.LastIndexOfOrNone(NonExistingNeedle, startIndex: 0, count: 1));
-            AssertIsNone(Haystack.LastIndexOfOrNone(NonExistingNeedle, StringComparison.InvariantCulture));
-            AssertIsNone(Haystack.LastIndexOfOrNone(NonExistingNeedle, startIndex: 0, StringComparison.InvariantCulture));
-            AssertIsNone(Haystack.LastIndexOfOrNone(NonExistingNeedle, startIndex: 0, count: 1, StringComparison.InvariantCulture));
-            AssertIsNone(Haystack.LastIndexOfAnyOrNone(new[] { NonExistingNeedleChar }));
-            AssertIsNone(Haystack.LastIndexOfAnyOrNone(new[] { NonExistingNeedleChar }, startIndex: 0));
-            AssertIsNone(Haystack.LastIndexOfAnyOrNone(new[] { NonExistingNeedleChar }, startIndex: 0, count: 1));
+            Assert.Equal(Option<int>.None(), index);
+        }
+
+        public static TheoryData<Option<int>> InvalidIndexes()
+            => new TheoryData<Option<int>>
+            {
+                Haystack.IndexOfOrNone(NonExistingNeedleChar),
+                Haystack.IndexOfOrNone(NonExistingNeedleChar, startIndex: 0),
+                Haystack.IndexOfOrNone(NonExistingNeedleChar, StringComparison.InvariantCulture),
+                Haystack.IndexOfOrNone(NonExistingNeedleChar, startIndex: 0, count: 1),
+                Haystack.IndexOfOrNone(NonExistingNeedle),
+                Haystack.IndexOfOrNone(NonExistingNeedle, startIndex: 0),
+                Haystack.IndexOfOrNone(NonExistingNeedle, startIndex: 0, count: 1),
+                Haystack.IndexOfOrNone(NonExistingNeedle, StringComparison.InvariantCulture),
+                Haystack.IndexOfOrNone(NonExistingNeedle, startIndex: 0, StringComparison.InvariantCulture),
+                Haystack.IndexOfOrNone(NonExistingNeedle, startIndex: 0, count: 1, StringComparison.InvariantCulture),
+                Haystack.IndexOfAnyOrNone(new[] { NonExistingNeedleChar }),
+                Haystack.IndexOfAnyOrNone(new[] { NonExistingNeedleChar }, startIndex: 0),
+                Haystack.IndexOfAnyOrNone(new[] { NonExistingNeedleChar }, startIndex: 0, count: 1),
+                Haystack.LastIndexOfOrNone(NonExistingNeedleChar),
+                Haystack.LastIndexOfOrNone(NonExistingNeedleChar, startIndex: 0),
+                Haystack.LastIndexOfOrNone(NonExistingNeedleChar, startIndex: 0, count: 1),
+                Haystack.LastIndexOfOrNone(NonExistingNeedle),
+                Haystack.LastIndexOfOrNone(NonExistingNeedle, startIndex: 0),
+                Haystack.LastIndexOfOrNone(NonExistingNeedle, startIndex: 0, count: 1),
+                Haystack.LastIndexOfOrNone(NonExistingNeedle, StringComparison.InvariantCulture),
+                Haystack.LastIndexOfOrNone(NonExistingNeedle, startIndex: 0, StringComparison.InvariantCulture),
+                Haystack.LastIndexOfOrNone(NonExistingNeedle, startIndex: 0, count: 1, StringComparison.InvariantCulture),
+                Haystack.LastIndexOfAnyOrNone(new[] { NonExistingNeedleChar }),
+                Haystack.LastIndexOfAnyOrNone(new[] { NonExistingNeedleChar }, startIndex: 0),
+                Haystack.LastIndexOfAnyOrNone(new[] { NonExistingNeedleChar }, startIndex: 0, count: 1),
+            };
+
+        [Theory]
+        [MemberData(nameof(ValidIndexes))]
+        public void ReturnsIndexIfNeedleIsFound(Option<int> index)
+        {
+            Assert.Equal(Option.Some(NeedlePosition), index);
+        }
+
+        public static TheoryData<Option<int>> ValidIndexes()
+            => new TheoryData<Option<int>>
+            {
+                Haystack.IndexOfOrNone(ExistingNeedleChar),
+                Haystack.IndexOfOrNone(ExistingNeedleChar, startIndex: 0),
+                Haystack.IndexOfOrNone(ExistingNeedleChar, StringComparison.InvariantCulture),
+                Haystack.IndexOfOrNone(ExistingNeedleChar, startIndex: 0, count: Haystack.Length),
+                Haystack.IndexOfOrNone(ExistingNeedle),
+                Haystack.IndexOfOrNone(ExistingNeedle, startIndex: 0),
+                Haystack.IndexOfOrNone(ExistingNeedle, startIndex: 0, count: Haystack.Length),
+                Haystack.IndexOfOrNone(ExistingNeedle, StringComparison.InvariantCulture),
+                Haystack.IndexOfOrNone(ExistingNeedle, startIndex: 0, StringComparison.InvariantCulture),
+                Haystack.IndexOfOrNone(ExistingNeedle, startIndex: 0, count: Haystack.Length, StringComparison.InvariantCulture),
+                Haystack.IndexOfAnyOrNone(new[] { ExistingNeedleChar }),
+                Haystack.IndexOfAnyOrNone(new[] { ExistingNeedleChar }, startIndex: 0),
+                Haystack.IndexOfAnyOrNone(new[] { ExistingNeedleChar }, startIndex: 0, count: Haystack.Length),
+                Haystack.LastIndexOfOrNone(ExistingNeedleChar),
+                Haystack.LastIndexOfOrNone(ExistingNeedleChar, startIndex: Haystack.Length - 1),
+                Haystack.LastIndexOfOrNone(ExistingNeedleChar, startIndex: Haystack.Length - 1, count: Haystack.Length),
+                Haystack.LastIndexOfOrNone(ExistingNeedle),
+                Haystack.LastIndexOfOrNone(ExistingNeedle, startIndex: Haystack.Length - 1),
+                Haystack.LastIndexOfOrNone(ExistingNeedle, startIndex: Haystack.Length - 1, count: Haystack.Length),
+                Haystack.LastIndexOfOrNone(ExistingNeedle, StringComparison.InvariantCulture),
+                Haystack.LastIndexOfOrNone(ExistingNeedle, startIndex: Haystack.Length - 1, StringComparison.InvariantCulture),
+                Haystack.LastIndexOfOrNone(ExistingNeedle, startIndex: Haystack.Length - 1, count: Haystack.Length, StringComparison.InvariantCulture),
+                Haystack.LastIndexOfAnyOrNone(new[] { ExistingNeedleChar }),
+                Haystack.LastIndexOfAnyOrNone(new[] { ExistingNeedleChar }, startIndex: Haystack.Length - 1),
+                Haystack.LastIndexOfAnyOrNone(new[] { ExistingNeedleChar }, startIndex: Haystack.Length - 1, count: Haystack.Length),
+            };
+
+        [Fact]
+        public void ValidIndexesCoversAllOverloads()
+        {
+            var overloadCount = CountIndexOfOverloads();
+            var validIndexesCount = ValidIndexes().Count();
+            Assert.Equal(overloadCount, validIndexesCount);
         }
 
         [Fact]
-        public void ReturnsIndexIfNeedleIsFound()
+        public void InvalidIndexesCoversAllOverloads()
         {
-            AssertIsSome(NeedlePosition, Haystack.IndexOfOrNone(ExistingNeedleChar));
-            AssertIsSome(NeedlePosition, Haystack.IndexOfOrNone(ExistingNeedleChar, startIndex: 0));
-            AssertIsSome(NeedlePosition, Haystack.IndexOfOrNone(ExistingNeedleChar, StringComparison.InvariantCulture));
-            AssertIsSome(NeedlePosition, Haystack.IndexOfOrNone(ExistingNeedleChar, startIndex: 0, count: Haystack.Length));
-            AssertIsSome(NeedlePosition, Haystack.IndexOfOrNone(ExistingNeedle));
-            AssertIsSome(NeedlePosition, Haystack.IndexOfOrNone(ExistingNeedle, startIndex: 0));
-            AssertIsSome(NeedlePosition, Haystack.IndexOfOrNone(ExistingNeedle, startIndex: 0, count: Haystack.Length));
-            AssertIsSome(NeedlePosition, Haystack.IndexOfOrNone(ExistingNeedle, StringComparison.InvariantCulture));
-            AssertIsSome(NeedlePosition, Haystack.IndexOfOrNone(ExistingNeedle, startIndex: 0, StringComparison.InvariantCulture));
-            AssertIsSome(NeedlePosition, Haystack.IndexOfOrNone(ExistingNeedle, startIndex: 0, count: Haystack.Length, StringComparison.InvariantCulture));
-            AssertIsSome(NeedlePosition, Haystack.IndexOfAnyOrNone(new[] { ExistingNeedleChar }));
-            AssertIsSome(NeedlePosition, Haystack.IndexOfAnyOrNone(new[] { ExistingNeedleChar }, startIndex: 0));
-            AssertIsSome(NeedlePosition, Haystack.IndexOfAnyOrNone(new[] { ExistingNeedleChar }, startIndex: 0, count: Haystack.Length));
-            AssertIsSome(NeedlePosition, Haystack.LastIndexOfOrNone(ExistingNeedleChar));
-            AssertIsSome(NeedlePosition, Haystack.LastIndexOfOrNone(ExistingNeedleChar, startIndex: Haystack.Length - 1));
-            AssertIsSome(NeedlePosition, Haystack.LastIndexOfOrNone(ExistingNeedleChar, startIndex: Haystack.Length - 1, count: Haystack.Length));
-            AssertIsSome(NeedlePosition, Haystack.LastIndexOfOrNone(ExistingNeedle));
-            AssertIsSome(NeedlePosition, Haystack.LastIndexOfOrNone(ExistingNeedle, startIndex: Haystack.Length - 1));
-            AssertIsSome(NeedlePosition, Haystack.LastIndexOfOrNone(ExistingNeedle, startIndex: Haystack.Length - 1, count: Haystack.Length));
-            AssertIsSome(NeedlePosition, Haystack.LastIndexOfOrNone(ExistingNeedle, StringComparison.InvariantCulture));
-            AssertIsSome(NeedlePosition, Haystack.LastIndexOfOrNone(ExistingNeedle, startIndex: Haystack.Length - 1, StringComparison.InvariantCulture));
-            AssertIsSome(NeedlePosition, Haystack.LastIndexOfOrNone(ExistingNeedle, startIndex: Haystack.Length - 1, count: Haystack.Length, StringComparison.InvariantCulture));
-            AssertIsSome(NeedlePosition, Haystack.LastIndexOfAnyOrNone(new[] { ExistingNeedleChar }));
-            AssertIsSome(NeedlePosition, Haystack.LastIndexOfAnyOrNone(new[] { ExistingNeedleChar }, startIndex: Haystack.Length - 1));
-            AssertIsSome(NeedlePosition, Haystack.LastIndexOfAnyOrNone(new[] { ExistingNeedleChar }, startIndex: Haystack.Length - 1, count: Haystack.Length));
+            var overloadCount = CountIndexOfOverloads();
+            var validIndexesCount = InvalidIndexes().Count();
+            Assert.Equal(overloadCount, validIndexesCount);
         }
 
-        private static void AssertIsNone<TItem>(Option<TItem> option)
-            where TItem : notnull
-            => Assert.Equal(default, option);
-
-        private static void AssertIsSome<TItem>(TItem value, Option<TItem> option)
-            where TItem : notnull
-            => Assert.Equal(Option.Some(value), option);
+        private static int CountIndexOfOverloads()
+            => typeof(StringExtensions)
+                .GetMethods(BindingFlags.Public | BindingFlags.Static)
+                .Length;
     }
 }
