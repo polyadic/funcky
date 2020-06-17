bdaaca507114d693a49dc8a521732964bc668ed4
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2020-06-17 12:51:21 +0200

    Add extension methods for throwing stream properties

---
+++ Funcky/Extensions/StreamExtensions.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using Funcky.Monads;
+
+namespace Funcky.Extensions
+{
+    public static class StreamExtensions
+    {
+        /// <summary>Gets the length in bytes of the stream.</summary>
+        /// <returns>A long value representing the length of the stream in bytes or None if the <see cref="Stream"/> does not support seeking.</returns>
+        /// <exception cref="ObjectDisposedException">Methods were called after the stream was closed.</exception>
+        public static Option<long> GetLengthOrNone(this Stream stream)
+            => CatchInvalidOperationException(() => stream.Length);
+
+        /// <summary>Gets or sets a value, in milliseconds, that determines how long the stream will attempt to read before timing out.</summary>
+        /// <returns>A value, in milliseconds, that determines how long the stream will attempt to read before timing out.</returns>
+        public static Option<int> GetReadTimeoutOrNone(this Stream stream)
+            => CatchInvalidOperationException(() => stream.ReadTimeout);
+
+        /// <summary>Gets or sets a value, in milliseconds, that determines how long the stream will attempt to write before timing out.</summary>
+        /// <returns>A value, in milliseconds, that determines how long the stream will attempt to write before timing out.</returns>
+        public static Option<int> GetWriteTimeoutOrNone(this Stream stream)
+            => CatchInvalidOperationException(() => stream.WriteTimeout);
+
+        private static Option<TResult> CatchInvalidOperationException<TResult>(Func<TResult> func)
+            where TResult : notnull
+        {
+            try
+            {
+                return Option.Some(func());
+            }
+            catch (InvalidOperationException)
+            {
+                return Option<TResult>.None();
+            }
+        }
+    }
+}
