f2258bdf2e16019d9447d4098a25fee84ea99555
Thomas Bruderer <apophis@apophis.ch>
2020-06-17 14:37:01 +0200

    Curry, Flip, Uncurry is not and Extension function, should they be?

--- Funcky/Functional/Uncurry.cs
+++ Funcky/Functional/Uncurry.cs
@@ -9,49 +9,49 @@ namespace Funcky
         /// Transforms a curried function into a function that takes multiple arguments.
         /// </summary>
         [Pure]
-        public static Func<T1, T2, TResult> Uncurry<T1, T2, TResult>(this Func<T1, Func<T2, TResult>> function) =>
+        public static Func<T1, T2, TResult> Uncurry<T1, T2, TResult>(Func<T1, Func<T2, TResult>> function) =>
             (p1, p2) => function(p1)(p2);
 
         /// <summary>
         /// Transforms a curried function into a function that takes multiple arguments.
         /// </summary>
         [Pure]
-        public static Func<T1, T2, T3, TResult> Uncurry<T1, T2, T3, TResult>(this Func<T1, Func<T2, Func<T3, TResult>>> function) =>
+        public static Func<T1, T2, T3, TResult> Uncurry<T1, T2, T3, TResult>(Func<T1, Func<T2, Func<T3, TResult>>> function) =>
             (p1, p2, p3) => function(p1)(p2)(p3);
 
         /// <summary>
         /// Transforms a curried function into a function that takes multiple arguments.
         /// </summary>
         [Pure]
-        public static Func<T1, T2, T3, T4, TResult> Uncurry<T1, T2, T3, T4, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, TResult>>>> function) =>
+        public static Func<T1, T2, T3, T4, TResult> Uncurry<T1, T2, T3, T4, TResult>(Func<T1, Func<T2, Func<T3, Func<T4, TResult>>>> function) =>
             (p1, p2, p3, p4) => function(p1)(p2)(p3)(p4);
 
         /// <summary>
         /// Transforms a curried function into a function that takes multiple arguments.
         /// </summary>
         [Pure]
-        public static Func<T1, T2, T3, T4, T5, TResult> Uncurry<T1, T2, T3, T4, T5, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, TResult>>>>> function) =>
+        public static Func<T1, T2, T3, T4, T5, TResult> Uncurry<T1, T2, T3, T4, T5, TResult>(Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, TResult>>>>> function) =>
             (p1, p2, p3, p4, p5) => function(p1)(p2)(p3)(p4)(p5);
 
         /// <summary>
         /// Transforms a curried function into a function that takes multiple arguments.
         /// </summary>
         [Pure]
-        public static Func<T1, T2, T3, T4, T5, T6, TResult> Uncurry<T1, T2, T3, T4, T5, T6, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, TResult>>>>>> function) =>
+        public static Func<T1, T2, T3, T4, T5, T6, TResult> Uncurry<T1, T2, T3, T4, T5, T6, TResult>(Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, TResult>>>>>> function) =>
             (p1, p2, p3, p4, p5, p6) => function(p1)(p2)(p3)(p4)(p5)(p6);
 
         /// <summary>
         /// Transforms a curried function into a function that takes multiple arguments.
         /// </summary>
         [Pure]
-        public static Func<T1, T2, T3, T4, T5, T6, T7, TResult> Uncurry<T1, T2, T3, T4, T5, T6, T7, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, TResult>>>>>>> function) =>
+        public static Func<T1, T2, T3, T4, T5, T6, T7, TResult> Uncurry<T1, T2, T3, T4, T5, T6, T7, TResult>(Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, TResult>>>>>>> function) =>
             (p1, p2, p3, p4, p5, p6, p7) => function(p1)(p2)(p3)(p4)(p5)(p6)(p7);
 
         /// <summary>
         /// Transforms a curried function into a function that takes multiple arguments.
         /// </summary>
         [Pure]
-        public static Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> Uncurry<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, TResult>>>>>>>> function) =>
+        public static Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> Uncurry<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, TResult>>>>>>>> function) =>
             (p1, p2, p3, p4, p5, p6, p7, p8) => function(p1)(p2)(p3)(p4)(p5)(p6)(p7)(p8);
     }
 }
