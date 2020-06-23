b480ebc5fdebe11f96cedb9ed57fd3d4d3e9ea51
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2020-06-23 16:05:50 +0200

    Add custom debugger display for Result

---
+++ Funcky/Monads/Result/Result.Debug.cs
@@ -0,0 +1,28 @@
+using System.Diagnostics;
+
+namespace Funcky.Monads
+{
+    [DebuggerDisplay("{" + nameof(DebuggerDisplay) + ",nq}")]
+    [DebuggerTypeProxy(typeof(ResultDebugView<>))]
+    public readonly partial struct Result<TValidResult>
+    {
+        private string DebuggerDisplay => Match(
+            ok: _ => "Ok",
+            error: _ => "Error");
+    }
+
+    internal sealed class ResultDebugView<TValidResult>
+    {
+        private readonly Result<TValidResult> _option;
+
+        public ResultDebugView(Result<TValidResult> option)
+        {
+            _option = option;
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+        public object Value => _option.Match(
+            ok: value => (object)new { Value = value },
+            error: exception => new { Exception = exception });
+    }
+}
