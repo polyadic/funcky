c8e537281b88a0ffc18e670aefb22ad0fa317b67
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2020-06-26 10:34:38 +0200

    Catch correct exception

--- Funcky/Extensions/StreamExtensions.cs
+++ Funcky/Extensions/StreamExtensions.cs
@@ -10,26 +10,26 @@ namespace Funcky.Extensions
         /// <returns>A long value representing the length of the stream in bytes or None if the <see cref="Stream"/> does not support seeking.</returns>
         /// <exception cref="ObjectDisposedException">Methods were called after the stream was closed.</exception>
         public static Option<long> GetLengthOrNone(this Stream stream)
-            => CatchInvalidOperationException(() => stream.Length);
+            => GetSupportedPropertyOrNone(() => stream.Length);
 
         /// <summary>Gets or sets a value, in milliseconds, that determines how long the stream will attempt to read before timing out.</summary>
         /// <returns>A value, in milliseconds, that determines how long the stream will attempt to read before timing out.</returns>
         public static Option<int> GetReadTimeoutOrNone(this Stream stream)
-            => CatchInvalidOperationException(() => stream.ReadTimeout);
+            => GetSupportedPropertyOrNone(() => stream.ReadTimeout);
 
         /// <summary>Gets or sets a value, in milliseconds, that determines how long the stream will attempt to write before timing out.</summary>
         /// <returns>A value, in milliseconds, that determines how long the stream will attempt to write before timing out.</returns>
         public static Option<int> GetWriteTimeoutOrNone(this Stream stream)
-            => CatchInvalidOperationException(() => stream.WriteTimeout);
+            => GetSupportedPropertyOrNone(() => stream.WriteTimeout);
 
-        private static Option<TResult> CatchInvalidOperationException<TResult>(Func<TResult> func)
+        private static Option<TResult> GetSupportedPropertyOrNone<TResult>(Func<TResult> func)
             where TResult : notnull
         {
             try
             {
                 return Option.Some(func());
             }
-            catch (InvalidOperationException)
+            catch (NotSupportedException)
             {
                 return Option<TResult>.None();
             }
