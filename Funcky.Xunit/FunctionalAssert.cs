a48dc410670bf86593c7ebfbe888d07ef73e8546
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2020-07-06 12:32:54 +0200

    Improve assert exception messages
    - Make sure no unnecessary stack frames appear
    - Expected/actual

--- Funcky.Xunit/FunctionalAssert.cs
+++ Funcky.Xunit/FunctionalAssert.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
 using Funcky.Monads;
 using Xunit;
 using Xunit.Sdk;
@@ -9,40 +10,101 @@ namespace Funcky.Xunit
 {
     public static class FunctionalAssert
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void IsNone<TItem>(Option<TItem> option)
             where TItem : notnull
-            => Assert.Equal(Option<TItem>.None(), option);
+        {
+            try
+            {
+                option.Match(
+                    none: NoOperation,
+                    some: value => throw new IsNoneException(value));
+            }
+            catch (IsNoneException exception)
+            {
+                throw exception;
+            }
+        }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void IsSome<TItem>(TItem expectedValue, Option<TItem> option)
             where TItem : notnull
-            => Assert.Equal(Option.Some(expectedValue), option);
+        {
+            try
+            {
+                Assert.Equal(Option.Some(expectedValue), option);
+            }
+            catch (EqualException)
+            {
+                throw new IsSomeWithExpectedValueException(expectedValue, option);
+            }
+        }
 
         [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static TItem IsSome<TItem>(Option<TItem> option)
             where TItem : notnull
-            => option.Match(
-                   some: Identity,
-                   none: () => throw new XunitException("Expected option to have a value, but was None instead"));
+        {
+            try
+            {
+                return option.Match(
+                    some: Identity,
+                    none: () => throw new IsSomeException());
+            }
+            catch (IsSomeException exception)
+            {
+                throw exception;
+            }
+        }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Exception IsError<TResult>(Result<TResult> result)
-            => result.Match(
-                   error: Identity,
-                   ok: value => throw new XunitException($"Expected result to be an Error, but got a '{value}' instead"));
+        {
+            try
+            {
+                return result.Match(
+                    error: Identity,
+                    ok: value => throw new IsErrorException(value));
+            }
+            catch (IsErrorException exception)
+            {
+                throw exception;
+            }
+        }
 
-        public static void IsError<TResult>(Exception expectedException, Result<TResult> result)
-            => Assert.True(result.Match(ok: False, error: exception => expectedException == exception));
-
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static TResult IsOk<TResult>(Result<TResult> result)
-            => result.Match(
-                   ok: Identity,
-                   error: _ => throw new XunitException("Expected result to be an Ok, but got an Error instead"));
+        {
+            try
+            {
+                return result.Match(
+                    ok: Identity,
+                    error: exception => throw new IsOkException(exception));
+            }
+            catch (IsOkException exception)
+            {
+                throw exception;
+            }
+        }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void IsOk<TResult>(TResult expectedResult, Result<TResult> result)
-            => Assert.Equal(Result.Ok(expectedResult), result);
+        {
+            try
+            {
+                Assert.Equal(Result.Ok(expectedResult), result);
+            }
+            catch (EqualException)
+            {
+                throw new IsOkWithExpectedValueException(expectedResult, result.Select(value => (object?)value));
+            }
+        }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static void Unmatched()
             => throw new UnmatchedException();
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static void Unmatched(string unmatchedCase)
             => throw new UnmatchedException(unmatchedCase);
     }
