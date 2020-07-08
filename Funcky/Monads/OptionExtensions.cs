a75a142718a245bded7b5fed5280317271d447d8
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2020-07-08 15:43:13 +0200

    Make Option<Task> and Option<ValueTask> awaitable

---
+++ Funcky/Monads/OptionExtensions.cs
@@ -0,0 +1,45 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Funcky.Monads
+{
+    public static class OptionExtensions
+    {
+        /// <summary>
+        /// This method is intended for compiler use only
+        /// and are therefore exempt from typical semver guarantees.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static TaskAwaiter GetAwaiter(this Option<Task> option)
+            => option.GetOrElse(Task.CompletedTask).GetAwaiter();
+
+        /// <summary>
+        /// This method is intended for compiler use only
+        /// and are therefore exempt from typical semver guarantees.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static OptionTaskAwaiter<TItem> GetAwaiter<TItem>(this Option<Task<TItem>> option)
+            where TItem : notnull
+            => new OptionTaskAwaiter<TItem>(option.Select(t => t.GetAwaiter()));
+
+        #if NETSTANDARD2_1
+        /// <summary>
+        /// This method is intended for compiler use only
+        /// and are therefore exempt from typical semver guarantees.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static ValueTaskAwaiter GetAwaiter(this Option<ValueTask> option)
+            => option.GetOrElse(default(ValueTask)).GetAwaiter();
+
+        /// <summary>
+        /// This method is intended for compiler use only
+        /// and are therefore exempt from typical semver guarantees.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static OptionValueTaskAwaiter<TItem> GetAwaiter<TItem>(this Option<ValueTask<TItem>> option)
+            where TItem : notnull
+            => new OptionValueTaskAwaiter<TItem>(option.Select(t => t.GetAwaiter()));
+        #endif
+    }
+}
