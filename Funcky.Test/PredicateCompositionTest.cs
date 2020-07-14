79cc0923003d561f0bd835ca999fe782cb309ca7
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2020-07-14 10:48:46 +0200

    Improve naming

--- Funcky.Test/PredicateCompositionTest.cs
+++ Funcky.Test/PredicateCompositionTest.cs
@@ -5,62 +5,62 @@ namespace Funcky.Test
 {
     public class PredicateCompositionTest
     {
-        private const string DummyString = "foo";
+        private const string PlaceholderValue = "foo";
 
         [Fact]
         public void AllReturnsTrueWhenNoPredicatesAreGiven()
         {
             var predicate = All<string>();
-            Assert.True(predicate(DummyString));
+            Assert.True(predicate(PlaceholderValue));
         }
 
         [Fact]
         public void AllReturnsFalseWhenAllOfThePredicatesReturnsFalse()
         {
             var predicate = All<string>(False, False, False);
-            Assert.False(predicate(DummyString));
+            Assert.False(predicate(PlaceholderValue));
         }
 
         [Fact]
         public void AllReturnsFalseWhenOneOfThePredicatesReturnsFalse()
         {
             var predicate = All<string>(False, True, False);
-            Assert.False(predicate(DummyString));
+            Assert.False(predicate(PlaceholderValue));
         }
 
         [Fact]
         public void AllReturnsTrueWhenAllOfThePredicatesReturnsFalse()
         {
             var predicate = All<string>(True, True, True);
-            Assert.True(predicate(DummyString));
+            Assert.True(predicate(PlaceholderValue));
         }
 
         [Fact]
         public void AnyReturnsFalseWhenNoPredicatesAreGiven()
         {
             var predicate = Any<string>();
-            Assert.False(predicate(DummyString));
+            Assert.False(predicate(PlaceholderValue));
         }
 
         [Fact]
         public void AnyReturnsTrueWhenOneOfThePredicatesReturnsTrue()
         {
             var predicate = Any<string>(False, True, False);
-            Assert.True(predicate(DummyString));
+            Assert.True(predicate(PlaceholderValue));
         }
 
         [Fact]
         public void AnyReturnsTrueWhenAllOfThePredicatesReturnsTrue()
         {
             var predicate = Any<string>(True, True, True);
-            Assert.True(predicate(DummyString));
+            Assert.True(predicate(PlaceholderValue));
         }
 
         [Fact]
         public void AnyReturnsFalseWhenAllOfThePredicatesReturnsFalse()
         {
             var predicate = Any<string>(False, False, False);
-            Assert.False(predicate(DummyString));
+            Assert.False(predicate(PlaceholderValue));
         }
     }
 }
