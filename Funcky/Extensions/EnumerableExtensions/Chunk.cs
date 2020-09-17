fb5fcdaa3dae11279c18657ba126c91beee7e257
Thomas Bruderer <apophis@apophis.ch>
2020-09-17 09:57:49 +0200

    Optimize Chunk implementation

--- Funcky/Extensions/EnumerableExtensions/Chunk.cs
+++ Funcky/Extensions/EnumerableExtensions/Chunk.cs
@@ -16,16 +16,41 @@ namespace Funcky.Extensions
         /// <returns>A sequence of equally sized chunks containing elements of the source collection.</returns>
         [Pure]
         public static IEnumerable<IEnumerable<TSource>> Chunk<TSource>(this IEnumerable<TSource> source, int size)
-            => Chunk(source, size, x => x);
+            => Chunk(source, size, Functional.Identity);
 
         [Pure]
         public static IEnumerable<TResult> Chunk<TSource, TResult>(this IEnumerable<TSource> source, int size, Func<IEnumerable<TSource>, TResult> resultSelector)
         {
-            while (source.Any())
+            ValidateSize(size);
+
+            return ChunkEnumerable(source, size, resultSelector);
+        }
+
+        private static void ValidateSize(int size)
+        {
+            if (size <= 0)
             {
-                yield return resultSelector(source.Take(size));
-                source = source.Skip(size);
+                throw new ArgumentOutOfRangeException(nameof(size), size, "Size must be bigger than 0");
             }
+        }
+
+        private static IEnumerable<TResult> ChunkEnumerable<TSource, TResult>(IEnumerable<TSource> source, int size, Func<IEnumerable<TSource>, TResult> resultSelector)
+        {
+            using var sourceEnumerator = source.GetEnumerator();
+
+            while (sourceEnumerator.MoveNext())
+            {
+                yield return resultSelector(TakeSkip(sourceEnumerator, size).ToList());
+            }
+        }
+
+        private static IEnumerable<TSource> TakeSkip<TSource>(IEnumerator<TSource> source, int size)
+        {
+            do
+            {
+                yield return source.Current;
+            }
+            while (--size > 0 && source.MoveNext());
         }
     }
 }
