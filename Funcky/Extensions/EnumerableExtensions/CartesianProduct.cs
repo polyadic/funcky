0ff2ba83372cd431938e1ce3752bcf65c0586f0c
Thomas Bruderer <apophis@apophis.ch>
2020-09-18 15:44:17 +0200

    improved implementation of  CartesianProduct

--- Funcky/Extensions/EnumerableExtensions/CartesianProduct.cs
+++ Funcky/Extensions/EnumerableExtensions/CartesianProduct.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Linq;
@@ -7,14 +8,18 @@ namespace Funcky.Extensions
     public static partial class EnumerableExtensions
     {
         [Pure]
-        public static IEnumerable<(TFirstSource A, TSecondSource B)> CartesianProduct<TFirstSource, TSecondSource>(
-            this IEnumerable<TFirstSource> first, IEnumerable<TSecondSource> second)
-            where TFirstSource : notnull
-            where TSecondSource : notnull
-        {
-            return from a in first
-                   from b in second
-                   select (a, b);
-        }
+        public static IEnumerable<(TFirstSource First, TSecondSource Second)> CartesianProduct<TFirstSource,
+            TSecondSource>(
+            this IEnumerable<TFirstSource> firstSequence, IEnumerable<TSecondSource> secondSequence) =>
+                CartesianProduct(firstSequence, secondSequence, ValueTuple.Create);
+
+        [Pure]
+        public static IEnumerable<TResult> CartesianProduct<TSource1, TSource2, TResult>(
+            this IEnumerable<TSource1> firstSequence,
+            IEnumerable<TSource2> secondSequence,
+            Func<TSource1, TSource2, TResult> resultSelector) =>
+                from first in firstSequence
+                from second in secondSequence
+                select resultSelector(first, second);
     }
 }
