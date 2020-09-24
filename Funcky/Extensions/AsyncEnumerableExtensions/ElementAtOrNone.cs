44c118996a269ac84224e96c44225f36ad4fadcb
Thomas Bruderer <apophis@apophis.ch>
2020-09-24 14:45:17 +0200

    Async version of ElementAtOrNone

--- Funcky/Extensions/AsyncEnumerableExtensions/ElementAtOrNone.cs
+++ Funcky/Extensions/AsyncEnumerableExtensions/ElementAtOrNone.cs
@@ -8,11 +8,27 @@ namespace Funcky.Extensions
 {
     public static partial class AsyncEnumerableExtensions
     {
+        /// <summary>
+        /// Returns the element at a specified index in a sequence or an <see cref="Option{T}.None" /> value if the index is out of range.
+        /// </summary>
+        /// <typeparam name="TSource">The type of element contained by the sequence.</typeparam>
+        /// <param name="source">The sequence to find an element in.</param>
+        /// <param name="index">The index for the element to retrieve.</param>
+        /// <param name="cancellationToken">cancellation token.</param>
+        /// <returns>The item at the specified index, or <see cref="Option{T}.None" /> if the index is not found.</returns>
         [Pure]
         public static ValueTask<Option<TSource>> ElementAtOrNoneAsync<TSource>(this IAsyncEnumerable<TSource> source, int index, CancellationToken cancellationToken = default)
             where TSource : notnull
             => source.ElementAtOrNoneWithCancellationAsync(index, cancellationToken);
 
+        /// <summary>
+        /// Returns the element at a specified index in a sequence or an <see cref="Option{T}.None" /> value if the index is out of range.
+        /// </summary>
+        /// <typeparam name="TSource">The type of element contained by the sequence.</typeparam>
+        /// <param name="source">The sequence to find an element in.</param>
+        /// <param name="index">The index for the element to retrieve.</param>
+        /// <param name="cancellationToken">cancellation token.</param>
+        /// <returns>The item at the specified index, or <see cref="Option{T}.None" /> if the index is not found.</returns>
         [Pure]
         public static async ValueTask<Option<TSource>> ElementAtOrNoneWithCancellationAsync<TSource>(this IAsyncEnumerable<TSource> source, int index, CancellationToken cancellationToken = default)
             where TSource : notnull
@@ -24,6 +40,7 @@ namespace Funcky.Extensions
                 {
                     return Option.Some(item);
                 }
+
                 currentIndex++;
             }
 
