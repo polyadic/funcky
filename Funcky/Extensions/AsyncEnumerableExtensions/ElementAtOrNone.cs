08ee87d1b80994376e6e0ea1e2711cfbebd7a936
Thomas Bruderer <apophis@apophis.ch>
2020-09-24 14:30:33 +0200

    Added an Async of `ElementAtOrNone`

---
+++ Funcky/Extensions/AsyncEnumerableExtensions/ElementAtOrNone.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Threading;
+using System.Threading.Tasks;
+using Funcky.Monads;
+
+namespace Funcky.Extensions
+{
+    public static partial class AsyncEnumerableExtensions
+    {
+        [Pure]
+        public static ValueTask<Option<TSource>> ElementAtOrNoneAsync<TSource>(this IAsyncEnumerable<TSource> source, int index, CancellationToken cancellationToken = default)
+            where TSource : notnull
+            => source.ElementAtOrNoneWithCancellationAsync(index, cancellationToken);
+
+        [Pure]
+        public static async ValueTask<Option<TSource>> ElementAtOrNoneWithCancellationAsync<TSource>(this IAsyncEnumerable<TSource> source, int index, CancellationToken cancellationToken = default)
+            where TSource : notnull
+        {
+            int currentIndex = 0;
+            await foreach (var item in source.WithCancellation(cancellationToken))
+            {
+                if (currentIndex == index)
+                {
+                    return Option.Some(item);
+                }
+            }
+
+            return Option<TSource>.None();
+        }
+    }
+}
--- changelog.md
+++ changelog.md
@@ -99,7 +99,7 @@
 
 ## Unreleased
 * Funcky now uses `.ConfigureAwait(false)` everywhere `await` is used.
-* Add `WhereSelect`, `FirstOrNoneAsync`, `LastOrNoneAsync` and `SingleOrNoneAsync` extension methods for `IAsyncEnumerable`.
+* Add `WhereSelect`, `FirstOrNoneAsync`, `LastOrNoneAsync`, `SingleOrNoneAsync` and `ElementAtOrNone` extension methods for `IAsyncEnumerable`.
 * Depend on `System.Collections.Immutable`
 * Add `Chunk`, `Interleave` extension methods for `IEnumerable`.
-* Add `ElementAtOrNone` extension methods for `IEnumerable`.
+* Add `ElementAtOrNone` extension methods for `IEnumerable`
