69e9935388f80cc402e2475e08ce8e37b5dd6d63
Thomas Bruderer <apophis@apophis.ch>
2020-09-24 17:28:29 +0200

    An Async variant for TakeEvery...

---
+++ Funcky/Extensions/AsyncEnumerableExtensions/TakeEvery.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Threading;
+using System.Threading.Tasks;
+using Funcky.Monads;
+
+namespace Funcky.Extensions
+{
+    public static partial class AsyncEnumerableExtensions
+    {
+        /// <summary>
+        /// Returns every n'th (interval) element from the source sequence.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+        /// <param name="source">The source sequence.</param>
+        /// <param name="interval">the interval between elements in the source sequences.</param>
+        /// <returns>Returns a sequence where only every n'th element (interval) of source sequnce is used. </returns>
+        [Pure]
+        public static async IAsyncEnumerable<TSource> TakeEvery<TSource>(this IAsyncEnumerable<TSource> source, int interval)
+            where TSource : notnull
+        {
+            var currentIndex = 0;
+            await foreach (var item in source)
+            {
+                if (currentIndex % interval == 0)
+                {
+                    yield return item;
+                }
+
+                currentIndex++;
+            }
+        }
+    }
+}
