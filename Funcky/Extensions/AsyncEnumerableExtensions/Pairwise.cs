ba6fd8e4ebf5ebf4f05a8dd2557d1d1afaec4a78
Thomas Bruderer <apophis@apophis.ch>
2020-09-25 16:31:08 +0200

    Added Pairwise for IAsyncEnumerable

---
+++ Funcky/Extensions/AsyncEnumerableExtensions/Pairwise.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Funcky.Extensions
+{
+    public static partial class AsyncEnumerableExtensions
+    {
+        /// <summary>
+        /// Returns a sequence of pair-wise tuples from the underlying sequence.
+        /// </summary>
+        /// <param name="source">The source sequence.</param>
+        /// <param name="cancellationToken">A cancellation token.</param>
+        /// <typeparam name="TSource">Type of the elements in <paramref name="source"/> sequence.</typeparam>
+        /// <returns>Returns a sequence of ValueTuple-pairs.</returns>
+        [Pure]
+        public static IAsyncEnumerable<(TSource Front, TSource Back)> Pairwise<TSource>(this IAsyncEnumerable<TSource> source, CancellationToken cancellationToken = default)
+            => Pairwise(source, ValueTuple.Create, cancellationToken);
+
+        /// <summary>
+        /// Applies a function to the element and its successor pairwise from the underlying sequence and returns a new sequence with these results.
+        /// I.e. the resulting sequence has one element less then source sequence.
+        /// </summary>
+        /// <param name="source">The source sequence.</param>
+        /// <param name="resultSelector">A function of type (TSource, TSource) -> TResult which creates the results from the pairs.</param>
+        /// <param name="cancellationToken">A cancellation token.</param>
+        /// <typeparam name="TSource">Type of the elements in <paramref name="source"/> sequence.</typeparam>
+        /// <typeparam name="TResult">The type of the elements in the result Sequence.</typeparam>
+        /// <returns>Returns a sequence of ValueTuple-pairs.</returns>
+        [Pure]
+        public static async IAsyncEnumerable<TResult> Pairwise<TSource, TResult>(this IAsyncEnumerable<TSource> source, Func<TSource, TSource, TResult> resultSelector, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            await using var enumerator = source.GetAsyncEnumerator(cancellationToken);
+
+            if (await enumerator.MoveNextAsync() == false)
+            {
+                yield break;
+            }
+
+            for (var previous = enumerator.Current; await enumerator.MoveNextAsync(); previous = enumerator.Current)
+            {
+                yield return resultSelector(previous, enumerator.Current);
+            }
+        }
+    }
+}
