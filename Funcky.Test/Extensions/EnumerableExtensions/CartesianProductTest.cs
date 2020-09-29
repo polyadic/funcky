74cc6652f395fb7924073aac5ac392d6a050fb8b
Thomas Bruderer <apophis@apophis.ch>
2020-09-29 14:56:49 +0200

    Add test and documentation for CartesianProduct

---
+++ Funcky.Test/Extensions/EnumerableExtensions/CartesianProductTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Funcky.Extensions;
+using Xunit;
+
+namespace Funcky.Test.Extensions.EnumerableExtensions
+{
+    public class CartesianProductTest
+    {
+        [Fact]
+        public void GivenTwoEmptySetsCartesianProductReturnsAnEmptySet()
+        {
+            var ints = Enumerable.Empty<int>();
+            var strings = Enumerable.Empty<string>();
+
+            Assert.Empty(ints.CartesianProduct(strings));
+        }
+
+        [Fact]
+        public void GivenOneEmptySetCartesianProductReturnsAnEmptySet()
+        {
+            var ints = ImmutableList<int>.Empty.Add(1).Add(2).Add(42);
+            var strings = Enumerable.Empty<string>();
+
+            Assert.Empty(ints.CartesianProduct(strings));
+        }
+
+        [Fact]
+        public void GivenTwoSingleElementSequencesWeGetASinglePairBack()
+        {
+            var ints = ImmutableList<int>.Empty.Add(1);
+            var strings = ImmutableList<string>.Empty.Add("Hello");
+
+            var product = ints.CartesianProduct(strings);
+            Assert.Single(product);
+        }
+
+        [Fact]
+        public void GivenTowSequencesWithMultipleItemsCartesianProductWithProjectReturnsAllPossibleCombinations()
+        {
+            var names = ImmutableList<string>.Empty.Add("Ruby").Add("Lucas");
+            var greetings = ImmutableList<string>.Empty.Add("Hello").Add("Good Morning");
+
+            var allPossibleGreetings = greetings.CartesianProduct(names, (greeting, name) => $"{greeting} {name}");
+            Assert.Equal(new[] { "Hello Ruby", "Hello Lucas", "Good Morning Ruby", "Good Morning Lucas" }, allPossibleGreetings);
+        }
+    }
+}
--- Funcky/Extensions/EnumerableExtensions/CartesianProduct.cs
+++ Funcky/Extensions/EnumerableExtensions/CartesianProduct.cs
@@ -7,19 +7,37 @@ namespace Funcky.Extensions
 {
     public static partial class EnumerableExtensions
     {
+        /// <summary>
+        /// CartesianProduct returns a set of ordered pairs where each element of the first sequence is paired with each element of the second sequence.
+        /// </summary>
+        /// <param name="firstSequence">The first sequence.</param>
+        /// <param name="secondSequence">The second sequence.</param>
+        /// <typeparam name="TFirstSource">The type of the elements in the first sequence.</typeparam>
+        /// <typeparam name="TSecondSource">The type of the elements in the second sequence.</typeparam>
+        /// <returns>Returns a list of all combinations as pairs.</returns>
         [Pure]
         public static IEnumerable<(TFirstSource First, TSecondSource Second)> CartesianProduct<TFirstSource,
             TSecondSource>(
             this IEnumerable<TFirstSource> firstSequence, IEnumerable<TSecondSource> secondSequence) =>
                 CartesianProduct(firstSequence, secondSequence, ValueTuple.Create);
 
+        /// <summary>
+        /// CartesianProduct returns a set the projection of ordered pairs where each element of the first sequence is paired with each element of the second sequence.
+        /// </summary>
+        /// <param name="firstSequence">The first sequence.</param>
+        /// <param name="secondSequence">The second sequence.</param>
+        /// <param name="resultSelector">A projection which has transforms the input pairs into a result.</param>
+        /// <typeparam name="TFirstSource">The type of the elements in the first sequence.</typeparam>
+        /// <typeparam name="TSecondSource">The type of the elements in the second sequence.</typeparam>
+        /// <typeparam name="TResult">The result type of the given projection.</typeparam>
+        /// <returns> sequence of elements returned by/// <paramref name="resultSelector"/>.</returns>
         [Pure]
-        public static IEnumerable<TResult> CartesianProduct<TSource1, TSource2, TResult>(
-            this IEnumerable<TSource1> firstSequence,
-            IEnumerable<TSource2> secondSequence,
-            Func<TSource1, TSource2, TResult> resultSelector) =>
-                from first in firstSequence
-                from second in secondSequence
-                select resultSelector(first, second);
+        public static IEnumerable<TResult> CartesianProduct<TFirstSource, TSecondSource, TResult>(
+            this IEnumerable<TFirstSource> firstSequence,
+            IEnumerable<TSecondSource> secondSequence,
+            Func<TFirstSource, TSecondSource, TResult> resultSelector) =>
+                from firstElement in firstSequence
+                from secondElement in secondSequence
+                select resultSelector(firstElement, secondElement);
     }
 }
