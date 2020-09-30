49bc64de5feeff7f5c20427929b8e44886392a61
Thomas Bruderer <apophis@apophis.ch>
2020-09-30 11:05:33 +0200

    Fix spelling

--- Funcky/Extensions/EnumerableExtensions/TakeEvery.cs
+++ Funcky/Extensions/EnumerableExtensions/TakeEvery.cs
@@ -8,12 +8,12 @@ namespace Funcky.Extensions
     public static partial class EnumerableExtensions
     {
         /// <summary>
-        /// Returns every n'th (interval) element from the source sequence.
+        /// Returns every nth (interval) element from the source sequence.
         /// </summary>
         /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
         /// <param name="source">The source sequence.</param>
         /// <param name="interval">the interval between elements in the source sequences.</param>
-        /// <returns>Returns a sequence where only every n'th element (interval) of source sequnce is used. </returns>
+        /// <returns>Returns a sequence where only every nth element (interval) of source sequence is used. </returns>
         [Pure]
         public static IEnumerable<TSource> TakeEvery<TSource>(this IEnumerable<TSource> source, int interval)
         {
