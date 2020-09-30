83d8567996343dcdf09e841d74460d9769054518
Thomas Bruderer <apophis@apophis.ch>
2020-09-30 10:14:17 +0200

    Refactor Merge implementation

--- Funcky/Extensions/EnumerableExtensions/Merge.cs
+++ Funcky/Extensions/EnumerableExtensions/Merge.cs
@@ -1,37 +1,66 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics.Contracts;
 using System.Linq;
+using Funcky.Monads;
 
 namespace Funcky.Extensions
 {
     public static partial class EnumerableExtensions
     {
         [Pure]
-        public static IEnumerable<TSource> Merge<TSource>(this IEnumerable<TSource> sequence, IComparer<TSource> comparer, params IEnumerable<TSource>[] otherSequences)
-            => ImmutableList.Create(sequence).AddRange(otherSequences).Merge(comparer);
+        public static IEnumerable<TSource> Merge<TSource>(this IEnumerable<TSource> sequence1, IEnumerable<TSource> sequence2, IComparer<TSource>? comparer = null)
+            => ImmutableList.Create(sequence1).Add(sequence2).Merge(comparer);
 
         [Pure]
-        public static IEnumerable<TSource> Merge<TSource>(this IEnumerable<IEnumerable<TSource>> sources, IComparer<TSource> comparer)
-        {
-            var sourceEnumerators = ImmutableList.Create<IEnumerator<TSource>>().AddRange(sources.Select(s => s.GetEnumerator()));
-            var enumerators = sourceEnumerators.RemoveAll(s => !s.MoveNext());
+        public static IEnumerable<TSource> Merge<TSource>(this IEnumerable<TSource> sequence1, IEnumerable<TSource> sequence2, IEnumerable<TSource> sequence3, IComparer<TSource>? comparer = null)
+            => ImmutableList.Create(sequence1).Add(sequence2).Add(sequence3).Merge(comparer);
 
+        [Pure]
+        public static IEnumerable<TSource> Merge<TSource>(this IEnumerable<TSource> sequence1, IEnumerable<TSource> sequence2, IEnumerable<TSource> sequence3, IEnumerable<TSource> sequence4, IComparer<TSource>? comparer = null)
+            => ImmutableList.Create(sequence1).Add(sequence2).Add(sequence3).Add(sequence4).Merge(comparer);
+
+        [Pure]
+        public static IEnumerable<TSource> Merge<TSource>(this IEnumerable<IEnumerable<TSource>> sources, IComparer<TSource>? comparer = null)
+        {
+            var enumerators = GetMergeEnumerators(sources);
+            var result = MergeEnumerators(enumerators.RemoveAll(MoveNextMerge), GetMergeComparer(comparer));
+
+            enumerators.ForEach(s => s.Dispose());
+
+            return result;
+        }
+
+        private static ImmutableList<IEnumerator<TSource>> GetMergeEnumerators<TSource>(IEnumerable<IEnumerable<TSource>> sources)
+            => ImmutableList.Create<IEnumerator<TSource>>().AddRange(sources.Select(s => s.GetEnumerator()));
+
+        private static IComparer<TSource> GetMergeComparer<TSource>(IComparer<TSource>? comparer)
+            => Option.FromNullable(comparer).GetOrElse(Comparer<TSource>.Default);
+
+        private static bool MoveNextMerge<TSource>(IEnumerator<TSource> enumerator)
+            => !enumerator.MoveNext();
+
+        private static IEnumerable<TSource> MergeEnumerators<TSource>(ImmutableList<IEnumerator<TSource>> enumerators, IComparer<TSource> comparer)
+        {
             while (!enumerators.IsEmpty)
             {
                 var minimum = FindMinimum(enumerators, comparer);
                 yield return minimum.Current;
 
-                if (!minimum.MoveNext())
-                {
-                    enumerators = enumerators.Remove(minimum);
-                }
+                enumerators = RemoveYielded(minimum, enumerators);
             }
-
-            sourceEnumerators.ForEach(s => s.Dispose());
         }
 
+        private static ImmutableList<IEnumerator<TSource>> RemoveYielded<TSource>(IEnumerator<TSource> minimum, ImmutableList<IEnumerator<TSource>> enumerators) =>
+            minimum.MoveNext()
+                ? enumerators
+                : enumerators.Remove(minimum);
+
         private static IEnumerator<TSource> FindMinimum<TSource>(ImmutableList<IEnumerator<TSource>> enumerators, IComparer<TSource> comparer)
-            => enumerators.Aggregate((enumerator, minimum) => comparer.Compare(minimum.Current, enumerator.Current) <= 0 ? minimum : enumerator);
+            => enumerators.Aggregate(Minimum(comparer));
+
+        private static Func<IEnumerator<TSource>, IEnumerator<TSource>, IEnumerator<TSource>> Minimum<TSource>(IComparer<TSource> comparer)
+            => (enumerator, minimum) => comparer.Compare(minimum.Current, enumerator.Current) <= 0 ? minimum : enumerator;
     }
 }
