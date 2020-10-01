e9ba75b052772d042ef550c3d5cc0891f251961e
Thomas Bruderer <apophis@apophis.ch>
2020-10-01 08:30:48 +0200

    Add first implementation of WithLast

---
+++ Funcky.Test/Extensions/EnumerableExtensions/WithLastTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using Funcky.Extensions;
+using Xunit;
+
+namespace Funcky.Test.Extensions.EnumerableExtensions
+{
+    public class WithLastTest
+    {
+        [Fact]
+        public void AnEmptySequenceWithLastReturnsAnEmptySequence()
+        {
+            var emptySequence = Enumerable.Empty<string>();
+
+            Assert.Empty(emptySequence.WithLast());
+        }
+
+        [Fact]
+        public void ASequenceWithOneElementWithFirstHasOneElementWhichIsMarkedLast()
+        {
+            const string expectedValue = "Hello world!";
+            var oneElementSequence = Enumerable.Repeat(expectedValue, 1);
+
+            foreach (var (value, isLast) in oneElementSequence.WithLast())
+            {
+                Assert.Equal(expectedValue, value);
+                Assert.True(isLast);
+            }
+        }
+
+        [Fact]
+        public void ASequenceWithMultipleElementsWithLastMarksTheLastElement()
+        {
+            const int length = 20;
+            var sequence = Enumerable.Range(1, length);
+
+            foreach (var (value, isLast) in sequence.WithLast())
+            {
+                Assert.Equal(value == length, isLast);
+            }
+        }
+    }
+}
---
+++ Funcky/Extensions/EnumerableExtensions/WithLast.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace Funcky.Extensions
+{
+    public static partial class EnumerableExtensions
+    {
+        [Pure]
+        public static IEnumerable<ValueWithLast<TSource>> WithLast<TSource>(this IEnumerable<TSource> source)
+        {
+            using var enumerator = source.GetEnumerator();
+
+            if (!enumerator.MoveNext())
+            {
+                yield break;
+            }
+
+            var current = enumerator.Current;
+            while (enumerator.MoveNext())
+            {
+                yield return new ValueWithLast<TSource>(current, false);
+                current = enumerator.Current;
+            }
+
+            yield return new ValueWithLast<TSource>(current, true);
+        }
+
+        public readonly struct ValueWithLast<TValue>
+        {
+            public ValueWithLast(TValue value, bool isLast)
+            {
+                Value = value;
+                IsLast = isLast;
+            }
+
+            public TValue Value { get; }
+
+            public bool IsLast { get; }
+
+            public void Deconstruct(out TValue value, out bool isLast)
+            {
+                value = Value;
+                isLast = IsLast;
+            }
+        }
+    }
+}
