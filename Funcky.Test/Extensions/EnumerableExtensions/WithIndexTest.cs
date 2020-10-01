93007ffdb5dcf5a4ec2465f54c28f48745435766
Thomas Bruderer <apophis@apophis.ch>
2020-10-01 08:30:27 +0200

    Add first implementation of WithIndex

---
+++ Funcky.Test/Extensions/EnumerableExtensions/WithIndexTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using Funcky.Extensions;
+using Xunit;
+
+namespace Funcky.Test.Extensions.EnumerableExtensions
+{
+    public class WithIndexTest
+    {
+        [Fact]
+        public void AnEmptySequenceWithIndexReturnsAnEmptySequence()
+        {
+            var emptySequence = Enumerable.Empty<string>();
+
+            Assert.Empty(emptySequence.WithIndex());
+        }
+
+        [Fact]
+        public void ASequenceWithOneElementWithIndexHasTheIndexZero()
+        {
+            const string expectedValue = "Hello world!";
+            var oneElementSequence = Enumerable.Repeat(expectedValue, 1);
+
+            foreach (var (value, index) in oneElementSequence.WithIndex())
+            {
+                Assert.Equal(expectedValue, value);
+                Assert.Equal(0, index);
+            }
+        }
+
+        [Fact]
+        public void ASequenceWithMultipleElementsWithIndexHaveAscendingIndices()
+        {
+            var sequence = Enumerable.Range(0, 20);
+
+            foreach (var (value, index) in sequence.WithIndex())
+            {
+                Assert.Equal(value, index);
+            }
+        }
+    }
+}
---
+++ Funcky/Extensions/EnumerableExtensions/WithIndex.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
+namespace Funcky.Extensions
+{
+    public static partial class EnumerableExtensions
+    {
+        [Pure]
+        public static IEnumerable<ValueWithIndex<TSource>> WithIndex<TSource>(this IEnumerable<TSource> source)
+            => source.Select((value, index) => new ValueWithIndex<TSource>(value, index));
+
+        public readonly struct ValueWithIndex<TValue>
+        {
+            public ValueWithIndex(TValue value, int index)
+            {
+                Value = value;
+                Index = index;
+            }
+
+            public TValue Value { get; }
+
+            public int Index { get; }
+
+            public void Deconstruct(out TValue value, out int index)
+            {
+                value = Value;
+                index = Index;
+            }
+        }
+    }
+}
