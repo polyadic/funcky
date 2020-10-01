012f3a8a38fbafa8fb5ee92a656f767152d96526
Thomas Bruderer <apophis@apophis.ch>
2020-10-01 16:41:01 +0200

    We should use Option<T> instead of ?

--- Funcky.Xunit/FunctionalAssert/SetEquals.cs
+++ Funcky.Xunit/FunctionalAssert/SetEquals.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using Funcky.Monads;
 using Xunit.Sdk;
@@ -8,11 +9,11 @@ namespace Funcky.Xunit
     public static partial class FunctionalAssert
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void IsSetEquals<TITem>(IEnumerable<TITem> expected, IEnumerable<TITem> actual, IEqualityComparer<TITem>? equalityComparer = null)
+        public static void IsSetEquals<TITem>(IEnumerable<TITem> expected, IEnumerable<TITem> actual, Option<IEqualityComparer<TITem>> equalityComparer = default)
         {
             try
             {
-                var referenceSet = new HashSet<TITem>(expected, Option.FromNullable(equalityComparer).GetOrElse(EqualityComparer<TITem>.Default));
+                var referenceSet = new HashSet<TITem>(expected, equalityComparer.GetOrElse(EqualityComparer<TITem>.Default));
                 if (!referenceSet.SetEquals(actual))
                 {
                     throw new NotEqualException(expected.ToString(), actual.ToString());
