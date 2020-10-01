3585a341e5ad1c64b886e1edae8867f6ff0125d5
Thomas Bruderer <apophis@apophis.ch>
2020-10-01 16:07:55 +0200

    Add basic SetEquals assert

---
+++ Funcky.Xunit/FunctionalAssert/SetEquals.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Xunit.Sdk;
+
+namespace Funcky.Xunit
+{
+    public static partial class FunctionalAssert
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void IsSetEquals<TITem>(IEnumerable<TITem> expected, IEnumerable<TITem> actual)
+        {
+            try
+            {
+                var referenceSet = new HashSet<TITem>(expected, EqualityComparer<TITem>.Default);
+                if (!referenceSet.SetEquals(actual))
+                {
+                    throw new NotEqualException(expected.ToString(), actual.ToString());
+                }
+            }
+            catch (IsNoneException exception)
+            {
+                throw exception;
+            }
+        }
+    }
+}
