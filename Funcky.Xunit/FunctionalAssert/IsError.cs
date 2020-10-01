bd0e4f2cc353678b2c371ad982c002ca730be6c0
Thomas Bruderer <apophis@apophis.ch>
2020-10-01 15:44:12 +0200

    Split the function asserts into a partial class

---
+++ Funcky.Xunit/FunctionalAssert/IsError.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Funcky.Monads;
+using Xunit.Sdk;
+using static Funcky.Functional;
+
+namespace Funcky.Xunit
+{
+    public static partial class FunctionalAssert
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Exception IsError<TResult>(Result<TResult> result)
+        {
+            try
+            {
+                return result.Match(
+                    error: Identity,
+                    ok: value => throw new IsErrorException(value));
+            }
+            catch (IsErrorException exception)
+            {
+                throw exception;
+            }
+        }
+    }
+}
