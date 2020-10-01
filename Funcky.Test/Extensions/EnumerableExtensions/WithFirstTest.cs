5bfc9b4fe38e9861bf0a8c910346d46b25a676e3
Thomas Bruderer <apophis@apophis.ch>
2020-10-01 08:30:40 +0200

    Add first implementation of WithFirst

---
+++ Funcky.Test/Extensions/EnumerableExtensions/WithFirstTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using Funcky.Extensions;
+using Xunit;
+
+namespace Funcky.Test.Extensions.EnumerableExtensions
+{
+    public class WithFirstTest
+    {
+        [Fact]
+        public void AnEmptySequenceWithFirstReturnsAnEmptySequence()
+        {
+            var emptySequence = Enumerable.Empty<string>();
+
+            Assert.Empty(emptySequence.WithFirst());
+        }
+
+        [Fact]
+        public void ASequenceWithOneElementWithFirstHasOneElementWhichIsMarkedFirst()
+        {
+            const string expectedValue = "Hello world!";
+            var oneElementSequence = Enumerable.Repeat(expectedValue, 1);
+
+            foreach (var (value, isFirst) in oneElementSequence.WithFirst())
+            {
+                Assert.Equal(expectedValue, value);
+                Assert.True(isFirst);
+            }
+        }
+
+        [Fact]
+        public void ASequenceWithMultipleElementsWithFirstMarksTheFirstElement()
+        {
+            const int length = 20;
+
+            var sequence = Enumerable.Range(1, length);
+
+            foreach (var (value, isFirst) in sequence.WithFirst())
+            {
+                Assert.Equal(value == 1, isFirst);
+            }
+        }
+    }
+}
---
+++ Funcky/Extensions/EnumerableExtensions/WithFirst.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace Funcky.Extensions
+{
+    public static partial class EnumerableExtensions
+    {
+        [Pure]
+        public static IEnumerable<ValueWithFirst<TSource>> WithFirst<TSource>(this IEnumerable<TSource> source)
+        {
+            using var enumerator = source.GetEnumerator();
+
+            if (!enumerator.MoveNext())
+            {
+                yield break;
+            }
+
+            yield return new ValueWithFirst<TSource>(enumerator.Current, true);
+
+            while (enumerator.MoveNext())
+            {
+                yield return new ValueWithFirst<TSource>(enumerator.Current, false);
+            }
+        }
+
+        public readonly struct ValueWithFirst<TValue>
+        {
+            public ValueWithFirst(TValue value, bool isFirst)
+            {
+                Value = value;
+                IsFirst = isFirst;
+            }
+
+            public TValue Value { get; }
+
+            public bool IsFirst { get; }
+
+            public void Deconstruct(out TValue value, out bool isFirst)
+            {
+                value = Value;
+                isFirst = IsFirst;
+            }
+        }
+    }
+}
