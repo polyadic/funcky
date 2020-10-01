2168591e02af0fca732f6fdba67e40d95671718e
Thomas Bruderer <apophis@apophis.ch>
2020-10-01 15:12:34 +0200

    Improve tests

--- Funcky.Test/Extensions/EnumerableExtensions/CartesianProductTest.cs
+++ Funcky.Test/Extensions/EnumerableExtensions/CartesianProductTest.cs
@@ -28,21 +28,33 @@ namespace Funcky.Test.Extensions.EnumerableExtensions
         [Fact]
         public void GivenTwoSingleElementSequencesWeGetASinglePairBack()
         {
-            var ints = ImmutableList<int>.Empty.Add(1);
-            var strings = ImmutableList<string>.Empty.Add("Hello");
+            var ints = ImmutableList.Create(1);
+            var strings = ImmutableList.Create("Hello");
 
             var product = ints.CartesianProduct(strings);
             Assert.Single(product);
         }
 
         [Fact]
-        public void GivenTowSequencesWithMultipleItemsCartesianProductWithProjectReturnsAllPossibleCombinations()
+        public void GivenTwoSequencesWithMultipleItemsCartesianProductWithProjectReturnsAllPossibleCombinations()
         {
-            var names = ImmutableList<string>.Empty.Add("Ruby").Add("Lucas");
-            var greetings = ImmutableList<string>.Empty.Add("Hello").Add("Good Morning");
+            var names = ImmutableList.Create("Ruby", "Lucas");
+            var greetings = ImmutableList.Create("Hello", "Good Morning");
 
             var allPossibleGreetings = greetings.CartesianProduct(names, (greeting, name) => $"{greeting} {name}");
             Assert.Equal(new[] { "Hello Ruby", "Hello Lucas", "Good Morning Ruby", "Good Morning Lucas" }, allPossibleGreetings);
+        }
+
+        [Fact]
+        public void GivenTwoDifferentlySizedSequencesTheCartesianProductReturnsAllPossibleCombinations()
+        {
+            var suits = ImmutableList.Create("♠", "♣", "♥", "♦");
+            var values = ImmutableList.Create("2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A");
+
+            var cards = suits.CartesianProduct(values, (suit, value) => $"{value}{suit}").ToList();
+            Assert.Equal(52, cards.Count());
+            Assert.Equal("2♠", cards.First());
+            Assert.Equal("A♦", cards.Last());
         }
     }
 }
