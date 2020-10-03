579afc600053e5a9d30f763efbd6d8b35fd27de7
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2020-10-03 12:49:35 +0200

    Document Partition

--- Funcky/Extensions/EnumerableExtensions/Partition.cs
+++ Funcky/Extensions/EnumerableExtensions/Partition.cs
@@ -7,11 +7,30 @@ namespace Funcky.Extensions
 {
     public static partial class EnumerableExtensions
     {
+        /// <summary>
+        /// Partitions the items in an <see cref="IEnumerable{T}"/> by the given <paramref name="predicate"/>.
+        /// </summary>
+        /// <example>
+        /// <code><![CDATA[
+        /// using System.Linq;
+        /// using Funcky.Extensions;
+        ///
+        /// var (evens, odds) = Enumerable.Range(0, 6).Partition(n => n % 2 == 0);
+        /// ]]></code>
+        /// </example>
+        /// <remarks>This method causes the items in <see cref="source"/> to be materialized.</remarks>
+        /// <returns>A tuple with the items for which the predicate holds, and for those for which it doesn't.</returns>
         public static (IEnumerable<TItem> True, IEnumerable<TItem> False) Partition<TItem>(
             this IEnumerable<TItem> source,
             Func<TItem, bool> predicate)
             => source.Partition(predicate, ValueTuple.Create);
 
+        /// <summary>
+        /// Partitions the items in an <see cref="IEnumerable{T}"/> by the given <paramref name="predicate"/>.
+        /// The <paramref name="resultSelector"/> receives the items for which the predicate holds and the items
+        /// for which it doesn't as separate parameters.
+        /// </summary>
+        /// <remarks>This method causes the items in <see cref="source"/> to be materialized.</remarks>
         public static TResult Partition<TItem, TResult>(
             this IEnumerable<TItem> source,
             Func<TItem, bool> predicate,
