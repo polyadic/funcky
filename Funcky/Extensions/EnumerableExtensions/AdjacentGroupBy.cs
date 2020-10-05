a4ac7d4d310644028b69a84bf04d653526efdea3
Thomas Bruderer <apophis@apophis.ch>
2020-10-05 15:29:28 +0200

    Move the implementation to an IGrouping

--- Funcky/Extensions/EnumerableExtensions/AdjacentGroupBy.cs
+++ Funcky/Extensions/EnumerableExtensions/AdjacentGroupBy.cs
@@ -1,7 +1,8 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Diagnostics.Contracts;
+using System.Linq;
 
 namespace Funcky.Extensions
 {
@@ -9,8 +10,11 @@ namespace Funcky.Extensions
     {
         [Pure]
         public static IEnumerable<IEnumerable<TSource>> AdjacentGroupBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+            => AdjacentGroupBy(source, keySelector, EqualityComparer<TKey>.Default);
+
+        [Pure]
+        public static IEnumerable<IGrouping<TKey, TSource>> AdjacentGroupBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer)
         {
-            IEqualityComparer<TKey> comparer = EqualityComparer<TKey>.Default;
             using var enumerator = source.GetEnumerator();
 
             if (!enumerator.MoveNext())
@@ -18,24 +22,82 @@ namespace Funcky.Extensions
                 yield break;
             }
 
-            var group = ImmutableList.Create(enumerator.Current);
-            var currentKey = keySelector(enumerator.Current);
+            var group = CreateGrouping(keySelector(enumerator.Current), enumerator.Current);
 
             while (enumerator.MoveNext())
             {
-                if (comparer.Equals(currentKey, keySelector(enumerator.Current)))
+                if (comparer.Equals(group.Key, keySelector(enumerator.Current)))
                 {
-                    group = group.Add(enumerator.Current);
+                    group.Add(enumerator.Current);
                 }
                 else
                 {
                     yield return group;
-                    group = ImmutableList.Create(enumerator.Current);
-                    currentKey = keySelector(enumerator.Current);
+                    group = CreateGrouping(keySelector(enumerator.Current), enumerator.Current);
                 }
             }
 
             yield return group;
+        }
+
+        internal static Grouping<TKey, TElement> CreateGrouping<TKey, TElement>(TKey key, TElement element)
+        {
+            return new Grouping<TKey, TElement>(key, element);
+        }
+
+        public class Grouping<TKey, TElement> : IGrouping<TKey, TElement>, IList<TElement>
+        {
+            private readonly List<TElement> _elements = new List<TElement>();
+
+            internal Grouping(TKey key, TElement element)
+            {
+                Key = key;
+                _elements.Add(element);
+            }
+
+            public TKey Key { get; }
+
+            public int Count
+                => _elements.Count;
+
+            public bool IsReadOnly
+                => true;
+
+            public TElement this[int index]
+            {
+                get => _elements[index];
+                set => throw new NotImplementedException();
+            }
+
+            public void Add(TElement element)
+                => _elements.Add(element);
+
+            public void Clear()
+                => throw new NotSupportedException();
+
+            public bool Contains(TElement element)
+                => _elements.Contains(element);
+
+            public void CopyTo(TElement[] array, int arrayIndex)
+                => throw new NotImplementedException();
+
+            public IEnumerator<TElement> GetEnumerator()
+                => _elements.GetEnumerator();
+
+            public int IndexOf(TElement element)
+                => _elements.IndexOf(element);
+
+            public void Insert(int index, TElement element)
+                => throw new NotSupportedException();
+
+            public bool Remove(TElement element)
+                => throw new NotSupportedException();
+
+            public void RemoveAt(int index)
+                => throw new NotSupportedException();
+
+            IEnumerator IEnumerable.GetEnumerator()
+                => GetEnumerator();
         }
     }
 }
