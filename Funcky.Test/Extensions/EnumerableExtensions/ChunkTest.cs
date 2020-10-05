355a89eda2fc93ed8dd909142ad19ae1976b154e
Mathias Fischler <mathias.fischler@messerli.ch>
2020-10-05 14:52:59 +0200

    Revert accidentially changed code

--- Funcky.Test/Extensions/EnumerableExtensions/ChunkTest.cs
+++ Funcky.Test/Extensions/EnumerableExtensions/ChunkTest.cs
@@ -111,7 +111,7 @@ namespace Funcky.Test.Extensions.EnumerableExtensions
             var magicSquare = new List<int> { 4, 9, 2, 3, 5, 7, 8, 1, 6 };
 
             magicSquare
-                .Chunk(3, number => Enumerable.Average(number))
+                .Chunk(3, Enumerable.Average)
                 .ForEach(average => Assert.Equal(5, average));
         }
     }
