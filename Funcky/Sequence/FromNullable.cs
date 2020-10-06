646cebcdcf0b7f874b04d4cde924ae6a15eb60da
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2020-10-06 09:49:44 +0200

    Split Sequence class into multiple files

---
+++ Funcky/Sequence/FromNullable.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using Funcky.GenericConstraints;
+
+namespace Funcky
+{
+    public static partial class Sequence
+    {
+        /// <returns>An <see cref="IEnumerable{T}" /> consisting of a single item or zero items.</returns>
+        [Pure]
+        public static IEnumerable<T> FromNullable<T>(T? item, RequireClass<T>? ω = null)
+            where T : class
+            => item is { } ? Return(item) : Enumerable.Empty<T>();
+
+        /// <inheritdoc cref="FromNullable{T}(T, Funcky.GenericConstraints.RequireClass{T})"/>
+        [Pure]
+        public static IEnumerable<T> FromNullable<T>(T? item, RequireStruct<T>? ω = null)
+            where T : struct
+            => item.HasValue ? Return(item.Value) : Enumerable.Empty<T>();
+    }
+}
--- Funcky/Sequence/Generate.cs
+++ Funcky/Sequence/Generate.cs
@@ -1,16 +1,12 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.Contracts;
 using System.Linq;
-using Funcky.GenericConstraints;
 using Funcky.Monads;
 
 namespace Funcky
 {
-    public static class Sequence
+    public static partial class Sequence
     {
-        public static IEnumerable<TItem> Return<TItem>(TItem item) => Enumerable.Repeat(item, 1);
-
         /// <summary>
         /// Returns an endless <see cref="IEnumerable{T}"/> that yields values based on the previous value
         /// using the <paramref name="next" /> function.
@@ -41,17 +37,5 @@ namespace Funcky
                 yield return previousItem = item;
             }
         }
-
-        /// <returns>An <see cref="IEnumerable{T}" /> consisting of a single item or zero items.</returns>
-        [Pure]
-        public static IEnumerable<T> FromNullable<T>(T? item, RequireClass<T>? ω = null)
-            where T : class
-            => item is { } ? Return(item) : Enumerable.Empty<T>();
-
-        /// <inheritdoc cref="FromNullable{T}(T, Funcky.GenericConstraints.RequireClass{T})"/>
-        [Pure]
-        public static IEnumerable<T> FromNullable<T>(T? item, RequireStruct<T>? ω = null)
-            where T : struct
-            => item.HasValue ? Return(item.Value) : Enumerable.Empty<T>();
     }
 }
