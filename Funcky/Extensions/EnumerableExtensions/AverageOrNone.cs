ef3bbd15368a4aa1a502da4d7dc24e2fca67e437
Thomas Bruderer <apophis@apophis.ch>
2020-10-08 10:58:25 +0200

    Improve Average, Make calculator immutable and use Aggregate

--- Funcky/Extensions/EnumerableExtensions/AverageOrNone.cs
+++ Funcky/Extensions/EnumerableExtensions/AverageOrNone.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
+using System.Linq;
 using Funcky.Monads;
 using static Funcky.Functional;
 
@@ -10,17 +11,7 @@ namespace Funcky.Extensions
     {
         [Pure]
         public static Option<double> AverageOrNone(this IEnumerable<int> source)
-        {
-            using IEnumerator<int> enumerator = source.GetEnumerator();
-            var calculator = new AverageCalculatorDouble();
-
-            while (enumerator.MoveNext())
-            {
-                calculator.Add(enumerator.Current);
-            }
-
-            return calculator.Average;
-        }
+            => source.Aggregate(AverageCalculatorDouble.Empty, (calculator, element) => calculator.Add(element)).Average;
 
         [Pure]
         public static Option<double> AverageOrNone(this IEnumerable<Option<int>> source)
@@ -28,17 +19,7 @@ namespace Funcky.Extensions
 
         [Pure]
         public static Option<double> AverageOrNone(this IEnumerable<long> source)
-        {
-            using IEnumerator<long> enumerator = source.GetEnumerator();
-            var calculator = new AverageCalculatorDouble();
-
-            while (enumerator.MoveNext())
-            {
-                calculator.Add(enumerator.Current);
-            }
-
-            return calculator.Average;
-        }
+            => source.Aggregate(AverageCalculatorDouble.Empty, (calculator, element) => calculator.Add(element)).Average;
 
         [Pure]
         public static Option<double> AverageOrNone(this IEnumerable<Option<long>> source)
@@ -46,17 +27,7 @@ namespace Funcky.Extensions
 
         [Pure]
         public static Option<float> AverageOrNone(this IEnumerable<float> source)
-        {
-            using IEnumerator<float> enumerator = source.GetEnumerator();
-            var calculator = new AverageCalculatorFloat();
-
-            while (enumerator.MoveNext())
-            {
-                calculator.Add(enumerator.Current);
-            }
-
-            return calculator.Average;
-        }
+            => source.Aggregate(AverageCalculatorFloat.Empty, (calculator, element) => calculator.Add(element)).Average;
 
         [Pure]
         public static Option<float> AverageOrNone(this IEnumerable<Option<float>> source)
@@ -64,17 +35,7 @@ namespace Funcky.Extensions
 
         [Pure]
         public static Option<double> AverageOrNone(this IEnumerable<double> source)
-        {
-            using IEnumerator<double> enumerator = source.GetEnumerator();
-            var calculator = new AverageCalculatorDouble();
-
-            while (enumerator.MoveNext())
-            {
-                calculator.Add(enumerator.Current);
-            }
-
-            return calculator.Average;
-        }
+            => source.Aggregate(AverageCalculatorDouble.Empty, (calculator, element) => calculator.Add(element)).Average;
 
         [Pure]
         public static Option<double> AverageOrNone(this IEnumerable<Option<double>> source)
@@ -82,17 +43,7 @@ namespace Funcky.Extensions
 
         [Pure]
         public static Option<decimal> AverageOrNone(this IEnumerable<decimal> source)
-        {
-            using IEnumerator<decimal> enumerator = source.GetEnumerator();
-            var calculator = new AverageCalculatorDecimal();
-
-            while (enumerator.MoveNext())
-            {
-                calculator.Add(enumerator.Current);
-            }
-
-            return calculator.Average;
-        }
+            => source.Aggregate(AverageCalculatorDecimal.Empty, (calculator, element) => calculator.Add(element)).Average;
 
         [Pure]
         public static Option<decimal> AverageOrNone(this IEnumerable<Option<decimal>> source)
@@ -100,196 +51,105 @@ namespace Funcky.Extensions
 
         [Pure]
         public static Option<double> AverageOrNone<TSource>(this IEnumerable<TSource> source, Func<TSource, int> selector)
-        {
-            using IEnumerator<TSource> enumerator = source.GetEnumerator();
-            var calculator = new AverageCalculatorDouble();
-
-            while (enumerator.MoveNext())
-            {
-                calculator.Add(selector(enumerator.Current));
-            }
-
-            return calculator.Average;
-        }
+            => source.Aggregate(AverageCalculatorDouble.Empty, (calculator, element) => calculator.Add(selector(element))).Average;
 
         [Pure]
         public static Option<double> AverageOrNone<TSource>(this IEnumerable<TSource> source, Func<TSource, Option<int>> selector)
-        {
-            using IEnumerator<TSource> enumerator = source.GetEnumerator();
-            var calculator = new AverageCalculatorDouble();
-
-            while (enumerator.MoveNext())
-            {
-                selector(enumerator.Current).AndThen(c => calculator.Add(c));
-            }
-
-            return calculator.Average;
-        }
+            => source.WhereSelect(selector).AverageOrNone();
 
         [Pure]
         public static Option<double> AverageOrNone<TSource>(this IEnumerable<TSource> source, Func<TSource, long> selector)
-        {
-            using IEnumerator<TSource> enumerator = source.GetEnumerator();
-            var calculator = new AverageCalculatorDouble();
-
-            while (enumerator.MoveNext())
-            {
-                calculator.Add(selector(enumerator.Current));
-            }
-
-            return calculator.Average;
-        }
+            => source.Aggregate(AverageCalculatorDouble.Empty, (calculator, element) => calculator.Add(selector(element))).Average;
 
         [Pure]
         public static Option<double> AverageOrNone<TSource>(this IEnumerable<TSource> source, Func<TSource, Option<long>> selector)
-        {
-            using IEnumerator<TSource> enumerator = source.GetEnumerator();
-            var calculator = new AverageCalculatorDouble();
-
-            while (enumerator.MoveNext())
-            {
-                selector(enumerator.Current).AndThen(c => calculator.Add(c));
-            }
-
-            return calculator.Average;
-        }
+            => source.WhereSelect(selector).AverageOrNone();
 
         [Pure]
         public static Option<float> AverageOrNone<TSource>(this IEnumerable<TSource> source, Func<TSource, float> selector)
-        {
-            using IEnumerator<TSource> enumerator = source.GetEnumerator();
-            var calculator = new AverageCalculatorFloat();
-
-            while (enumerator.MoveNext())
-            {
-                calculator.Add(selector(enumerator.Current));
-            }
-
-            return calculator.Average;
-        }
+            => source.Aggregate(AverageCalculatorFloat.Empty, (calculator, element) => calculator.Add(selector(element))).Average;
 
         [Pure]
         public static Option<float> AverageOrNone<TSource>(this IEnumerable<TSource> source, Func<TSource, Option<float>> selector)
-        {
-            using IEnumerator<TSource> enumerator = source.GetEnumerator();
-            var calculator = new AverageCalculatorFloat();
-
-            while (enumerator.MoveNext())
-            {
-                selector(enumerator.Current).AndThen(c => calculator.Add(c));
-            }
-
-            return calculator.Average;
-        }
+            => source.WhereSelect(selector).AverageOrNone();
 
         [Pure]
         public static Option<double> AverageOrNone<TSource>(this IEnumerable<TSource> source, Func<TSource, double> selector)
-        {
-            using IEnumerator<TSource> enumerator = source.GetEnumerator();
-            var calculator = new AverageCalculatorDouble();
-
-            while (enumerator.MoveNext())
-            {
-                calculator.Add(selector(enumerator.Current));
-            }
-
-            return calculator.Average;
-        }
+            => source.Aggregate(AverageCalculatorDouble.Empty, (calculator, element) => calculator.Add(selector(element))).Average;
 
         [Pure]
         public static Option<double> AverageOrNone<TSource>(this IEnumerable<TSource> source, Func<TSource, Option<double>> selector)
-        {
-            using IEnumerator<TSource> enumerator = source.GetEnumerator();
-            var calculator = new AverageCalculatorDouble();
-
-            while (enumerator.MoveNext())
-            {
-                selector(enumerator.Current).AndThen(c => calculator.Add(c));
-            }
-
-            return calculator.Average;
-        }
+            => source.WhereSelect(selector).AverageOrNone();
 
         [Pure]
         public static Option<decimal> AverageOrNone<TSource>(this IEnumerable<TSource> source, Func<TSource, decimal> selector)
-        {
-            using IEnumerator<TSource> enumerator = source.GetEnumerator();
-            var calculator = new AverageCalculatorDecimal();
-
-            while (enumerator.MoveNext())
-            {
-                calculator.Add(selector(enumerator.Current));
-            }
-
-            return calculator.Average;
-        }
+            => source.Aggregate(AverageCalculatorDecimal.Empty, (calculator, element) => calculator.Add(selector(element))).Average;
 
         [Pure]
         public static Option<decimal> AverageOrNone<TSource>(this IEnumerable<TSource> source, Func<TSource, Option<decimal>> selector)
-        {
-            using IEnumerator<TSource> enumerator = source.GetEnumerator();
-            var calculator = new AverageCalculatorDecimal();
-
-            while (enumerator.MoveNext())
-            {
-                selector(enumerator.Current).AndThen(c => calculator.Add(c));
-            }
-
-            return calculator.Average;
-        }
+            => source.WhereSelect(selector).AverageOrNone();
 
         private class AverageCalculatorDouble
         {
-            private Option<double> _sum;
-            private int _count;
+            public static readonly AverageCalculatorDouble Empty = new AverageCalculatorDouble();
+
+            private readonly int _count;
+            private readonly Option<double> _sum;
+
+            public AverageCalculatorDouble(int count = default, Option<double> sum = default)
+            {
+                _count = count;
+                _sum = sum;
+            }
 
             public Option<double> Average => _sum.Select(s => s / _count);
 
-            public void Add(int term)
-            {
-                _sum = Option.Some(_sum.Match(term, s => s + term));
-                ++_count;
-            }
+            public AverageCalculatorDouble Add(int term)
+                => new AverageCalculatorDouble(_count + 1, from sum in _sum select sum + term);
 
-            public void Add(long term)
-            {
-                _sum = Option.Some(_sum.Match(term, s => s + term));
-                ++_count;
-            }
+            public AverageCalculatorDouble Add(long term)
+                => new AverageCalculatorDouble(_count + 1, from sum in _sum select sum + term);
 
-            public void Add(double term)
-            {
-                _sum = Option.Some(_sum.Match(term, s => s + term));
-                ++_count;
-            }
+            public AverageCalculatorDouble Add(double term)
+                => new AverageCalculatorDouble(_count + 1, from sum in _sum select sum + term);
         }
 
         private class AverageCalculatorFloat
         {
-            private Option<float> _sum;
-            private int _count;
+            public static readonly AverageCalculatorFloat Empty = new AverageCalculatorFloat();
+
+            private readonly Option<float> _sum;
+            private readonly int _count;
+
+            public AverageCalculatorFloat(int count = default, Option<float> sum = default)
+            {
+                _count = count;
+                _sum = sum;
+            }
 
             public Option<float> Average => _sum.Select(s => s / _count);
 
-            public void Add(float term)
-            {
-                _sum = Option.Some(_sum.Match(term, s => s + term));
-                ++_count;
-            }
+            public AverageCalculatorFloat Add(float term)
+                => new AverageCalculatorFloat(_count + 1, from sum in _sum select sum + term);
         }
 
         private class AverageCalculatorDecimal
         {
-            private Option<decimal> _sum;
-            private int _count;
+            public static readonly AverageCalculatorDecimal Empty = new AverageCalculatorDecimal();
+
+            private readonly Option<decimal> _sum;
+            private readonly int _count;
+
+            public AverageCalculatorDecimal(int count = default, Option<decimal> sum = default)
+            {
+                _count = count;
+                _sum = sum;
+            }
 
             public Option<decimal> Average => _sum.Select(s => s / _count);
 
-            public void Add(decimal term)
-            {
-                _sum = Option.Some(_sum.Match(term, s => s + term));
-                ++_count;
-            }
+            public AverageCalculatorDecimal Add(decimal term)
+                => new AverageCalculatorDecimal(_count + 1, from sum in _sum select sum + term);
         }
     }
 }
