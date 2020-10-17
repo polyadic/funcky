454f18562fed5836952001820f557a14725684f9
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2020-10-17 11:58:10 +0200

    Reduce duplicated MaxOrNone methods

--- Funcky/Extensions/EnumerableExtensions/MaxOrNone.cs
+++ Funcky/Extensions/EnumerableExtensions/MaxOrNone.cs
@@ -216,67 +216,21 @@ namespace Funcky.Extensions
         /// </summary>
         /// <typeparam name="TSource">The type of the elements of source.</typeparam>
         /// <param name="source">A sequence of generic values of TSource to determine the maximum value of.</param>
-        /// <param name="ω">Dummy Parameter, do not use.</param>
         /// <returns>The maximum value in the sequence or None.</returns>
         [Pure]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("ApiDesign", "RS0027:Public API with optional parameter(s) should have the most parameters amongst its public overloads.", Justification = "The RequireClass type is only for type deduction of the right overload.")]
-        public static Option<TSource> MaxOrNone<TSource>(this IEnumerable<TSource> source, RequireClass<TSource>? ω = null)
-            where TSource : class
-            => source.MaxOrNone(Identity);
-
-        /// <summary>
-        /// Returns the maximum value in a sequence of generic values compared by a <see cref="Comparer{T}"/>. If the sequence is empty it returns None.
-        /// </summary>
-        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
-        /// <param name="source">A sequence of generic values of TSource to determine the maximum value of.</param>
-        /// <param name="ω">Dummy Parameter, do not use.</param>
-        /// <returns>The maximum value in the sequence or None.</returns>
-        [Pure]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("ApiDesign", "RS0027:Public API with optional parameter(s) should have the most parameters amongst its public overloads.", Justification = "The RequireStruct type is only for type deduction of the right overload.")]
-        public static Option<TSource> MaxOrNone<TSource>(this IEnumerable<TSource> source, RequireStruct<TSource>? ω = null)
-            where TSource : struct
+        public static Option<TSource> MaxOrNone<TSource>(this IEnumerable<TSource> source)
+            where TSource : notnull
             => source.MaxOrNone(Identity);
 
         /// <summary>
         /// Returns the maximum value in a sequence of optional generic values compared by a <see cref="Comparer{T}"/>. If the sequence only consists of none or is empty it returns None.
         /// </summary>
-        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
         /// <param name="source">A sequence of optional generic values of TSource to determine the maximum value of.</param>
-        /// <param name="ω">Dummy Parameter, do not use.</param>
         /// <returns>The maximum value in the sequence or None.</returns>
         [Pure]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("ApiDesign", "RS0027:Public API with optional parameter(s) should have the most parameters amongst its public overloads.", Justification = "The RequireClass type is only for type deduction of the right overload.")]
-        public static Option<TSource> MaxOrNone<TSource>(this IEnumerable<Option<TSource>> source, RequireClass<TSource>? ω = null)
-            where TSource : class
+        public static Option<TSource> MaxOrNone<TSource>(this IEnumerable<Option<TSource>> source)
+            where TSource : notnull
             => source.WhereSelect(Identity).MaxOrNone(Identity);
-
-        /// <summary>
-        /// Returns the maximum value in a sequence of optional generic values compared by a <see cref="Comparer{T}"/>. If the sequence only consists of none or is empty it returns None.
-        /// </summary>
-        /// <param name="source">A sequence of optional generic values of TSource to determine the maximum value of.</param>
-        /// <param name="ω">Dummy Parameter, do not use.</param>
-        /// <returns>The maximum value in the sequence or None.</returns>
-        [Pure]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("ApiDesign", "RS0027:Public API with optional parameter(s) should have the most parameters amongst its public overloads.", Justification = "The RequireStruct type is only for type deduction of the right overload.")]
-        public static Option<TSource> MaxOrNone<TSource>(this IEnumerable<Option<TSource>> source, RequireStruct<TSource>? ω = null)
-            where TSource : struct
-            => source.WhereSelect(Identity).MaxOrNone(Identity);
-
-        /// <summary>
-        ///  Invokes a transform function on each element of a sequence and returns the maximum from the generic values compared by a <see cref="Comparer{T}"/>. If the transformed sequence only consists of none or is empty it returns None.
-        /// </summary>
-        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
-        /// <param name="source">A sequence of generic values of TSource to determine the maximum value of.</param>
-        /// <typeparam name="TResult">The type of the value returned by selector.</typeparam>
-        /// <param name="selector">A transform function to apply to each element.</param>
-        /// <param name="ω">Dummy Parameter, do not use.</param>
-        /// <returns>The maximum value in the sequence or None.</returns>
-        [Pure]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("ApiDesign", "RS0027:Public API with optional parameter(s) should have the most parameters amongst its public overloads.", Justification = "The RequireClass type is only for type deduction of the right overload.")]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("ApiDesign", "RS0026:Do not add multiple public overloads with optional parameters", Justification = "The RequireClass type is needed on each overload.")]
-        public static Option<TResult> MaxOrNone<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector, RequireClass<TResult>? ω = null)
-            where TResult : class
-            => source.Aggregate(Option<TResult>.None(), (max, current) => Option.Some(max.Match(selector(current), m => Comparer<TResult>.Default.Compare(m, selector(current)) > 0 ? selector(current) : m)));
 
         /// <summary>
         /// Invokes a transform function on each element of a sequence and returns the maximum from the generic values compared by a <see cref="Comparer{T}"/>. If the transformed sequence only consists of none or is empty it returns None.
@@ -285,13 +239,10 @@ namespace Funcky.Extensions
         /// <param name="source">A sequence of generic values of TSource to determine the maximum value of.</param>
         /// <typeparam name="TResult">The type of the value returned by selector.</typeparam>
         /// <param name="selector">A transform function to apply to each element.</param>
-        /// <param name="ω">Dummy Parameter, do not use.</param>
         /// <returns>The maximum value in the sequence or None.</returns>
         [Pure]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("ApiDesign", "RS0027:Public API with optional parameter(s) should have the most parameters amongst its public overloads.", Justification = "The RequireStruct type is only for type deduction of the right overload.")]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("ApiDesign", "RS0026:Do not add multiple public overloads with optional parameters", Justification = "The RequireStruct type is needed on each overload.")]
-        public static Option<TResult> MaxOrNone<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector, RequireStruct<TResult>? ω = null)
-            where TResult : struct
+        public static Option<TResult> MaxOrNone<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+            where TResult : notnull
             => source.Aggregate(Option<TResult>.None(), (max, current) => Option.Some(max.Match(selector(current), m => Comparer<TResult>.Default.Compare(m, selector(current)) > 0 ? selector(current) : m)));
 
         /// <summary>
@@ -301,29 +252,10 @@ namespace Funcky.Extensions
         /// <param name="source">A sequence of generic values of TSource to determine the maximum value of.</param>
         /// <typeparam name="TResult">The type of the value returned by selector.</typeparam>
         /// <param name="selector">A transform function to apply to each element.</param>
-        /// <param name="ω">Dummy Parameter, do not use.</param>
         /// <returns>The maximum value in the sequence or None.</returns>
         [Pure]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("ApiDesign", "RS0027:Public API with optional parameter(s) should have the most parameters amongst its public overloads.", Justification = "The RequireClass type is only for type deduction of the right overload.")]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("ApiDesign", "RS0026:Do not add multiple public overloads with optional parameters", Justification = "The RequireClass type is needed on each overload.")]
-        public static Option<TResult> MaxOrNone<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, Option<TResult>> selector, RequireClass<TResult>? ω = null)
-            where TResult : class
-            => source.WhereSelect(selector).MaxOrNone(Identity);
-
-        /// <summary>
-        /// Invokes a transform function on each element of a sequence and returns the maximum from the optional generic values compared by a <see cref="Comparer{T}"/>. If the transformed sequence only consists of none or is empty it returns None.
-        /// </summary>
-        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
-        /// <param name="source">A sequence of generic values of TSource to determine the maximum value of.</param>
-        /// <typeparam name="TResult">The type of the value returned by selector.</typeparam>
-        /// <param name="selector">A transform function to apply to each element.</param>
-        /// <param name="ω">Dummy Parameter, do not use.</param>
-        /// <returns>The maximum value in the sequence or None.</returns>
-        [Pure]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("ApiDesign", "RS0027:Public API with optional parameter(s) should have the most parameters amongst its public overloads.", Justification = "The RequireStruct type is only for type deduction of the right overload.")]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("ApiDesign", "RS0026:Do not add multiple public overloads with optional parameters", Justification = "The RequireStruct type is needed on each overload.")]
-        public static Option<TResult> MaxOrNone<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, Option<TResult>> selector, RequireStruct<TResult>? ω = null)
-            where TResult : struct
+        public static Option<TResult> MaxOrNone<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, Option<TResult>> selector)
+            where TResult : notnull
             => source.WhereSelect(selector).MaxOrNone(Identity);
 
         private static float DefaultComparerMax(float left, float right)
