59656b00c5289ccd651535b8cdad31c7d17713a1
Thomas <apophis@apophis.ch>
2020-10-21 09:00:08 +0200

    Update Funcky/Extensions/EnumerableExtensions/MaxOrNone.cs
    Co-authored-by: Ruben Schmidmeister <ruben.schmidmeister@icloud.com>

--- Funcky/Extensions/EnumerableExtensions/MaxOrNone.cs
+++ Funcky/Extensions/EnumerableExtensions/MaxOrNone.cs
@@ -153,7 +153,7 @@ namespace Funcky.Extensions
         /// Invokes a transform function on each element of a sequence and returns the maximum optional <see cref="float"/> value. If the sequence is empty it returns None.
         /// </summary>
         /// <typeparam name="TSource">The type of the elements of source.</typeparam>
-        /// <param name="source">A sequence of values to calculate the average of.</param>
+        /// <param name="source">A sequence of values to calculate the maximum value of.</param>
         /// <param name="selector">A transform function to apply to each element.</param>
         /// <returns>The maximum value in the sequence or None.</returns>
         [Pure]
