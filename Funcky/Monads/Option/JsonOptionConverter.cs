fa4d0470f4bfd8ec5c3108da1f6694c854686a7c
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2020-12-09 20:05:35 +0100

    Fix caching of item converter for older versions of the BCL

--- Funcky/Monads/Option/JsonOptionConverter.cs
+++ Funcky/Monads/Option/JsonOptionConverter.cs
@@ -7,19 +7,33 @@ namespace Funcky.Monads
     internal sealed class JsonOptionConverter<TItem> : JsonConverter<Option<TItem>>
         where TItem : notnull
     {
-        private readonly JsonConverter<TItem> _itemConverter;
+        private readonly JsonConverter<TItem>? _itemConverter;
 
         public JsonOptionConverter(JsonSerializerOptions options)
-            => _itemConverter = (JsonConverter<TItem>)options.GetConverter(typeof(TItem));
+        {
+            _itemConverter = (JsonConverter<TItem>)options.GetConverter(typeof(TItem));
+        }
 
         public override Option<TItem> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
             => reader.TokenType == JsonTokenType.Null
                 ? Option<TItem>.None()
-                : Option.Some(JsonSerializer.Deserialize<TItem>(ref reader, options)!);
+                : _itemConverter is not null
+                    ? _itemConverter.Read(ref reader, typeof(TItem), options)!
+                    : JsonSerializer.Deserialize<TItem>(ref reader, options);
 
         public override void Write(Utf8JsonWriter writer, Option<TItem> value, JsonSerializerOptions options)
             => value.Match(
                 none: writer.WriteNullValue,
-                some: item => _itemConverter.Write(writer, item, options));
+                some: item =>
+                {
+                    if (_itemConverter is not null)
+                    {
+                        _itemConverter.Write(writer, item, options);
+                    }
+                    else
+                    {
+                        JsonSerializer.Serialize(writer, item, options);
+                    }
+                });
     }
 }
