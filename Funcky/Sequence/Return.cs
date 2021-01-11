3855728b65b66a4fa3da577bf37ec0af643d83f0
Thomas Bruderer <apophis@apophis.ch>
2021-01-11 09:58:33 +0100

    Remove notnull constraint on Return * maybe a breaking change

--- Funcky/Sequence/Return.cs
+++ Funcky/Sequence/Return.cs
@@ -6,7 +6,6 @@ namespace Funcky
     public static partial class Sequence
     {
         public static IEnumerable<TItem> Return<TItem>(TItem item)
-            where TItem : notnull
             => Enumerable.Repeat(item, 1);
     }
 }
