f7135559354e5737f0463b83b18ff439f475e777
Thomas Bruderer <apophis@apophis.ch>
2021-01-11 11:56:12 +0100

    Clarify that the sequence cannot be empty for CycleRange

--- Funcky/Sequence/CycleRange.cs
+++ Funcky/Sequence/CycleRange.cs
@@ -12,7 +12,7 @@ namespace Funcky
         /// Generates a sequence that contains the same sequence of elements over and over again as an endless generator.
         /// </summary>
         /// <typeparam name="TItem">Type of the elements to be cycled.</typeparam>
-        /// <param name="sequence">The sequence of elements which are cycled.</param>
+        /// <param name="sequence">The sequence of elements which are cycled. Throws an exception if the sequence is empty.</param>
         /// <returns>Returns an infinite IEnumerable repeating the same sequence of elements.</returns>
         public static IEnumerable<TItem> CycleRange<TItem>(IEnumerable<TItem> sequence)
             where TItem : notnull
