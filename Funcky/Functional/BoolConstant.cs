97e4e072e0899659bc0109f3d89a710e7854b2ef
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2021-01-12 17:41:58 +0100

    Add XML documentation for True and False

--- Funcky/Functional/BoolConstant.cs
+++ Funcky/Functional/BoolConstant.cs
@@ -4,15 +4,19 @@ namespace Funcky
 {
     public static partial class Functional
     {
+        /// <summary>A function that always returns <see langword="true"/>.</summary>
         [Pure]
         public static bool True<T>(T ω) => true;
 
+        /// <inheritdoc cref="True{T}"/>
         [Pure]
         public static bool True() => true;
 
+        /// <summary>A function that always returns <see langword="false"/>.</summary>
         [Pure]
         public static bool False<T>(T ω) => false;
 
+        /// <inheritdoc cref="False{T}"/>
         [Pure]
         public static bool False() => false;
     }
