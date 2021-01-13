4718cf30dce6d5a47bf46e305baa318d928749b6
Thomas Bruderer <apophis@apophis.ch>
2021-01-13 10:23:58 +0100

    Document the deferred execution

--- Funcky/Extensions/EnumerableExtensions/Shuffle.cs
+++ Funcky/Extensions/EnumerableExtensions/Shuffle.cs
@@ -11,6 +11,7 @@ namespace Funcky.Extensions
         /// Returns the given sequence in random Order in O(n).
         /// </summary>
         /// <typeparam name="TSource">The type of the elements in the enumerable.</typeparam>
+        /// <remarks>This method is implemented by using deferred execution. The immediate return value is an object that stores all the information that is required to perform the action. The query represented by this method is not executed until the object is enumerated either by calling its GetEnumerator method directly or by using foreach.</remarks>
         [Pure]
         public static IEnumerable<TSource> Shuffle<TSource>(this IEnumerable<TSource> source)
             where TSource : notnull
