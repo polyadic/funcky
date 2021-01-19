932cf76fc9eb731dbede7d5e1095cdef29fd3831
Thomas Bruderer <apophis@apophis.ch>
2021-01-19 13:55:05 +0100

    Add Generator to Funcky

---
+++ Funcky.SourceGenerator/TryPattern.cs
@@ -0,0 +1,15 @@
+namespace Funcky.Money.SourceGenerator
+{
+    internal class TryPattern
+    {
+        public TryPattern()
+        {
+            FileName = "FileName";
+            Source = string.Empty;
+        }
+
+        public string FileName { get; internal set; }
+
+        public string Source { get; internal set; }
+    }
+}
