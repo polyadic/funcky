02ed1391e564615110a3a980959c9fe4a8bc1d1a
Thomas Bruderer <apophis@apophis.ch>
2021-02-10 14:20:20 +0100

    Add an extension function to allow function composition

---
+++ Funcky/Extensions/FunctionCompositionExtensions.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Diagnostics.Contracts;
+
+namespace Funcky.Extensions
+{
+    public static class FunctionCompositionExtensions
+    {
+        /// <summary>
+        /// Function composition.
+        /// </summary>
+        /// <returns>f ∘ g, v => f(g(v)).</returns>
+        [Pure]
+        public static Func<TInput, TOutput> Compose<TInput, TIntermediate, TOutput>(this Func<TIntermediate, TOutput> f, Func<TInput, TIntermediate> g)
+            => v => f(g(v));
+
+        /// <summary>
+        /// Function composition.
+        /// </summary>
+        /// <returns>f ∘ g,() => f(g()).</returns>
+        [Pure]
+        public static Func<TOutput> Compose<TIntermediate, TOutput>(this Func<TIntermediate, TOutput> f, Func<TIntermediate> g)
+            => () => f(g());
+
+        /// <summary>
+        /// Function composition with action.
+        /// </summary>
+        /// <returns>f ∘ g,v => f(g(v)).</returns>
+        [Pure]
+        public static Action<TInput> Compose<TInput, TIntermediate>(this Action<TIntermediate> f, Func<TInput, TIntermediate> g)
+            => v => f(g(v));
+
+        /// <summary>
+        /// Function composition with Action.
+        /// </summary>
+        /// <returns>f ∘ g, () => f(g()).</returns>
+        [Pure]
+        public static Action Compose<TIntermediate>(this Action<TIntermediate> f, Func<TIntermediate> g)
+            => () => f(g());
+    }
+}
