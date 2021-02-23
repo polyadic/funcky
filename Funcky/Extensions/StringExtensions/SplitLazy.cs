1bd969fc9bb80e09404b4c365e0f2cce16018083
Thomas Bruderer <apophis@apophis.ch>
2021-02-23 10:42:25 +0100

    Extract the code from SplitLines into its own file

--- Funcky/Extensions/StringExtensions/SplitLazy.cs
+++ Funcky/Extensions/StringExtensions/SplitLazy.cs
@@ -14,15 +14,6 @@ namespace Funcky.Extensions
         private delegate int GetLength(int index, bool hasCarriageReturn);
 
         /// <summary>
-        /// Splits a string into individual lines lazily, by any new line (CR, LF, CRLF).
-        /// </summary>
-        /// <param name="text">The input text.</param>
-        /// <returns>A lazy IEnumerable containing the lines.</returns>
-        [Pure]
-        public static IEnumerable<string> SplitLines(this string text)
-            => text.SplitBy(ExtractBy(GetNextLine));
-
-        /// <summary>
         /// Splits a string into individual parts by a given separator.
         /// </summary>
         /// <param name="text">The input text.</param>
@@ -62,7 +53,6 @@ namespace Funcky.Extensions
         public static IEnumerable<string> SplitLazy(this string text, params string[] separators)
             => text.SplitBy(ExtractByIndex(IndexOfStringSeparators(separators)));
 
-        [Pure]
         private static IEnumerable<string> SplitBy(this string text, ExtractElement extractNext)
         {
             if (text == string.Empty)
@@ -103,46 +93,12 @@ namespace Funcky.Extensions
                     ? extractElement(text, startIndex)
                     : Option<SplitResult>.None();
 
-        private static Option<SplitResult> GetNextLine(string text, int startIndex)
-            => GetNextLine(GetLengthFrom(startIndex))(text, startIndex);
-
-        private static ExtractElement GetNextLine(GetLength getLength)
-            => (text, startIndex)
-                =>
-                {
-                    var seenCarriageReturn = false;
-                    for (var index = startIndex; ; ++index)
-                    {
-                        if (IsEndOfLine(text, index) || seenCarriageReturn)
-                        {
-                            return new SplitResult(text.Substring(startIndex, getLength(index, seenCarriageReturn)), NextStartIndex(index, IsEndOfLine(text, index)));
-                        }
-
-                        seenCarriageReturn = text[index] is '\r';
-                    }
-                };
-
         private static ExtractElement GetIndex(FindNextIndex getIndex)
             => (text, startIndex)
                 => getIndex(text, startIndex)
                     .Match(
                         none: () => new SplitResult(text.Substring(startIndex), text.Length + 1),
                         some: index => new SplitResult(text.Substring(startIndex, index - startIndex), index + 1));
-
-        private static bool IsEndOfLine(string text, int index)
-            => index == text.Length
-               || text[index] is '\n';
-
-        private static int NextStartIndex(int index, bool indexIsAtTheEndOfALine)
-            => indexIsAtTheEndOfALine
-                ? index + 1
-                : index;
-
-        private static GetLength GetLengthFrom(int startIndex)
-            => (index, hasCarriageReturn)
-                => hasCarriageReturn
-                    ? index - startIndex - 1
-                    : index - startIndex;
 
         private sealed class SplitResult
         {
---
+++ Funcky/Extensions/StringExtensions/SplitLines.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using Funcky.Monads;
+
+namespace Funcky.Extensions
+{
+    public static partial class StringExtensions
+    {
+        /// <summary>
+        /// Splits a string into individual lines lazily, by any new line (CR, LF, CRLF).
+        /// </summary>
+        /// <param name="text">The input text.</param>
+        /// <returns>A lazy IEnumerable containing the lines.</returns>
+        [Pure]
+        public static IEnumerable<string> SplitLines(this string text)
+            => text.SplitBy(ExtractBy(GetNextLine));
+
+        private static Option<SplitResult> GetNextLine(string text, int startIndex)
+            => GetNextLine(GetLengthFrom(startIndex))(text, startIndex);
+
+        private static ExtractElement GetNextLine(GetLength getLength)
+            => (text, startIndex)
+                =>
+            {
+                var seenCarriageReturn = false;
+                for (var index = startIndex; ; ++index)
+                {
+                    if (IsEndOfLine(text, index) || seenCarriageReturn)
+                    {
+                        return new SplitResult(text.Substring(startIndex, getLength(index, seenCarriageReturn)), NextStartIndex(index, IsEndOfLine(text, index)));
+                    }
+
+                    seenCarriageReturn = text[index] is '\r';
+                }
+            };
+
+        private static GetLength GetLengthFrom(int startIndex)
+            => (index, hasCarriageReturn)
+                => hasCarriageReturn
+                    ? index - startIndex - 1
+                    : index - startIndex;
+
+        private static bool IsEndOfLine(string text, int index)
+            => index == text.Length
+               || text[index] is '\n';
+
+        private static int NextStartIndex(int index, bool indexIsAtTheEndOfALine)
+            => indexIsAtTheEndOfALine
+                ? index + 1
+                : index;
+
+    }
+}
