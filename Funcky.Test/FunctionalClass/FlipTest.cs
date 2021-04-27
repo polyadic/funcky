6ab5709f5c8fbaf6fa06bd60bc1db3d192a473ec
Thomas Bruderer <apophis@apophis.ch>
2021-04-27 16:43:11 +0200

    * Add Flip and Uncurry to the Functional Class * Add Tests for Flip of Actions

--- Funcky.Test/FunctionalClass/FlipTest.cs
+++ Funcky.Test/FunctionalClass/FlipTest.cs
@@ -58,5 +58,96 @@ namespace Funcky.Test.FunctionalClass
 
             return (f(number, text, true, false, true, false, true, false) == Flip(f)(text, number, true, false, true, false, true, false)).ToProperty();
         }
+
+        [Property]
+        public Property GivenAnActionWith2ParametersTheFirstTwoParametersGetFlipped(int number, string text)
+        {
+            string side = string.Empty;
+            Action<int, string> f = (number, text) => side = $"number:{number}, text:{text}";
+
+            f(number, text);
+            var expected = side;
+            Flip(f)(text, number);
+
+            return (expected == side).ToProperty();
+        }
+
+        [Property]
+        public Property GivenAnActionWith3ParametersTheFirstTwoParametersGetFlipped(int number, string text)
+        {
+            string side = string.Empty;
+            Action<int, string, bool> f = (number, text, p3) => side = $"number:{number}, text:{text}, {p3}";
+
+            f(number, text, true);
+            var expected = side;
+            Flip(f)(text, number, true);
+
+            return (expected == side).ToProperty();
+        }
+
+        [Property]
+        public Property GivenAnActionWith4ParametersTheFirstTwoParametersGetFlipped(int number, string text)
+        {
+            string side = string.Empty;
+            Action<int, string, bool, bool> f = (number, text, p3, p4) => side = $"number:{number}, text:{text}, {p3}, {p4}";
+
+            f(number, text, true, false);
+            var expected = side;
+            Flip(f)(text, number, true, false);
+
+            return (expected == side).ToProperty();
+        }
+
+        [Property]
+        public Property GivenAnActionWith5ParametersTheFirstTwoParametersGetFlipped(int number, string text)
+        {
+            string side = string.Empty;
+            Action<int, string, bool, bool, bool> f = (number, text, p3, p4, p5) => side = $"number:{number}, text:{text}, {p3}, {p4}, {p5}";
+
+            f(number, text, true, false, false);
+            var expected = side;
+            Flip(f)(text, number, true, false, false);
+
+            return (expected == side).ToProperty();
+        }
+
+        [Property]
+        public Property GivenAnActionWith6ParametersTheFirstTwoParametersGetFlipped(int number, string text)
+        {
+            string side = string.Empty;
+            Action<int, string, bool, bool, bool, bool> f = (number, text, p3, p4, p5, p6) => side = $"number:{number}, text:{text}, {p3}, {p4}, {p5}, {p6}";
+
+            f(number, text, true, false, false, true);
+            var expected = side;
+            Flip(f)(text, number, true, false, false, true);
+
+            return (expected == side).ToProperty();
+        }
+
+        [Property]
+        public Property GivenAnActionWith7ParametersTheFirstTwoParametersGetFlipped(int number, string text)
+        {
+            string side = string.Empty;
+            Action<int, string, bool, bool, bool, bool, bool> f = (number, text, p3, p4, p5, p6, p7) => side = $"number:{number}, text:{text}, {p3}, {p4}, {p5}, {p6}, {p7}";
+
+            f(number, text, true, false, false, true, true);
+            var expected = side;
+            Flip(f)(text, number, true, false, false, true, true);
+
+            return (expected == side).ToProperty();
+        }
+
+        [Property]
+        public Property GivenAnActionWith8ParametersTheFirstTwoParametersGetFlipped(int number, string text)
+        {
+            string side = string.Empty;
+            Action<int, string, bool, bool, bool, bool, bool, bool> f = (number, text, p3, p4, p5, p6, p7, p8) => side = $"number:{number}, text:{text}, {p3}, {p4}, {p5}, {p6}, {p7}, {p8}";
+
+            f(number, text, true, false, false, true, true, true);
+            var expected = side;
+            Flip(f)(text, number, true, false, false, true, true, true);
+
+            return (expected == side).ToProperty();
+        }
     }
 }
