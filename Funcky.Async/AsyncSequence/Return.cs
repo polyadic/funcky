68197538d6f285aa0eb10149fccac849a1f20ff3
Thomas Bruderer <thomas.bruderer@messerli.ch>
2021-05-20 17:24:56 +0200

    Add Puire to AsyncSequnce.Return

--- Funcky.Async/AsyncSequence/Return.cs
+++ Funcky.Async/AsyncSequence/Return.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 
 namespace Funcky
 {
     public static partial class AsyncSequence
     {
+        [Pure]
         public static IAsyncEnumerable<TItem> Return<TItem>(TItem item)
             => AsyncEnumerable.Repeat(item, 1);
     }
