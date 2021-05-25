b0413c5259fda1e15007a7b796add7916a24b532
Thomas Bruderer <thomas.bruderer@messerli.ch>
2021-05-25 11:22:38 +0200

    Implement InspectAwait

--- Funcky.Async.Test/Extensions/AsyncEnumerableExtensions/InspectTest.cs
+++ Funcky.Async.Test/Extensions/AsyncEnumerableExtensions/InspectTest.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Funcky.Async.Extensions;
 using Funcky.Async.Test.TestUtilities;
 using Xunit;
+using static Funcky.Async.Functional;
 using static Funcky.Functional;
 
 namespace Funcky.Async.Test.Extensions.AsyncEnumerableExtensions
@@ -19,13 +20,41 @@ namespace Funcky.Async.Test.Extensions.AsyncEnumerableExtensions
         }
 
         [Fact]
-        public async Task GivenAnEnumerableAndInjectWeCanApplySideEffectsToEnumerables()
+        public void InspectAwaitIsEnumeratedLazily()
+        {
+            var doNotEnumerate = new FailOnEnumerateAsyncSequence<object>();
+
+            _ = doNotEnumerate.InspectAwait(static async o => await NoOperationAsync(o));
+        }
+
+        [Fact]
+        public async Task GivenAnAsyncEnumerableAndInjectWeCanApplySideEffectsToAsyncEnumerables()
         {
             var sideEffect = 0;
             var numbers = new List<int> { 1, 2, 3, 42 }.ToAsyncEnumerable();
 
             var numbersWithSideEffect = numbers
                 .Inspect(n => { ++sideEffect; });
+
+            Assert.Equal(0, sideEffect);
+
+            await numbersWithSideEffect.ForEachAsync(NoOperation<int>);
+
+            Assert.Equal(await numbers.CountAsync(), sideEffect);
+        }
+
+        [Fact]
+        public async Task GivenAnAsyncEnumerableAndInjectAnAsynchronouseActionWeCanApplySideEffectsToAsyncEnumerables()
+        {
+            var sideEffect = 0;
+            var numbers = new List<int> { 1, 2, 3, 42 }.ToAsyncEnumerable();
+
+            var numbersWithSideEffect = numbers
+                .InspectAwait(n =>
+                {
+                    ++sideEffect;
+                    return default;
+                });
 
             Assert.Equal(0, sideEffect);
 
--- Funcky.Async/Extensions/AsyncEnumerableExtensions/Inspect.cs
+++ Funcky.Async/Extensions/AsyncEnumerableExtensions/Inspect.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Funcky.Async.Extensions
 {
@@ -8,16 +11,42 @@ namespace Funcky.Async.Extensions
     {
         /// <summary>
         /// An IEnumerable that calls a function on each element before yielding it. It can be used to encode side effects without enumerating.
-        /// The side effect will be executed when enumerating the result.
+        /// The side effect will be executed whenever enumerating of the result happens.
         /// </summary>
-        /// <typeparam name="T">the inner type of the enumerable.</typeparam>
+        /// <typeparam name="TSource">the inner type of the async enumerable.</typeparam>
+        /// <param name="elements">An async enumerable.</param>
+        /// <param name="action">A synchronous action.</param>
         /// <returns>returns an <see cref="IEnumerable{T}" /> with the side effect defined by action encoded in the enumerable.</returns>
         [Pure]
-        public static async IAsyncEnumerable<T> Inspect<T>(this IAsyncEnumerable<T> elements, Action<T> action)
+        public static IAsyncEnumerable<TSource> Inspect<TSource>(this IAsyncEnumerable<TSource> elements, Action<TSource> action)
+            => AsyncEnumerable.Create(cancellationToken => elements.InspectInternal(action, cancellationToken));
+
+        /// <summary>
+        /// An IEnumerable that calls and awaits the function on each element before yielding it. It can be used to encode side effects without enumerating.
+        /// The side effect will be executed whenever enumerating of the result happens.
+        /// </summary>
+        /// <typeparam name="TSource">the inner type of the async enumerable.</typeparam>
+        /// <param name="elements">An async enumerable.</param>
+        /// <param name="action">An asynchronous action.</param>
+        /// <returns>returns an <see cref="IEnumerable{T}" /> with the side effect defined by action encoded in the enumerable.</returns>
+        [Pure]
+        public static IAsyncEnumerable<TSource> InspectAwait<TSource>(this IAsyncEnumerable<TSource> elements, Func<TSource, ValueTask> action)
+            => AsyncEnumerable.Create(cancellationToken => elements.InspectAwaitInternal(action, cancellationToken));
+
+        private static async IAsyncEnumerator<TSource> InspectInternal<TSource>(this IAsyncEnumerable<TSource> elements, Action<TSource> action, CancellationToken cancellationToken)
         {
-            await foreach (var element in elements)
+            await foreach (var element in elements.WithCancellation(cancellationToken))
             {
                 action(element);
+                yield return element;
+            }
+        }
+
+        private static async IAsyncEnumerator<TSource> InspectAwaitInternal<TSource>(this IAsyncEnumerable<TSource> elements, Func<TSource, ValueTask> action, CancellationToken cancellationToken)
+        {
+            await foreach (var element in elements.WithCancellation(cancellationToken))
+            {
+                await action(element);
                 yield return element;
             }
         }
