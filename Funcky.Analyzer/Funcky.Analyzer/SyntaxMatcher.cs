155432681bb7ffeae50dd3334e5672c7be9ed6ab
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2021-08-12 17:00:29 +0200

    Compare for symbol equality

--- Funcky.Analyzer/Funcky.Analyzer/SyntaxMatcher.cs
+++ Funcky.Analyzer/Funcky.Analyzer/SyntaxMatcher.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -11,7 +9,6 @@ namespace Funcky.Analyzer
 {
     public sealed class SyntaxMatcher
     {
-        private const string NamespaceSeparator = ".";
         private readonly SyntaxNodeAnalysisContext _analysisContext;
 
         public SyntaxMatcher(SyntaxNodeAnalysisContext analysisContext)
@@ -24,7 +21,7 @@ namespace Funcky.Analyzer
         public bool MatchStaticCall(string fullTypeName, string methodName)
             => _analysisContext.SemanticModel.GetOperation(InvocationExpr) is IInvocationOperation { TargetMethod: { } method }
                 && method.Name == methodName
-                && FullTypeName(method) == fullTypeName;
+                && SymbolEqualityComparer.Default.Equals(_analysisContext.Compilation.GetTypeByMetadataName(fullTypeName), method.ContainingType);
 
         public bool MatchArgument<TArgument>(int argumentPosition, TArgument argumentValue)
             => GetArgument(argumentPosition) is { } argument
@@ -47,13 +44,5 @@ namespace Funcky.Analyzer
                 && argumentList.Arguments.Count > argumentPosition
                     ? argumentList.Arguments[argumentPosition]
                     : null;
-
-        private static string FullTypeName(IMethodSymbol method)
-            => string.Join(NamespaceSeparator, NamespaceParts(method.ContainingNamespace).Append(method.ContainingType.Name));
-
-        private static IEnumerable<string> NamespaceParts(INamespaceSymbol namespaceSymbol)
-            => namespaceSymbol.ContainingNamespace.IsGlobalNamespace
-                ? Enumerable.Repeat(namespaceSymbol.Name, 1)
-                : NamespaceParts(namespaceSymbol.ContainingNamespace).Append(namespaceSymbol.Name);
     }
 }
