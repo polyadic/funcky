6f69e4856966616c843d5c2d0c55490c59dc7dcb
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2021-09-12 21:16:35 +0200

    Remove no longer needed global using

--- Funcky/GlobalUsings.cs
+++ Funcky/GlobalUsings.cs
@@ -5,8 +5,6 @@ global using System;
 global using System.Collections.Generic;
 global using System.IO;
 global using System.Linq;
-global using System.Threading;
-global using System.Threading.Tasks;
 
 // TODO: Move thse to .csproj once .NET 6 Preview 7 is available
 global using System.Diagnostics.Contracts;
