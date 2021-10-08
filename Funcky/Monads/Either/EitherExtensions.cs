064cc0d99679169b74d55cd649ef19dae4509cdb
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2021-10-08 09:54:08 +0200

    Relax overly strict constraints

--- Funcky/Monads/Either/EitherExtensions.cs
+++ Funcky/Monads/Either/EitherExtensions.cs
@@ -5,14 +5,12 @@ public static class EitherExtensions
     /// <summary>Returns the left value or <see cref="Option{TItem}.None()"/> if the <paramref name="either"/> is a right value.</summary>
     public static Option<TLeft> LeftOrNone<TLeft, TRight>(this Either<TLeft, TRight> either)
         where TLeft : notnull
-        where TRight : notnull
         => either.Match(
             left: Option.Some,
             right: static _ => Option<TLeft>.None());
 
     /// <summary>Returns the right value or <see cref="Option{TItem}.None()"/> if the <paramref name="either"/> is a left value.</summary>
     public static Option<TRight> RightOrNone<TLeft, TRight>(this Either<TLeft, TRight> either)
-        where TLeft : notnull
         where TRight : notnull
         => either.Match(
             left: static _ => Option<TRight>.None(),
