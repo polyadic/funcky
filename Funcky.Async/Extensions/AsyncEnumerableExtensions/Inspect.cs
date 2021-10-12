729c9ba6501d3b753dc4d56bfcfba2cf4ff66e6a
Thomas Bruderer <apophis@apophis.ch>
2021-10-12 10:21:16 +0200

    Fix name inconsistency

--- Funcky.Async/Extensions/AsyncEnumerableExtensions/Inspect.cs
+++ Funcky.Async/Extensions/AsyncEnumerableExtensions/Inspect.cs
@@ -7,24 +7,24 @@ namespace Funcky.Async.Extensions
         /// The side effect will be executed whenever enumerating of the result happens.
         /// </summary>
         /// <typeparam name="TSource">The inner type of the async enumerable.</typeparam>
-        /// <param name="elements">An async enumerable.</param>
+        /// <param name="source">An async enumerable.</param>
         /// <param name="action">A synchronous action.</param>
         /// <returns>Returns an <see cref="IEnumerable{T}" /> with the side effect defined by action encoded in the enumerable.</returns>
         [Pure]
-        public static IAsyncEnumerable<TSource> Inspect<TSource>(this IAsyncEnumerable<TSource> elements, Action<TSource> action)
-            => AsyncEnumerable.Create(cancellationToken => elements.InspectInternal(action, cancellationToken));
+        public static IAsyncEnumerable<TSource> Inspect<TSource>(this IAsyncEnumerable<TSource> source, Action<TSource> action)
+            => AsyncEnumerable.Create(cancellationToken => source.InspectInternal(action, cancellationToken));
 
         /// <summary>
         /// An IEnumerable that calls and awaits the function on each element before yielding it. It can be used to encode side effects without enumerating.
         /// The side effect will be executed whenever enumerating of the result happens.
         /// </summary>
         /// <typeparam name="TSource">The inner type of the async enumerable.</typeparam>
-        /// <param name="elements">An async enumerable.</param>
+        /// <param name="source">An async enumerable.</param>
         /// <param name="action">An asynchronous action.</param>
         /// <returns>Returns an <see cref="IEnumerable{T}" /> with the side effect defined by action encoded in the enumerable.</returns>
         [Pure]
-        public static IAsyncEnumerable<TSource> InspectAwait<TSource>(this IAsyncEnumerable<TSource> elements, Func<TSource, ValueTask> action)
-            => AsyncEnumerable.Create(cancellationToken => elements.InspectAwaitInternal(action, cancellationToken));
+        public static IAsyncEnumerable<TSource> InspectAwait<TSource>(this IAsyncEnumerable<TSource> source, Func<TSource, ValueTask> action)
+            => AsyncEnumerable.Create(cancellationToken => source.InspectAwaitInternal(action, cancellationToken));
 
         private static async IAsyncEnumerator<TSource> InspectInternal<TSource>(this IAsyncEnumerable<TSource> elements, Action<TSource> action, CancellationToken cancellationToken)
         {
@@ -35,9 +35,9 @@ namespace Funcky.Async.Extensions
             }
         }
 
-        private static async IAsyncEnumerator<TSource> InspectAwaitInternal<TSource>(this IAsyncEnumerable<TSource> elements, Func<TSource, ValueTask> action, CancellationToken cancellationToken)
+        private static async IAsyncEnumerator<TSource> InspectAwaitInternal<TSource>(this IAsyncEnumerable<TSource> source, Func<TSource, ValueTask> action, CancellationToken cancellationToken)
         {
-            await foreach (var element in elements.WithCancellation(cancellationToken))
+            await foreach (var element in source.WithCancellation(cancellationToken))
             {
                 await action(element).ConfigureAwait(false);
                 yield return element;
