e9bdd820ff46ce9589c667b588ed0497eec71692
Thomas Bruderer <apophis@apophis.ch>
2021-10-12 15:26:24 +0200

    Implement all overloads for [Min|Max|Average]OrNone*Async
    * Handle selectors
    * Extract Aggregators and share with Funcky.Async

--- Funcky/Extensions/EnumerableExtensions/MaxOrNone.cs
+++ Funcky/Extensions/EnumerableExtensions/MaxOrNone.cs
@@ -1,3 +1,5 @@
+using Funcky.Internal.Aggregators;
+
 namespace Funcky.Extensions
 {
     public static partial class EnumerableExtensions
@@ -34,7 +36,7 @@ namespace Funcky.Extensions
         [Pure]
         public static Option<TResult> MaxOrNone<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
             where TResult : notnull
-            => source.Select(selector).Aggregate(Option<TResult>.None, AggregateMax);
+            => source.Select(selector).Aggregate(Option<TResult>.None, MaxAggregator.Aggregate);
 
         /// <summary>
         /// Invokes a transform function on each element of a sequence and returns the maximum from the optional generic values compared by a <see cref="Comparer{T}"/>. If the transformed sequence only consists of none or is empty it returns None.
@@ -48,12 +50,5 @@ namespace Funcky.Extensions
         public static Option<TResult> MaxOrNone<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, Option<TResult>> selector)
             where TResult : notnull
             => source.WhereSelect(selector).MaxOrNone(Identity);
-
-        private static Option<TResult> AggregateMax<TResult>(Option<TResult> min, TResult current)
-            where TResult : notnull
-            => min.Match(current, m => Max(m, current));
-
-        private static TSource Max<TSource>(TSource left, TSource right)
-            => Comparer<TSource>.Default.Compare(left, right) > 0 ? left : right;
     }
 }
