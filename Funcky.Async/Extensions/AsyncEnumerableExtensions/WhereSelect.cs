dc4c4cf6a47c94aef5b8ea172c4fa29f3386f17f
Thomas Bruderer <apophis@apophis.ch>
2021-10-12 09:24:50 +0200

    Add WhereSelect for AsyncEnumerable again without an argument

--- Funcky.Async/Extensions/AsyncEnumerableExtensions/WhereSelect.cs
+++ Funcky.Async/Extensions/AsyncEnumerableExtensions/WhereSelect.cs
@@ -3,30 +3,51 @@ namespace Funcky.Async.Extensions
     public static partial class AsyncEnumerableExtensions
     {
         /// <summary>
+        /// Filters out all the empty values from an <c>IEnumerable&lt;Option&lt;T&gt;&gt;</c> and therefore returns an <see cref="IEnumerable{TItem}"/>.
+        /// </summary>
+        [Pure]
+        public static IAsyncEnumerable<TItem> WhereSelect<TItem>(this IAsyncEnumerable<Option<TItem>> source)
+            where TItem : notnull
+            => source.WhereSelect(Identity);
+
+        /// <summary>
         /// Projects and filters an <see cref="IAsyncEnumerable{T}"/> at the same time.
         /// This is done by filtering out any empty <see cref="Option{T}"/> values returned by the <paramref name="selector"/>.
         /// </summary>
         [Pure]
         public static IAsyncEnumerable<TOutput> WhereSelect<TSource, TOutput>(this IAsyncEnumerable<TSource> source, Func<TSource, Option<TOutput>> selector)
             where TOutput : notnull
-            => source.Select(selector).SelectMany(ToAsyncEnumerable);
+            => source.WhereSelectAwaitWithCancellation((item, _) => new ValueTask<Option<TOutput>>(selector(item)));
 
         /// <inheritdoc cref="WhereSelect{TSource,TOutput}"/>
         [Pure]
         public static IAsyncEnumerable<TOutput> WhereSelectAwait<TSource, TOutput>(this IAsyncEnumerable<TSource> source, Func<TSource, ValueTask<Option<TOutput>>> selector)
             where TOutput : notnull
-            => source.SelectAwait(selector).SelectMany(ToAsyncEnumerable);
+            => source.WhereSelectAwaitWithCancellation((item, _) => selector(item));
 
         /// <inheritdoc cref="WhereSelect{TSource,TOutput}"/>
         [Pure]
         public static IAsyncEnumerable<TOutput> WhereSelectAwaitWithCancellation<TSource, TOutput>(this IAsyncEnumerable<TSource> source, Func<TSource, CancellationToken, ValueTask<Option<TOutput>>> selector)
             where TOutput : notnull
-            => source.SelectAwaitWithCancellation(selector).SelectMany(ToAsyncEnumerable);
+            => AsyncEnumerable.Create(cancellationToken => WhereSelectAwaitWithCancellationInternal(source, selector, cancellationToken));
 
-        private static IAsyncEnumerable<TItem> ToAsyncEnumerable<TItem>(Option<TItem> option)
-            where TItem : notnull
-            => option.Match(
-                none: AsyncEnumerable.Empty<TItem>,
-                some: AsyncSequence.Return);
+#pragma warning disable 8425
+        private static async IAsyncEnumerator<TOutput> WhereSelectAwaitWithCancellationInternal<TSource, TOutput>(
+            IAsyncEnumerable<TSource> source,
+            Func<TSource, CancellationToken, ValueTask<Option<TOutput>>> selector,
+            CancellationToken cancellationToken)
+            where TOutput : notnull
+        {
+            await foreach (var item in source.WithCancellation(cancellationToken).ConfigureAwait(false))
+            {
+                var projectedItem = await selector(item, cancellationToken).ConfigureAwait(false);
+
+                foreach (var value in projectedItem.ToEnumerable())
+                {
+                    yield return value;
+                }
+            }
+        }
+#pragma warning restore 8425
     }
 }
