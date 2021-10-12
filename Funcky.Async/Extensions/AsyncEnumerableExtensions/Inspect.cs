29fa4f1ae82588bf37689a703213dd0904845503
Thomas Bruderer <apophis@apophis.ch>
2021-10-12 10:08:27 +0200

    Fix typos

--- Funcky.Async/Extensions/AsyncEnumerableExtensions/Inspect.cs
+++ Funcky.Async/Extensions/AsyncEnumerableExtensions/Inspect.cs
@@ -13,10 +13,10 @@ namespace Funcky.Async.Extensions
         /// An IEnumerable that calls a function on each element before yielding it. It can be used to encode side effects without enumerating.
         /// The side effect will be executed whenever enumerating of the result happens.
         /// </summary>
-        /// <typeparam name="TSource">the inner type of the async enumerable.</typeparam>
+        /// <typeparam name="TSource">The inner type of the async enumerable.</typeparam>
         /// <param name="elements">An async enumerable.</param>
         /// <param name="action">A synchronous action.</param>
-        /// <returns>returns an <see cref="IEnumerable{T}" /> with the side effect defined by action encoded in the enumerable.</returns>
+        /// <returns>Returns an <see cref="IEnumerable{T}" /> with the side effect defined by action encoded in the enumerable.</returns>
         [Pure]
         public static IAsyncEnumerable<TSource> Inspect<TSource>(this IAsyncEnumerable<TSource> elements, Action<TSource> action)
             => AsyncEnumerable.Create(cancellationToken => elements.InspectInternal(action, cancellationToken));
@@ -25,10 +25,10 @@ namespace Funcky.Async.Extensions
         /// An IEnumerable that calls and awaits the function on each element before yielding it. It can be used to encode side effects without enumerating.
         /// The side effect will be executed whenever enumerating of the result happens.
         /// </summary>
-        /// <typeparam name="TSource">the inner type of the async enumerable.</typeparam>
+        /// <typeparam name="TSource">The inner type of the async enumerable.</typeparam>
         /// <param name="elements">An async enumerable.</param>
         /// <param name="action">An asynchronous action.</param>
-        /// <returns>returns an <see cref="IEnumerable{T}" /> with the side effect defined by action encoded in the enumerable.</returns>
+        /// <returns>Returns an <see cref="IEnumerable{T}" /> with the side effect defined by action encoded in the enumerable.</returns>
         [Pure]
         public static IAsyncEnumerable<TSource> InspectAwait<TSource>(this IAsyncEnumerable<TSource> elements, Func<TSource, ValueTask> action)
             => AsyncEnumerable.Create(cancellationToken => elements.InspectAwaitInternal(action, cancellationToken));
