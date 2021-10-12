3ea3e43e8c1dd4522dd49535617bcdf045809ff4
Thomas Bruderer <apophis@apophis.ch>
2021-10-12 10:08:39 +0200

    Remove global usings

--- Funcky.Async/Extensions/AsyncEnumerableExtensions/Inspect.cs
+++ Funcky.Async/Extensions/AsyncEnumerableExtensions/Inspect.cs
@@ -1,10 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics.Contracts;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-
 namespace Funcky.Async.Extensions
 {
     public static partial class AsyncEnumerableExtensions
