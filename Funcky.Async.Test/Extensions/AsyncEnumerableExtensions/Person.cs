63e321940e45fbd108b89af35bb65a0efd4875e3
Thomas Bruderer <apophis@apophis.ch>
2021-11-01 10:36:45 +0100

    Better equality support for Person

--- Funcky.Async.Test/Extensions/AsyncEnumerableExtensions/Person.cs
+++ Funcky.Async.Test/Extensions/AsyncEnumerableExtensions/Person.cs
@@ -1,4 +1,4 @@
-namespace Funcky.Test.Extensions.EnumerableExtensions
+namespace Funcky.Async.Test.Extensions.AsyncEnumerableExtensions
 {
     internal sealed class Person : IComparable<Person>, IEquatable<Person>
     {
@@ -12,21 +12,12 @@ namespace Funcky.Test.Extensions.EnumerableExtensions
                 ? Age.CompareTo(other.Age)
                 : -1;
 
-        public static Person Create(int age)
-            => new(age);
-
-        public static Person? Create(int? age)
-            => age.HasValue
-                ? new Person(age.Value)
-                : null;
-
         public bool Equals(Person? other)
-             => other?.Age == Age;
+            => other?.Age == Age;
 
         public override bool Equals(object? other)
             => Equals(other as Person);
 
-        public override int GetHashCode()
-            => Age.GetHashCode();
+        public override int GetHashCode() => Age.GetHashCode();
     }
 }
