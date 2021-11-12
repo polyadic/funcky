bff216b0fc2ec8c4f9b9dd96137c0c72cabef454
Thomas Bruderer <apophis@apophis.ch>
2021-11-12 11:10:22 +0100

    Rename Funcky.Analyzer to Funcky.Analyzers

---
+++ Funcky.Analyzers/Funcky.Analyzers.Test/EnumerableRepeatNeverTest.cs
@@ -0,0 +1,49 @@
+using Xunit;
+using VerifyCS = Funcky.Analyzers.Test.CSharpCodeFixVerifier<Funcky.Analyzers.EnumerableRepeatNeverAnalyzer, Funcky.Analyzers.EnumerableRepeatNeverCodeFix>;
+
+namespace Funcky.Analyzers.Test
+{
+    public sealed class EnumerableRepeatNeverTest
+    {
+        [Fact]
+        public async Task EnumerableRepeatWithAnyNumberButZeroIssuesNoDiagnostic()
+        {
+            var inputCode = File.ReadAllText("TestCode/EnumerableRepeatWithAnyNumber.input");
+
+            await VerifyCS.VerifyAnalyzerAsync(inputCode);
+        }
+
+        [Fact]
+        public async Task UsingEnumerableRepeatNeverShowsTheSequenceReturnDiagnostic()
+        {
+            var expectedDiagnostic = VerifyCS
+                .Diagnostic(nameof(EnumerableRepeatNeverAnalyzer))
+                .WithSpan(10, 26, 10, 62)
+                .WithArguments("\"Hello world!\"", "string");
+
+            await VerifyWithSourceExample.VerifyDiagnosticAndCodeFix<EnumerableRepeatNeverAnalyzer, EnumerableRepeatNeverCodeFix>(expectedDiagnostic, "RepeatNever");
+        }
+
+        [Fact]
+        public async Task UsingEnumerableRepeatNeverViaConstantShowsTheSequenceReturnDiagnostic()
+        {
+            var expectedDiagnostic = VerifyCS
+                .Diagnostic(nameof(EnumerableRepeatNeverAnalyzer))
+                .WithSpan(11, 26, 11, 66)
+                .WithArguments("\"Hello world!\"", "string");
+
+            await VerifyWithSourceExample.VerifyDiagnosticAndCodeFix<EnumerableRepeatNeverAnalyzer, EnumerableRepeatNeverCodeFix>(expectedDiagnostic, "RepeatNeverWithConstant");
+        }
+
+        [Fact]
+        public async Task UsingEnumerableRepeatNeverWorksWithDifferentTypes()
+        {
+            var expectedDiagnostic = VerifyCS
+                .Diagnostic(nameof(EnumerableRepeatNeverAnalyzer))
+                .WithSpan(10, 26, 10, 52)
+                .WithArguments("1337", "int");
+
+            await VerifyWithSourceExample.VerifyDiagnosticAndCodeFix<EnumerableRepeatNeverAnalyzer, EnumerableRepeatNeverCodeFix>(expectedDiagnostic, "RepeatNeverWithInt");
+        }
+    }
+}
---
+++ Funcky.Analyzers/Funcky.Analyzers.Test/EnumerableRepeatOnceTest.cs
@@ -0,0 +1,38 @@
+using Xunit;
+using VerifyCS = Funcky.Analyzers.Test.CSharpCodeFixVerifier<Funcky.Analyzers.EnumerableRepeatOnceAnalyzer, Funcky.Analyzers.EnumerableRepeatOnceCodeFix>;
+
+namespace Funcky.Analyzers.Test
+{
+    public sealed class EnumerableRepeatOnceTest
+    {
+        [Fact]
+        public async Task EnumerableRepeatWithAnyNumberButOneIssuesNoDiagnostic()
+        {
+            var inputCode = File.ReadAllText("TestCode/EnumerableRepeatWithAnyNumber.input");
+
+            await VerifyCS.VerifyAnalyzerAsync(inputCode);
+        }
+
+        [Fact]
+        public async Task UsingEnumerableRepeatOnceShowsTheSequenceReturnDiagnostic()
+        {
+            var expectedDiagnostic = VerifyCS
+                .Diagnostic(nameof(EnumerableRepeatOnceAnalyzer))
+                .WithSpan(17, 26, 17, 62)
+                .WithArguments("\"Hello world!\"");
+
+            await VerifyWithSourceExample.VerifyDiagnosticAndCodeFix<EnumerableRepeatOnceAnalyzer, EnumerableRepeatOnceCodeFix>(expectedDiagnostic, "RepeatOnce");
+        }
+
+        [Fact]
+        public async Task UsingEnumerableRepeatOnceViaConstantShowsTheSequenceReturnDiagnostic()
+        {
+            var expectedDiagnostic = VerifyCS
+                .Diagnostic(nameof(EnumerableRepeatOnceAnalyzer))
+                .WithSpan(18, 26, 18, 65)
+                .WithArguments("\"Hello world!\"");
+
+            await VerifyWithSourceExample.VerifyDiagnosticAndCodeFix<EnumerableRepeatOnceAnalyzer, EnumerableRepeatOnceCodeFix>(expectedDiagnostic, "RepeatOnceWithConstant");
+        }
+    }
+}
