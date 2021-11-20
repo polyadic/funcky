166100c3bd39a1dbd7ebf619bc00b59411128c97
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2021-11-20 18:08:06 +0100

    Add documentation to attribute

--- Funcky/CodeAnalysis/UseWithArgumentNamesAttribute.cs
+++ Funcky/CodeAnalysis/UseWithArgumentNamesAttribute.cs
@@ -1,5 +1,6 @@
 namespace Funcky.CodeAnalysis
 {
+    /// <summary>Methods annotated with this attribute should be used with named arguments.</summary>
     [AttributeUsage(AttributeTargets.Method)]
     internal sealed class UseWithArgumentNamesAttribute : Attribute
     {
