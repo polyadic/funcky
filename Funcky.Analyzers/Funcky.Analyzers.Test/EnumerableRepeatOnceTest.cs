a64e1bf5e1d503f272c4798793aef5f85f858eff
Thomas Bruderer <apophis@apophis.ch>
2021-12-16 09:46:33 +0100

    Fix the DiagnosticId in tests

--- Funcky.Analyzers/Funcky.Analyzers.Test/EnumerableRepeatOnceTest.cs
+++ Funcky.Analyzers/Funcky.Analyzers.Test/EnumerableRepeatOnceTest.cs
@@ -17,7 +17,7 @@ namespace Funcky.Analyzers.Test
         public async Task UsingEnumerableRepeatOnceShowsTheSequenceReturnDiagnostic()
         {
             var expectedDiagnostic = VerifyCS
-                .Diagnostic(nameof(EnumerableRepeatOnceAnalyzer))
+                .Diagnostic(EnumerableRepeatOnceAnalyzer.DiagnosticId)
                 .WithSpan(17, 26, 17, 62)
                 .WithArguments("\"Hello world!\"");
 
@@ -28,7 +28,7 @@ namespace Funcky.Analyzers.Test
         public async Task UsingEnumerableRepeatOnceViaConstantShowsTheSequenceReturnDiagnostic()
         {
             var expectedDiagnostic = VerifyCS
-                .Diagnostic(nameof(EnumerableRepeatOnceAnalyzer))
+                .Diagnostic(EnumerableRepeatOnceAnalyzer.DiagnosticId)
                 .WithSpan(18, 26, 18, 65)
                 .WithArguments("\"Hello world!\"");
 
