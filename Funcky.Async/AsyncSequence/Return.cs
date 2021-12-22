fcacf6bfa6bc030e13e4175d906826de5fb98f8e
Thomas Bruderer <apophis@apophis.ch>
2021-12-22 11:07:08 +0100

    Implement Interleave on IAsyncEnumerable Fixes #338 and also implements AsyncSequence.Return with params

--- Funcky.Async/AsyncSequence/Return.cs
+++ Funcky.Async/AsyncSequence/Return.cs
@@ -5,5 +5,8 @@ namespace Funcky
         [Pure]
         public static IAsyncEnumerable<TItem> Return<TItem>(TItem item)
             => AsyncEnumerable.Repeat(item, 1);
+
+        public static IAsyncEnumerable<TItem> Return<TItem>(params TItem[] items)
+            => items.ToAsyncEnumerable();
     }
 }
