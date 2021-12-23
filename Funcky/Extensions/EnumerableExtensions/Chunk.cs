d0b1483e020d049b1125802520093c4a32fc130a
Thomas Bruderer <apophis@apophis.ch>
2021-12-23 15:07:21 +0100

    Implemented Chunk for IAsyncEnumerables.

--- Funcky/Extensions/EnumerableExtensions/Chunk.cs
+++ Funcky/Extensions/EnumerableExtensions/Chunk.cs
@@ -12,13 +12,13 @@ namespace Funcky.Extensions
         /// <param name="size">The desired size of the chunks.</param>
         /// <returns>A sequence of equally sized sequences containing elements of the source collection in the same order.</returns>
         [Pure]
-        #if NET6_0_OR_GREATER
+#if NET6_0_OR_GREATER
         [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
         public static IEnumerable<IEnumerable<TSource>> Chunk<TSource>(IEnumerable<TSource> source, int size)
-        #else
+#else
         public static IEnumerable<IEnumerable<TSource>> Chunk<TSource>(this IEnumerable<TSource> source, int size)
-        #endif
-            => Chunk(source, size, Identity);
+#endif
+            => ChunkEnumerable(source, ValidateChunkSize(size));
 
         /// <summary>
         /// Chunks the source sequence into equally sized chunks. The last chunk can be smaller.
@@ -31,27 +31,21 @@ namespace Funcky.Extensions
         /// <returns>A sequence of results based on equally sized chunks.</returns>
         [Pure]
         public static IEnumerable<TResult> Chunk<TSource, TResult>(this IEnumerable<TSource> source, int size, Func<IEnumerable<TSource>, TResult> resultSelector)
-        {
-            ValidateChunkSize(size);
+            => ChunkEnumerable(source, ValidateChunkSize(size))
+                .Select(resultSelector);
 
-            return ChunkEnumerable(source, size, resultSelector);
-        }
+        private static int ValidateChunkSize(int size)
+            => size > 0
+                ? size
+                : throw new ArgumentOutOfRangeException(nameof(size), size, "Size must be larger than 0");
 
-        private static void ValidateChunkSize(int size)
-        {
-            if (size <= 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(size), size, "Size must be larger than 0");
-            }
-        }
-
-        private static IEnumerable<TResult> ChunkEnumerable<TSource, TResult>(IEnumerable<TSource> source, int size, Func<IEnumerable<TSource>, TResult> resultSelector)
+        private static IEnumerable<IEnumerable<TSource>> ChunkEnumerable<TSource>(IEnumerable<TSource> source, int size)
         {
             using var sourceEnumerator = source.GetEnumerator();
 
             while (sourceEnumerator.MoveNext())
             {
-                yield return resultSelector(TakeSkip(sourceEnumerator, size).ToImmutableList());
+                yield return TakeSkip(sourceEnumerator, size).ToImmutableList();
             }
         }
 
