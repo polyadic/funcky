2c4356624c671ead1caec6e493480bb1b1a3feaf
Thomas Bruderer <apophis@apophis.ch>
2021-12-27 14:20:17 +0100

    Fix tests and improve code

--- Funcky/Internal/Mixer.cs
+++ Funcky/Internal/Mixer.cs
@@ -1,19 +1,25 @@
+using System.Collections.Immutable;
+
 namespace Funcky.Internal
 {
     internal static class Mixer
     {
-        public static IEnumerable<T> ToRandomEnumerable<T>(IList<T> source, Random random)
+        public static IEnumerable<TSource> ToRandomEnumerable<TSource>(IList<TSource> source, Random random)
+            => Enumerable
+                .Range(0, source.Count)
+                .Aggregate(ImmutableList<TSource>.Empty, AggregateShuffle(source, random));
+
+        public static Func<ImmutableList<TSource>, int, ImmutableList<TSource>> AggregateShuffle<TSource>(IList<TSource> source, Random random)
+            => (shuffled, currentIndex)
+                => shuffled.Add(UseElement(source, currentIndex, random.Next(currentIndex, source.Count)));
+
+        private static TSource UseElement<TSource>(IList<TSource> source, int currentIndex, int randomIndex)
         {
-            foreach (var (currentIndex, randomIndex) in source.RandomIndex(random))
-            {
-                yield return source[randomIndex];
+            var result = source[randomIndex];
 
-                source[randomIndex] = source[currentIndex];
-            }
+            source[randomIndex] = source[currentIndex];
+
+            return result;
         }
-
-        private static IEnumerable<(int Index, int Random)> RandomIndex<T>(this ICollection<T> buffer, Random random)
-            => Enumerable.Repeat(Unit.Value, buffer.Count)
-                .Select((_, i) => (i, random.Next(i, buffer.Count)));
     }
 }
