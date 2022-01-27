41142c99c28a82258dff95375de996ba29724ea4
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-01-27 21:07:34 +0100

    Add overload with result selector

--- Funcky/Extensions/EnumerableExtensions/PartitionEither.cs
+++ Funcky/Extensions/EnumerableExtensions/PartitionEither.cs
@@ -2,10 +2,15 @@ namespace Funcky.Extensions;
 
 public static partial class EnumerableExtensions
 {
+    /// <summary>Partitions the either values in an <see cref="IEnumerable{T}"/> into a left and a right partition.</summary>
     public static EitherPartitions<TLeft, TRight> Partition<TLeft, TRight>(this IEnumerable<Either<TLeft, TRight>> source)
+        => source.Partition((left, right) => new EitherPartitions<TLeft, TRight>(left, right));
+
+    /// <summary>Partitions the either values in an <see cref="IEnumerable{T}"/> into a left and a right partition.</summary>
+    public static TResult Partition<TLeft, TRight, TResult>(this IEnumerable<Either<TLeft, TRight>> source, Func<IReadOnlyCollection<TLeft>, IReadOnlyCollection<TRight>, TResult> resultSelector)
         => source
             .Aggregate(PartitionBuilder<TLeft, TRight>.Default, Add)
-            .Build((left, right) => new EitherPartitions<TLeft, TRight>(left, right));
+            .Build(resultSelector);
 
     private static PartitionBuilder<TLeft, TRight> Add<TLeft, TRight>(PartitionBuilder<TLeft, TRight> builder, Either<TLeft, TRight> either)
         => either.Match(left: builder.AddLeft, right: builder.AddRight);
