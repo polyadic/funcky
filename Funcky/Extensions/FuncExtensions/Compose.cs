b02b38911829e9244fc3183339608c8fb3cfa10c
Thomas Bruderer <apophis@apophis.ch>
2022-02-16 15:13:10 +0100

    Split the extensions for Func and Action

--- Funcky/Extensions/FuncExtensions/Compose.cs
+++ Funcky/Extensions/FuncExtensions/Compose.cs
@@ -17,21 +17,5 @@ namespace Funcky.Extensions
         [Pure]
         public static Func<TOutput> Compose<TIntermediate, TOutput>(this Func<TIntermediate, TOutput> f, Func<TIntermediate> g)
             => () => f(g());
-
-        /// <summary>
-        /// Function composition with action.
-        /// </summary>
-        /// <returns>f ∘ g,v => f(g(v)).</returns>
-        [Pure]
-        public static Action<TInput> Compose<TInput, TIntermediate>(this Action<TIntermediate> f, Func<TInput, TIntermediate> g)
-            => v => f(g(v));
-
-        /// <summary>
-        /// Function composition with Action.
-        /// </summary>
-        /// <returns>f ∘ g, () => f(g()).</returns>
-        [Pure]
-        public static Action Compose<TIntermediate>(this Action<TIntermediate> f, Func<TIntermediate> g)
-            => () => f(g());
     }
 }
