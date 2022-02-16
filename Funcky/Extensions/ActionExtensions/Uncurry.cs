0a5b5267b2a460cf2a8a81a0a09b3c267fb7c5df
Thomas Bruderer <apophis@apophis.ch>
2022-02-16 15:13:10 +0100

    Split the extensions for Func and Action

---
+++ Funcky/Extensions/ActionExtensions/Uncurry.cs
@@ -0,0 +1,54 @@
+namespace Funcky.Extensions
+{
+    public static partial class ActionExtensions
+    {
+        /// <summary>
+        /// Transforms a curried action into a action that takes multiple arguments.
+        /// </summary>
+        [Pure]
+        public static Action<T1, T2> Uncurry<T1, T2>(this Func<T1, Action<T2>> action)
+            => (p1, p2) => action(p1)(p2);
+
+        /// <summary>
+        /// Transforms a curried action into a action that takes multiple arguments.
+        /// </summary>
+        [Pure]
+        public static Action<T1, T2, T3> Uncurry<T1, T2, T3>(this Func<T1, Func<T2, Action<T3>>> action)
+            => (p1, p2, p3) => action(p1)(p2)(p3);
+
+        /// <summary>
+        /// Transforms a curried action into a action that takes multiple arguments.
+        /// </summary>
+        [Pure]
+        public static Action<T1, T2, T3, T4> Uncurry<T1, T2, T3, T4>(this Func<T1, Func<T2, Func<T3, Action<T4>>>> action)
+            => (p1, p2, p3, p4) => action(p1)(p2)(p3)(p4);
+
+        /// <summary>
+        /// Transforms a curried action into a action that takes multiple arguments.
+        /// </summary>
+        [Pure]
+        public static Action<T1, T2, T3, T4, T5> Uncurry<T1, T2, T3, T4, T5>(this Func<T1, Func<T2, Func<T3, Func<T4, Action<T5>>>>> action)
+            => (p1, p2, p3, p4, p5) => action(p1)(p2)(p3)(p4)(p5);
+
+        /// <summary>
+        /// Transforms a curried action into a action that takes multiple arguments.
+        /// </summary>
+        [Pure]
+        public static Action<T1, T2, T3, T4, T5, T6> Uncurry<T1, T2, T3, T4, T5, T6>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Action<T6>>>>>> action)
+            => (p1, p2, p3, p4, p5, p6) => action(p1)(p2)(p3)(p4)(p5)(p6);
+
+        /// <summary>
+        /// Transforms a curried action into a action that takes multiple arguments.
+        /// </summary>
+        [Pure]
+        public static Action<T1, T2, T3, T4, T5, T6, T7> Uncurry<T1, T2, T3, T4, T5, T6, T7>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Action<T7>>>>>>> action)
+            => (p1, p2, p3, p4, p5, p6, p7) => action(p1)(p2)(p3)(p4)(p5)(p6)(p7);
+
+        /// <summary>
+        /// Transforms a curried action into a action that takes multiple arguments.
+        /// </summary>
+        [Pure]
+        public static Action<T1, T2, T3, T4, T5, T6, T7, T8> Uncurry<T1, T2, T3, T4, T5, T6, T7, T8>(this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Action<T8>>>>>>>> action)
+            => (p1, p2, p3, p4, p5, p6, p7, p8) => action(p1)(p2)(p3)(p4)(p5)(p6)(p7)(p8);
+    }
+}
--- Funcky/Extensions/FuncExtensions/Curry.cs
+++ Funcky/Extensions/FuncExtensions/Curry.cs
@@ -50,54 +50,5 @@ namespace Funcky.Extensions
         [Pure]
         public static Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, TResult>>>>>>>> Curry<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(this Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> function)
             => p1 => p2 => p3 => p4 => p5 => p6 => p7 => p8 => function(p1, p2, p3, p4, p5, p6, p7, p8);
-
-        /// <summary>
-        /// Curries the given action.
-        /// </summary>
-        [Pure]
-        public static Func<T1, Action<T2>> Curry<T1, T2>(this Action<T1, T2> action)
-            => p1 => p2 => action(p1, p2);
-
-        /// <summary>
-        /// Curries the given action.
-        /// </summary>
-        [Pure]
-        public static Func<T1, Func<T2, Action<T3>>> Curry<T1, T2, T3>(this Action<T1, T2, T3> action)
-            => p1 => p2 => p3 => action(p1, p2, p3);
-
-        /// <summary>
-        /// Curries the given action.
-        /// </summary>
-        [Pure]
-        public static Func<T1, Func<T2, Func<T3, Action<T4>>>> Curry<T1, T2, T3, T4>(this Action<T1, T2, T3, T4> action)
-            => p1 => p2 => p3 => p4 => action(p1, p2, p3, p4);
-
-        /// <summary>
-        /// Curries the given action.
-        /// </summary>
-        [Pure]
-        public static Func<T1, Func<T2, Func<T3, Func<T4, Action<T5>>>>> Curry<T1, T2, T3, T4, T5>(this Action<T1, T2, T3, T4, T5> action)
-            => p1 => p2 => p3 => p4 => p5 => action(p1, p2, p3, p4, p5);
-
-        /// <summary>
-        /// Curries the given action.
-        /// </summary>
-        [Pure]
-        public static Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Action<T6>>>>>> Curry<T1, T2, T3, T4, T5, T6>(this Action<T1, T2, T3, T4, T5, T6> action)
-            => p1 => p2 => p3 => p4 => p5 => p6 => action(p1, p2, p3, p4, p5, p6);
-
-        /// <summary>
-        /// Curries the given action.
-        /// </summary>
-        [Pure]
-        public static Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Action<T7>>>>>>> Curry<T1, T2, T3, T4, T5, T6, T7>(this Action<T1, T2, T3, T4, T5, T6, T7> action)
-            => p1 => p2 => p3 => p4 => p5 => p6 => p7 => action(p1, p2, p3, p4, p5, p6, p7);
-
-        /// <summary>
-        /// Curries the given action.
-        /// </summary>
-        [Pure]
-        public static Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Action<T8>>>>>>>> Curry<T1, T2, T3, T4, T5, T6, T7, T8>(this Action<T1, T2, T3, T4, T5, T6, T7, T8> action)
-            => p1 => p2 => p3 => p4 => p5 => p6 => p7 => p8 => action(p1, p2, p3, p4, p5, p6, p7, p8);
     }
 }
