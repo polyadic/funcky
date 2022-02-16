79b0ae7cb6cd35bf6a29399e0d1e60676e960b3e
Thomas Bruderer <apophis@apophis.ch>
2022-02-16 15:01:59 +0100

    Remove the overloads on IList (they are destructive)

--- Funcky/Extensions/EnumerableExtensions/Shuffle.cs
+++ Funcky/Extensions/EnumerableExtensions/Shuffle.cs
@@ -27,25 +27,5 @@ namespace Funcky.Extensions
             => source
                 .ToList()
                 .ToRandomList(random);
-
-        /// <summary>
-        /// Returns the given sequence eagerly in random Order in O(n).
-        /// </summary>
-        /// <typeparam name="TSource">The type of the elements in the enumerable.</typeparam>
-        /// <remarks>This method is implemented by using deferred execution. The immediate return value is an object that stores all the information that is required to perform the action. The query represented by this method is not executed until the object is enumerated either by calling its GetEnumerator method directly or by using foreach.</remarks>
-        [Pure]
-        public static IReadOnlyList<TSource> Shuffle<TSource>(this IList<TSource> source)
-            => source
-                .ToRandomList(new Random());
-
-        /// <summary>
-        /// Returns the given sequence eagerly in random Order in O(n).
-        /// </summary>
-        /// <typeparam name="TSource">The type of the elements in the enumerable.</typeparam>
-        /// <remarks>This method is implemented by using deferred execution. The immediate return value is an object that stores all the information that is required to perform the action. The query represented by this method is not executed until the object is enumerated either by calling its GetEnumerator method directly or by using foreach.</remarks>
-        [Pure]
-        public static IReadOnlyList<TSource> Shuffle<TSource>(this IList<TSource> source, Random random)
-            => source
-                .ToRandomList(random);
     }
 }
