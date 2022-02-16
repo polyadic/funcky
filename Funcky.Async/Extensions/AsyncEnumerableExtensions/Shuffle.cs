36452ffa6ffd2ae5c068a75ec3d274bbe8f97530
Thomas Bruderer <apophis@apophis.ch>
2022-02-16 14:52:42 +0100

    Improve shuffle, and widen the return type to IReadOnlyList

--- Funcky.Async/Extensions/AsyncEnumerableExtensions/Shuffle.cs
+++ Funcky.Async/Extensions/AsyncEnumerableExtensions/Shuffle.cs
@@ -1,17 +1,32 @@
-using static Funcky.Internal.Mixer;
+using Funcky.Internal;
 
 namespace Funcky.Async.Extensions
 {
     public static partial class AsyncEnumerableExtensions
     {
         /// <summary>
-        /// Returns the given sequence in random Order in O(n).
+        /// Returns the given sequence eagerly in random Order in O(n).
         /// </summary>
         /// <typeparam name="TSource">The type of the elements in the enumerable.</typeparam>
         /// <remarks>This method is implemented by using deferred execution. The immediate return value is an object that stores all the information that is required to perform the action. The query represented by this method is not executed until the object is enumerated either by calling its GetEnumerator method directly or by using foreach.</remarks>
         [Pure]
-        public static async ValueTask<IEnumerable<TSource>> Shuffle<TSource>(this IAsyncEnumerable<TSource> source, CancellationToken cancellationToken = default)
+        public static async ValueTask<IEnumerable<TSource>> Shuffle<TSource>(this IAsyncEnumerable<TSource> source)
             where TSource : notnull
-            => ToRandomEnumerable(await source.ToListAsync(cancellationToken).ConfigureAwait(false), new Random());
+            => (await source.ToListAsync().ConfigureAwait(false)).ToRandomList(new Random());
+
+        [Pure]
+        public static async ValueTask<IEnumerable<TSource>> Shuffle<TSource>(this IAsyncEnumerable<TSource> source, Random random)
+            where TSource : notnull
+            => (await source.ToListAsync().ConfigureAwait(false)).ToRandomList(random);
+
+        [Pure]
+        public static async ValueTask<IEnumerable<TSource>> Shuffle<TSource>(this IAsyncEnumerable<TSource> source, CancellationToken cancellationToken)
+            where TSource : notnull
+            => (await source.ToListAsync(cancellationToken).ConfigureAwait(false)).ToRandomList(new Random());
+
+        [Pure]
+        public static async ValueTask<IEnumerable<TSource>> Shuffle<TSource>(this IAsyncEnumerable<TSource> source, Random random, CancellationToken cancellationToken)
+            where TSource : notnull
+            => (await source.ToListAsync(cancellationToken).ConfigureAwait(false)).ToRandomList(random);
     }
 }
