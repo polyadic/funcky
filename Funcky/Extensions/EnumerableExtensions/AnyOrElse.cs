deef0b805f150e214b101296cf4fd736e28c69c7
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-02-19 12:45:45 +0100

    Add overload with Func as fallback

--- Funcky/Extensions/EnumerableExtensions/AnyOrElse.cs
+++ Funcky/Extensions/EnumerableExtensions/AnyOrElse.cs
@@ -5,14 +5,19 @@ public static partial class EnumerableExtensions
     /// <summary>Returns the items from <paramref name="source"/> when it has any or otherwise the items from <paramref name="fallback"/>.</summary>
     [Pure]
     public static IEnumerable<TSource> AnyOrElse<TSource>(this IEnumerable<TSource> source, IEnumerable<TSource> fallback)
+        => AnyOrElse(source, () => fallback);
+
+    /// <summary>Returns the items from <paramref name="source"/> when it has any or otherwise the items from <paramref name="fallback"/>.</summary>
+    [Pure]
+    public static IEnumerable<TSource> AnyOrElse<TSource>(this IEnumerable<TSource> source, Func<IEnumerable<TSource>> fallback)
         => source switch
         {
-            IReadOnlyCollection<TSource> collection => collection.Count > 0 ? collection : fallback,
-            ICollection<TSource> collection => collection.Count > 0 ? collection : fallback,
+            IReadOnlyCollection<TSource> collection => collection.Count > 0 ? collection : fallback(),
+            ICollection<TSource> collection => collection.Count > 0 ? collection : fallback(),
             _ => AnyOrElseInternal(source, fallback),
         };
 
-    private static IEnumerable<TSource> AnyOrElseInternal<TSource>(IEnumerable<TSource> source, IEnumerable<TSource> fallback)
+    private static IEnumerable<TSource> AnyOrElseInternal<TSource>(IEnumerable<TSource> source, Func<IEnumerable<TSource>> fallback)
     {
         var hasItems = false;
 
@@ -24,7 +29,7 @@ public static partial class EnumerableExtensions
 
         if (!hasItems)
         {
-            foreach (var item in fallback)
+            foreach (var item in fallback())
             {
                 yield return item;
             }
