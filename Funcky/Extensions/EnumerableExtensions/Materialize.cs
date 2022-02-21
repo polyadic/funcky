5e264b84be443ad3df6ec391c216c46aa16005f9
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-02-21 16:29:03 +0100

    Return an IReadOnlyCollection from Materialize

--- Funcky/Extensions/EnumerableExtensions/Materialize.cs
+++ Funcky/Extensions/EnumerableExtensions/Materialize.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Immutable;
 
 namespace Funcky.Extensions
@@ -10,7 +11,7 @@ namespace Funcky.Extensions
         /// <typeparam name="TItem">Type of the items in the source sequence.</typeparam>
         /// <param name="source">The source sequence can be any <see cref="IEnumerable{TItem}" />.</param>
         /// <returns>A collection of the enumerated items.</returns>
-        public static IEnumerable<TItem> Materialize<TItem>(this IEnumerable<TItem> source)
+        public static IReadOnlyCollection<TItem> Materialize<TItem>(this IEnumerable<TItem> source)
             => source.Materialize(DefaultMaterialization);
 
         /// <summary>
@@ -22,15 +23,66 @@ namespace Funcky.Extensions
         /// <param name="source">The source sequence can be any <see cref="IEnumerable{TItem}" />.</param>
         /// <param name="materialize">A function which materializes a given sequence into a collection.</param>
         /// <returns>A collection of the enumerated items.</returns>
-        public static IEnumerable<TItem> Materialize<TItem, TMaterialization>(
+        public static IReadOnlyCollection<TItem> Materialize<TItem, TMaterialization>(
             this IEnumerable<TItem> source,
             Func<IEnumerable<TItem>, TMaterialization> materialize)
-            where TMaterialization : IEnumerable<TItem>
-            => source is IReadOnlyCollection<TItem> or ICollection<TItem>
-                ? source
-                : materialize(source);
+            where TMaterialization : IReadOnlyCollection<TItem>
+            => source switch
+            {
+                IReadOnlyCollection<TItem> readOnlyCollection => readOnlyCollection,
+                IList<TItem> list => new ListAsReadOnlyCollectionProxy<TItem>(list),
+                ICollection<TItem> collection => new CollectionAsReadOnlyCollectionProxy<TItem>(collection),
+                _ => materialize(source),
+            };
 
-        private static ImmutableList<TItem> DefaultMaterialization<TItem>(IEnumerable<TItem> source)
+        private static IReadOnlyCollection<TItem> DefaultMaterialization<TItem>(IEnumerable<TItem> source)
             => source.ToImmutableList();
+
+        private class CollectionAsReadOnlyCollectionProxy<T> : ICollection<T>, IReadOnlyCollection<T>
+        {
+            private readonly ICollection<T> _collection;
+
+            public CollectionAsReadOnlyCollectionProxy(ICollection<T> collection) => _collection = collection;
+
+            int ICollection<T>.Count => _collection.Count;
+
+            bool ICollection<T>.IsReadOnly => _collection.IsReadOnly;
+
+            int IReadOnlyCollection<T>.Count => _collection.Count;
+
+            public IEnumerator<T> GetEnumerator() => _collection.GetEnumerator();
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+            void ICollection<T>.Add(T item) => _collection.Add(item);
+
+            void ICollection<T>.Clear() => _collection.Clear();
+
+            bool ICollection<T>.Contains(T item) => _collection.Contains(item);
+
+            void ICollection<T>.CopyTo(T[] array, int arrayIndex) => _collection.CopyTo(array, arrayIndex);
+
+            bool ICollection<T>.Remove(T item) => _collection.Remove(item);
+        }
+
+        private sealed class ListAsReadOnlyCollectionProxy<T> : CollectionAsReadOnlyCollectionProxy<T>, IList<T>
+        {
+            private readonly IList<T> _list;
+
+            public ListAsReadOnlyCollectionProxy(IList<T> list)
+                : base(list) => _list = list;
+
+            public T this[int index]
+            {
+                get => _list[index];
+                set => _list[index] = value;
+            }
+
+            public int IndexOf(T item) => _list.IndexOf(item);
+
+            public void Insert(int index, T item) => _list.Insert(index, item);
+
+            public void RemoveAt(int index) => _list.RemoveAt(index);
+        }
     }
 }
