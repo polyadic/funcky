be723633c196efb9688a89252a5fcdd66721b56e
Thomas Bruderer <apophis@apophis.ch>
2022-02-21 10:26:36 +0100

    Add Tests for a given random Distribution for Shuffle

--- Funcky.Test/Extensions/EnumerableExtensions/ShuffleTest.cs
+++ Funcky.Test/Extensions/EnumerableExtensions/ShuffleTest.cs
@@ -15,6 +15,14 @@ namespace Funcky.Test.Extensions.EnumerableExtensions
             Assert.Throws<XunitException>(() => doNotEnumerate.Shuffle());
         }
 
+        [Fact]
+        public void AShuffleWithASpecificRandomDistributionAlwaysReturnsTheSameShuffle()
+        {
+            var source = Enumerable.Range(0, 16);
+
+            Assert.Equal(Sequence.Return(3, 2, 6, 15, 14, 0, 5, 8, 11, 7, 9, 12, 1, 13, 10, 4), source.Shuffle(new System.Random(1337)));
+        }
+
         [Property]
         public Property AShuffleHasTheSameElementsAsTheSource(List<int> source)
             => source
