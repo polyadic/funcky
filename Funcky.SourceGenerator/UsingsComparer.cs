69bf6007a397526b70e1a62df0991326d5872429
Thomas <apophis@apophis.ch>
2022-02-21 12:19:24 +0100

    Update Funcky.SourceGenerator/UsingsComparer.cs
    Co-authored-by: Ruben <4602612+bash@users.noreply.github.com>

--- Funcky.SourceGenerator/UsingsComparer.cs
+++ Funcky.SourceGenerator/UsingsComparer.cs
@@ -5,7 +5,7 @@ namespace Funcky.SourceGenerator;
 internal class UsingsComparer : IEqualityComparer<UsingDirectiveSyntax>
 {
     public bool Equals(UsingDirectiveSyntax left, UsingDirectiveSyntax right)
-    => left.Name.ToFullString() == right.Name.ToFullString();
+        => left.Name.ToFullString() == right.Name.ToFullString();
 
     public int GetHashCode(UsingDirectiveSyntax usingDirectiveSyntax)
     => usingDirectiveSyntax.Name.ToFullString().GetHashCode();
