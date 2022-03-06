11a274a71996f4bce3efa312dc7415b93fbee788
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-03-06 11:14:04 +0100

    Make TryGetValue public

--- Funcky/Monads/Option/Option.Internal.cs
+++ Funcky/Monads/Option/Option.Internal.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Funcky.Monads
 {
     public readonly partial struct Option<TItem>
     {
-        internal bool TryGetValue(
+        /// <summary>Extracts the value out of this option. This function should be used as a last resort.</summary>
+        /// <remarks>The only allowed uses of this function are as part of a loop condition in an iterator or as part of a catch filter clause (<c>catch ... when</c>).
+        /// All other uses will result in a <c>λ0001: Disallowed use of TryGetValue</c> error.</remarks>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public bool TryGetValue(
             [MaybeNullWhen(false)]
             out TItem item)
         {
