bf09aae47c7e99f20533710fb15b5d39d23e85d3
Thomas Bruderer <apophis@apophis.ch>
2022-04-04 11:00:26 +0200

    Fix namespaces for some tests

--- Funcky.Async.Test/Extensions/AsyncEnumerableExtensions/ScanTest.cs
+++ Funcky.Async.Test/Extensions/AsyncEnumerableExtensions/ScanTest.cs
@@ -4,80 +4,83 @@ using Funcky.Async.Extensions;
 using Funcky.Async.Test.TestUtilities;
 using Xunit;
 
-public class ScanTest
+namespace Funcky.Async.Test.Extensions.AsyncEnumerableExtensions
 {
-    [Fact]
-    public void InclusiveScanIsEnumeratedLazily()
+    public class ScanTest
     {
-        var doNotEnumerate = new FailOnEnumerateAsyncSequence<int>();
-
-        _ = doNotEnumerate.InclusiveScan(0, AddElement);
-    }
-
-    [Property]
-    public Property InclusiveScanOnAnEmptyListReturnsAnEmptyList(int neutral, Func<int, int, int> func)
-    {
-        var empty = AsyncEnumerable.Empty<int>();
-
-        return empty.InclusiveScan(neutral, func).NoneAsync().Result.ToProperty();
-    }
-
-    [Property]
-    public Property InclusiveScanCalculatesInclusivePrefixSum(int neutralElement, List<int> numbers)
-        => InclusiveScanCheck(neutralElement, numbers.ToAsyncEnumerable())
-            .Result
-            .ToProperty();
-
-    [Fact]
-    public void ExclusiveScanIsEnumeratedLazily()
-    {
-        var doNotEnumerate = new FailOnEnumerateAsyncSequence<int>();
-
-        _ = doNotEnumerate.ExclusiveScan(0, AddElement);
-    }
-
-    [Property]
-    public Property ExclusiveScanOnAnEmptyListReturnsAnEmptyList(int neutral, Func<int, int, int> func)
-    {
-        var empty = AsyncEnumerable.Empty<int>();
-
-        return empty.ExclusiveScan(neutral, func).NoneAsync().Result.ToProperty();
-    }
-
-    [Property]
-    public Property ExclusiveScanCalculatesInclusivePrefixSum(int neutralElement, List<int> numbers)
-        => ExclusiveScanCheck(neutralElement, numbers.ToAsyncEnumerable())
-            .Result
-            .ToProperty();
-
-    private static async Task<bool> InclusiveScanCheck(int neutralElement, IAsyncEnumerable<int> numbers)
-    {
-        var result = true;
-        var prefixSum = neutralElement;
-
-        await foreach (var (element, inclusiveSum) in numbers.Zip(numbers.InclusiveScan(neutralElement, AddElement)))
+        [Fact]
+        public void InclusiveScanIsEnumeratedLazily()
         {
-            prefixSum = AddElement(prefixSum, element);
-            result = result && inclusiveSum == prefixSum;
+            var doNotEnumerate = new FailOnEnumerateAsyncSequence<int>();
+
+            _ = doNotEnumerate.InclusiveScan(0, AddElement);
         }
 
-        return result;
-    }
-
-    private static async Task<bool> ExclusiveScanCheck(int neutralElement, IAsyncEnumerable<int> numbers)
-    {
-        var result = true;
-        var prefixSum = neutralElement;
-
-        await foreach (var (element, inclusiveSum) in numbers.Zip(numbers.InclusiveScan(neutralElement, AddElement)))
+        [Property]
+        public Property InclusiveScanOnAnEmptyListReturnsAnEmptyList(int neutral, Func<int, int, int> func)
         {
-            prefixSum = AddElement(prefixSum, element);
-            result = result && inclusiveSum == prefixSum;
+            var empty = AsyncEnumerable.Empty<int>();
+
+            return empty.InclusiveScan(neutral, func).NoneAsync().Result.ToProperty();
         }
 
-        return result;
-    }
+        [Property]
+        public Property InclusiveScanCalculatesInclusivePrefixSum(int neutralElement, List<int> numbers)
+            => InclusiveScanCheck(neutralElement, numbers.ToAsyncEnumerable())
+                .Result
+                .ToProperty();
 
-    private static int AddElement(int sum, int element)
-        => sum + element;
+        [Fact]
+        public void ExclusiveScanIsEnumeratedLazily()
+        {
+            var doNotEnumerate = new FailOnEnumerateAsyncSequence<int>();
+
+            _ = doNotEnumerate.ExclusiveScan(0, AddElement);
+        }
+
+        [Property]
+        public Property ExclusiveScanOnAnEmptyListReturnsAnEmptyList(int neutral, Func<int, int, int> func)
+        {
+            var empty = AsyncEnumerable.Empty<int>();
+
+            return empty.ExclusiveScan(neutral, func).NoneAsync().Result.ToProperty();
+        }
+
+        [Property]
+        public Property ExclusiveScanCalculatesInclusivePrefixSum(int neutralElement, List<int> numbers)
+            => ExclusiveScanCheck(neutralElement, numbers.ToAsyncEnumerable())
+                .Result
+                .ToProperty();
+
+        private static async Task<bool> InclusiveScanCheck(int neutralElement, IAsyncEnumerable<int> numbers)
+        {
+            var result = true;
+            var prefixSum = neutralElement;
+
+            await foreach (var (element, inclusiveSum) in numbers.Zip(numbers.InclusiveScan(neutralElement, AddElement)))
+            {
+                prefixSum = AddElement(prefixSum, element);
+                result = result && inclusiveSum == prefixSum;
+            }
+
+            return result;
+        }
+
+        private static async Task<bool> ExclusiveScanCheck(int neutralElement, IAsyncEnumerable<int> numbers)
+        {
+            var result = true;
+            var prefixSum = neutralElement;
+
+            await foreach (var (element, inclusiveSum) in numbers.Zip(numbers.InclusiveScan(neutralElement, AddElement)))
+            {
+                prefixSum = AddElement(prefixSum, element);
+                result = result && inclusiveSum == prefixSum;
+            }
+
+            return result;
+        }
+
+        private static int AddElement(int sum, int element)
+            => sum + element;
+    }
 }
