b912ca950683982d979f99af4820ab2fc9416634
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-04-16 18:26:14 +0200

    Add analyzer with fix that guides people towards Option.None<T>

--- Funcky.Analyzers/Funcky.BuiltinAnalyzers/WellKnownMemberNames.cs
+++ Funcky.Analyzers/Funcky.BuiltinAnalyzers/WellKnownMemberNames.cs
@@ -5,5 +5,11 @@ internal static class WellKnownMemberNames
     public static class OptionOfT
     {
         public const string TryGetValue = "TryGetValue";
+        public const string None = "None";
+    }
+
+    public static class Option
+    {
+        public const string None = "None";
     }
 }
