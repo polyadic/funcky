02995e678e798c47e7f6b8d603300d7fe87a88bc
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-04-17 12:36:35 +0200

    Apply code fixes

--- Funcky/Sequence/Sequence.Return.cs
+++ Funcky/Sequence/Sequence.Return.cs
@@ -1,8 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Funcky
 {
     public static partial class Sequence
     {
         [Pure]
+        [SuppressMessage("Funcky", "λ1001:Use of Enumerable.Repeat for a single element", Justification = "This is the implementation of Sequence.Return")]
         public static IEnumerable<TItem> Return<TItem>(TItem item)
             => Enumerable.Repeat(item, 1);
 
