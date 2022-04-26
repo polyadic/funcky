67854c823574c07223b045a7609cbb6b3540bdd2
Thomas Bruderer <apophis@apophis.ch>
2022-04-26 07:11:46 +0200

    Fix file-scoped namespaces

--- Funcky/Extensions/EnumerableExtensions/Memoize.cs
+++ Funcky/Extensions/EnumerableExtensions/Memoize.cs
@@ -1,91 +1,90 @@
 using System.Collections;
 
-namespace Funcky.Extensions
-{
-    public static partial class EnumerableExtensions
-    {
-        /// <summary>
-        /// Creates a buffer with a view over the source sequence, causing each enumerator to obtain access to all of the
-        /// sequence's elements without causing multiple enumerations over the source.
-        /// </summary>
-        /// <typeparam name="TSource">Type of the elements in <paramref name="sequence"/> sequence.</typeparam>
-        /// <param name="sequence">The source sequence.</param>
-        /// <returns>A lazy buffer of the underlying sequence.</returns>
-        [Pure]
-        public static IBuffer<TSource> Memoize<TSource>(this IEnumerable<TSource> sequence)
-            where TSource : notnull
-            => sequence is IBuffer<TSource> buffer
-                ? buffer
-                : MemoizedBuffer.Create(sequence);
+namespace Funcky.Extensions;
 
-        private static class MemoizedBuffer
+public static partial class EnumerableExtensions
+{
+    /// <summary>
+    /// Creates a buffer with a view over the source sequence, causing each enumerator to obtain access to all of the
+    /// sequence's elements without causing multiple enumerations over the source.
+    /// </summary>
+    /// <typeparam name="TSource">Type of the elements in <paramref name="sequence"/> sequence.</typeparam>
+    /// <param name="sequence">The source sequence.</param>
+    /// <returns>A lazy buffer of the underlying sequence.</returns>
+    [Pure]
+    public static IBuffer<TSource> Memoize<TSource>(this IEnumerable<TSource> sequence)
+        where TSource : notnull
+        => sequence is IBuffer<TSource> buffer
+            ? buffer
+            : MemoizedBuffer.Create(sequence);
+
+    private static class MemoizedBuffer
+    {
+        public static MemoizedBuffer<TSource> Create<TSource>(IEnumerable<TSource> source)
+            => new(source);
+    }
+
+    private sealed class MemoizedBuffer<T> : IBuffer<T>
+    {
+        private readonly List<T> _buffer = new();
+        private readonly IEnumerator<T> _source;
+
+        private bool _disposed;
+
+        public MemoizedBuffer(IEnumerable<T> source)
+            => _source = source.GetEnumerator();
+
+        public IEnumerator<T> GetEnumerator()
         {
-            public static MemoizedBuffer<TSource> Create<TSource>(IEnumerable<TSource> source)
-                => new(source);
+            ThrowIfDisposed();
+
+            return GetEnumeratorInternal();
         }
 
-        private sealed class MemoizedBuffer<T> : IBuffer<T>
+        IEnumerator IEnumerable.GetEnumerator()
         {
-            private readonly List<T> _buffer = new();
-            private readonly IEnumerator<T> _source;
+            ThrowIfDisposed();
 
-            private bool _disposed;
+            return GetEnumeratorInternal();
+        }
 
-            public MemoizedBuffer(IEnumerable<T> source)
-                => _source = source.GetEnumerator();
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _source.Dispose();
+                _buffer.Clear();
+                _disposed = true;
+            }
+        }
 
-            public IEnumerator<T> GetEnumerator()
+        private IEnumerator<T> GetEnumeratorInternal()
+        {
+            for (var index = 0; true; index++)
             {
                 ThrowIfDisposed();
 
-                return GetEnumeratorInternal();
-            }
-
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                ThrowIfDisposed();
-
-                return GetEnumeratorInternal();
-            }
-
-            public void Dispose()
-            {
-                if (!_disposed)
+                if (index == _buffer.Count)
                 {
-                    _source.Dispose();
-                    _buffer.Clear();
-                    _disposed = true;
-                }
-            }
-
-            private IEnumerator<T> GetEnumeratorInternal()
-            {
-                for (var index = 0; true; index++)
-                {
-                    ThrowIfDisposed();
-
-                    if (index == _buffer.Count)
+                    if (_source.MoveNext())
                     {
-                        if (_source.MoveNext())
-                        {
-                            _buffer.Add(_source.Current);
-                        }
-                        else
-                        {
-                            break;
-                        }
+                        _buffer.Add(_source.Current);
                     }
-
-                    yield return _buffer[index];
+                    else
+                    {
+                        break;
+                    }
                 }
+
+                yield return _buffer[index];
             }
+        }
 
-            private void ThrowIfDisposed()
+        private void ThrowIfDisposed()
+        {
+            if (_disposed)
             {
-                if (_disposed)
-                {
-                    throw new ObjectDisposedException(nameof(MemoizedBuffer));
-                }
+                throw new ObjectDisposedException(nameof(MemoizedBuffer));
             }
         }
     }
--- Funcky/IBuffer.cs
+++ Funcky/IBuffer.cs
@@ -1,10 +1,9 @@
-namespace Funcky
+namespace Funcky;
+
+/// <summary>
+/// Represents a buffer of an underlying <see cref="IEnumerable{TItem}"/> resource and is <see cref="IDisposable"/> accordingly.
+/// </summary>
+/// <typeparam name="TSource">Element type.</typeparam>
+public interface IBuffer<out TSource> : IEnumerable<TSource>, IDisposable
 {
-    /// <summary>
-    /// Represents a buffer of an underlying <see cref="IEnumerable{TItem}"/> resource and is <see cref="IDisposable"/> accordingly.
-    /// </summary>
-    /// <typeparam name="TSource">Element type.</typeparam>
-    public interface IBuffer<out TSource> : IEnumerable<TSource>, IDisposable
-    {
-    }
 }
