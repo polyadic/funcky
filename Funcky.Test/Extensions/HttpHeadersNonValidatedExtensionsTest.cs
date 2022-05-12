4692f2e1ccc3865fc485811150d3e8a8988e70ce
Thomas Bruderer <apophis@apophis.ch>
2022-05-12 17:45:26 +0200

    Implement `GetValuesOrNone` extension on `HttpHeadersNonValidated`

---
+++ Funcky.Test/Extensions/HttpHeadersNonValidatedExtensionsTest.cs
@@ -0,0 +1,20 @@
+#if HTTP_HEADERS_NON_VALIDATED
+using System.Net.Http.Headers;
+using FsCheck;
+using FsCheck.Xunit;
+
+namespace Funcky.Test.Extensions;
+public sealed class HttpHeadersNonValidatedExtensionsTest
+{
+    [Property]
+    public Property InAnEmptyHttpHeadersNonValidatedGetValueOrNoneIsAlwaysNone(string header)
+    {
+        var nonValidated = default(HttpHeadersNonValidated);
+
+        return nonValidated
+            .GetValuesOrNone(header)
+            .Match(none: true, some: False)
+            .ToProperty();
+    }
+}
+#endif
