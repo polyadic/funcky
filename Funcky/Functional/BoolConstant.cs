6f0b518dfe24be6dc81147e69967cef0c7945a31
Thomas Bruderer <apophis@apophis.ch>
2022-05-27 08:26:35 +0200

    Activate aggressive inling on True and False

--- Funcky/Functional/BoolConstant.cs
+++ Funcky/Functional/BoolConstant.cs
@@ -1,44 +1,56 @@
+using System.Runtime.CompilerServices;
+
 namespace Funcky;
 
 public static partial class Functional
 {
     /// <summary>A function that always returns <see langword="true"/>.</summary>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool True() => true;
 
     /// <inheritdoc cref="True"/>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool True<T1>(T1 ω1) => true;
 
     /// <inheritdoc cref="True"/>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool True<T1, T2>(T1 ω1, T2 ω2) => true;
 
     /// <inheritdoc cref="True"/>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool True<T1, T2, T3>(T1 ω1, T2 ω2, T3 ω3) => true;
 
     /// <inheritdoc cref="True"/>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool True<T1, T2, T3, T4>(T1 ω1, T2 ω2, T3 ω3, T4 ω4) => true;
 
     /// <summary>A function that always returns <see langword="false"/>.</summary>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool False() => false;
 
     /// <inheritdoc cref="False"/>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool False<T1>(T1 ω1) => false;
 
     /// <inheritdoc cref="False"/>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool False<T1, T2>(T1 ω1, T2 ω2) => false;
 
     /// <inheritdoc cref="False"/>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool False<T1, T2, T3>(T1 ω1, T2 ω2, T3 ω3) => false;
 
     /// <inheritdoc cref="False"/>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool False<T1, T2, T3, T4>(T1 ω1, T2 ω2, T3 ω3, T4 ω4) => false;
 }
