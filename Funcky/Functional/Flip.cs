d6bcea17db73790374df5cfd8185dadcc278e10b
Thomas Bruderer <apophis@apophis.ch>
2022-05-27 08:26:42 +0200

    Activate aggressive inling on Flip

--- Funcky/Functional/Flip.cs
+++ Funcky/Functional/Flip.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 namespace Funcky;
 
 public static partial class Functional
@@ -6,6 +8,7 @@ public static partial class Functional
     /// Flips the first two arguments of the function.
     /// </summary>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Func<T2, T1, TResult> Flip<T1, T2, TResult>(Func<T1, T2, TResult> function)
         => (p1, p2) => function(p2, p1);
 
@@ -13,6 +16,7 @@ public static partial class Functional
     /// Flips the first two arguments of the function.
     /// </summary>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Func<T2, T1, T3, TResult> Flip<T1, T2, T3, TResult>(Func<T1, T2, T3, TResult> function)
         => (p1, p2, p3) => function(p2, p1, p3);
 
@@ -20,6 +24,7 @@ public static partial class Functional
     /// Flips the first two arguments of the function.
     /// </summary>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Func<T2, T1, T3, T4, TResult> Flip<T1, T2, T3, T4, TResult>(Func<T1, T2, T3, T4, TResult> function)
         => (p1, p2, p3, p4) => function(p2, p1, p3, p4);
 
@@ -27,6 +32,7 @@ public static partial class Functional
     /// Flips the first two arguments of the function.
     /// </summary>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Func<T2, T1, T3, T4, T5, TResult> Flip<T1, T2, T3, T4, T5, TResult>(Func<T1, T2, T3, T4, T5, TResult> function)
         => (p1, p2, p3, p4, p5) => function(p2, p1, p3, p4, p5);
 
@@ -34,6 +40,7 @@ public static partial class Functional
     /// Flips the first two arguments of the function.
     /// </summary>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Func<T2, T1, T3, T4, T5, T6, TResult> Flip<T1, T2, T3, T4, T5, T6, TResult>(Func<T1, T2, T3, T4, T5, T6, TResult> function)
         => (p1, p2, p3, p4, p5, p6) => function(p2, p1, p3, p4, p5, p6);
 
@@ -41,6 +48,7 @@ public static partial class Functional
     /// Flips the first two arguments of the function.
     /// </summary>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Func<T2, T1, T3, T4, T5, T6, T7, TResult> Flip<T1, T2, T3, T4, T5, T6, T7, TResult>(Func<T1, T2, T3, T4, T5, T6, T7, TResult> function)
         => (p1, p2, p3, p4, p5, p6, p7) => function(p2, p1, p3, p4, p5, p6, p7);
 
@@ -48,6 +56,7 @@ public static partial class Functional
     /// Flips the first two arguments of the function.
     /// </summary>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Func<T2, T1, T3, T4, T5, T6, T7, T8, TResult> Flip<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> function)
         => (p1, p2, p3, p4, p5, p6, p7, p8) => function(p2, p1, p3, p4, p5, p6, p7, p8);
 
@@ -55,6 +64,7 @@ public static partial class Functional
     /// Flips the first two arguments of the function.
     /// </summary>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Action<T2, T1> Flip<T1, T2>(Action<T1, T2> function)
         => (p1, p2) => function(p2, p1);
 
@@ -62,6 +72,7 @@ public static partial class Functional
     /// Flips the first two arguments of the function.
     /// </summary>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Action<T2, T1, T3> Flip<T1, T2, T3>(Action<T1, T2, T3> action)
         => (p1, p2, p3) => action(p2, p1, p3);
 
@@ -69,6 +80,7 @@ public static partial class Functional
     /// Flips the first two arguments of the action.
     /// </summary>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Action<T2, T1, T3, T4> Flip<T1, T2, T3, T4>(Action<T1, T2, T3, T4> action)
         => (p1, p2, p3, p4) => action(p2, p1, p3, p4);
 
@@ -76,6 +88,7 @@ public static partial class Functional
     /// Flips the first two arguments of the action.
     /// </summary>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Action<T2, T1, T3, T4, T5> Flip<T1, T2, T3, T4, T5>(Action<T1, T2, T3, T4, T5> action)
         => (p1, p2, p3, p4, p5) => action(p2, p1, p3, p4, p5);
 
@@ -83,6 +96,7 @@ public static partial class Functional
     /// Flips the first two arguments of the action.
     /// </summary>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Action<T2, T1, T3, T4, T5, T6> Flip<T1, T2, T3, T4, T5, T6>(Action<T1, T2, T3, T4, T5, T6> action)
         => (p1, p2, p3, p4, p5, p6) => action(p2, p1, p3, p4, p5, p6);
 
@@ -90,6 +104,7 @@ public static partial class Functional
     /// Flips the first two arguments of the action.
     /// </summary>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Action<T2, T1, T3, T4, T5, T6, T7> Flip<T1, T2, T3, T4, T5, T6, T7>(Action<T1, T2, T3, T4, T5, T6, T7> action)
         => (p1, p2, p3, p4, p5, p6, p7) => action(p2, p1, p3, p4, p5, p6, p7);
 
@@ -97,6 +112,7 @@ public static partial class Functional
     /// Flips the first two arguments of the action.
     /// </summary>
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Action<T2, T1, T3, T4, T5, T6, T7, T8> Flip<T1, T2, T3, T4, T5, T6, T7, T8>(Action<T1, T2, T3, T4, T5, T6, T7, T8> action)
         => (p1, p2, p3, p4, p5, p6, p7, p8) => action(p2, p1, p3, p4, p5, p6, p7, p8);
 }
