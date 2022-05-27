c48e373d5aeebc1c2649d774adc26de3542cbd4c
Thomas Bruderer <apophis@apophis.ch>
2022-05-27 08:26:50 +0200

    Activate aggressive inling on Identity

--- Funcky/Functional/Identity.cs
+++ Funcky/Functional/Identity.cs
@@ -1,7 +1,10 @@
+using System.Runtime.CompilerServices;
+
 namespace Funcky;
 
 public static partial class Functional
 {
     [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static T Identity<T>(T value) => value;
 }
