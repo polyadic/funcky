8d8962ca90db040c65cb498e38d43d0fd7da43bb
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-06-11 18:25:43 +0200

    Remove `Option.None<T>`

--- Funcky/EitherOrBoth.cs
+++ Funcky/EitherOrBoth.cs
@@ -134,7 +134,7 @@ public static class EitherOrBoth
             left: Left<TLeft, TRight>,
             right: Right<TLeft, TRight>,
             leftAndRight: Both,
-            none: Option.None<EitherOrBoth<TLeft, TRight>>);
+            none: static () => Option<EitherOrBoth<TLeft, TRight>>.None);
 
     private static Option<EitherOrBoth<TLeft, TRight>> Left<TLeft, TRight>(TLeft left)
         where TLeft : notnull
--- Funcky/Monads/Option/Option.Monad.cs
+++ Funcky/Monads/Option/Option.Monad.cs
@@ -6,14 +6,14 @@ public readonly partial struct Option<TItem>
     public Option<TResult> Select<TResult>(Func<TItem, TResult> selector)
         where TResult : notnull
         => Match(
-             none: Option.None<TResult>,
+             none: Option<TResult>.None,
              some: item => selector(item));
 
     [Pure]
     public Option<TResult> SelectMany<TResult>(Func<TItem, Option<TResult>> selector)
         where TResult : notnull
         => Match(
-             none: Option.None<TResult>,
+             none: Option<TResult>.None,
              some: selector);
 
     [Pure]
