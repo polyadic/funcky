c5b59d288231ae3fd1ea0ab3ebd83511716521a0
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-08-15 22:10:24 +0200

    Convert to raw string literals

--- Funcky.SourceGenerator.Test/OrNoneGeneratorSnapshotTests.cs
+++ Funcky.SourceGenerator.Test/OrNoneGeneratorSnapshotTests.cs
@@ -3,40 +3,45 @@ namespace Funcky.SourceGenerator.Test;
 [UsesVerify] // 👈 Adds hooks for Verify into XUnit
 public class OrNoneGeneratorSnapshotTests
 {
-    private const string OptionSource = @"namespace Funcky.Monads
-{
-    public struct Option<TItem> where TItem : notnull
-    {
-        public static implicit operator Option<TItem>(TItem item) => default;
-    }
-}";
+    private const string OptionSource =
+        """
+        namespace Funcky.Monads
+        {
+            public struct Option<TItem> where TItem : notnull
+            {
+                public static implicit operator Option<TItem>(TItem item) => default;
+            }
+        }
+        """;
 
     [Fact]
     public Task GenerateSingleMethodWithTheSingleArgumentCandidate()
     {
-        const string source = @"
-#nullable enable
+        const string source =
+            """
+            #nullable enable
 
-using System.Diagnostics.Contracts;
-using Funcky.Internal;
-using Funcky.Monads;
+            using System.Diagnostics.Contracts;
+            using Funcky.Internal;
+            using Funcky.Monads;
 
-namespace Funcky.Extensions
-{
-    [OrNoneFromTryPattern(typeof(Target), nameof(Target.TryParse))]
-    public static partial class ParseExtensions
-    {
-    }
+            namespace Funcky.Extensions
+            {
+                [OrNoneFromTryPattern(typeof(Target), nameof(Target.TryParse))]
+                public static partial class ParseExtensions
+                {
+                }
 
-    public sealed class Target
-    {
-        public static bool TryParse(string candidate, out Target result)
-        {
-            result = default!;
-            return false;
-        }
-    }
-}";
+                public sealed class Target
+                {
+                    public static bool TryParse(string candidate, out Target result)
+                    {
+                        result = default!;
+                        return false;
+                    }
+                }
+            }
+            """;
 
         return TestHelper.Verify(source + Environment.NewLine + OptionSource);
     }
@@ -44,27 +49,28 @@ namespace Funcky.Extensions
     [Fact]
     public Task GeneratesMethodWhenTargetIsNotNullableAnnotated()
     {
-        const string source = @"
-using System.Diagnostics.Contracts;
-using Funcky.Internal;
-using Funcky.Monads;
+        const string source = """
+            using System.Diagnostics.Contracts;
+            using Funcky.Internal;
+            using Funcky.Monads;
 
-namespace Funcky.Extensions
-{
-    [OrNoneFromTryPattern(typeof(Target), nameof(Target.TryParse))]
-    public static partial class ParseExtensions
-    {
-    }
+            namespace Funcky.Extensions
+            {
+                [OrNoneFromTryPattern(typeof(Target), nameof(Target.TryParse))]
+                public static partial class ParseExtensions
+                {
+                }
 
-    public sealed class Target
-    {
-        public static bool TryParse(string candidate, out Target result)
-        {
-            result = default!;
-            return false;
-        }
-    }
-}";
+                public sealed class Target
+                {
+                    public static bool TryParse(string candidate, out Target result)
+                    {
+                        result = default!;
+                        return false;
+                    }
+                }
+            }
+            """;
 
         return TestHelper.Verify(source + Environment.NewLine + OptionSource);
     }
@@ -72,30 +78,31 @@ namespace Funcky.Extensions
     [Fact]
     public Task GenerateSingleMethodWithMultipleArgumentsToForward()
     {
-        const string source = @"
-#nullable enable
+        const string source = """
+            #nullable enable
 
-using System;
-using System.Diagnostics.Contracts;
-using Funcky.Internal;
-using Funcky.Monads;
+            using System;
+            using System.Diagnostics.Contracts;
+            using Funcky.Internal;
+            using Funcky.Monads;
 
-namespace Funcky.Extensions
-{
-    [OrNoneFromTryPattern(typeof(Target), nameof(Target.TryParse))]
-    public static partial class ParseExtensions
-    {
-    }
+            namespace Funcky.Extensions
+            {
+                [OrNoneFromTryPattern(typeof(Target), nameof(Target.TryParse))]
+                public static partial class ParseExtensions
+                {
+                }
 
-    public sealed class Target
-    {
-        public static bool TryParse(string candidate, bool caseSensitive, IFormatProvider provider, out Target result)
-        {
-            result = default!;
-            return false;
-        }
-    }
-}";
+                public sealed class Target
+                {
+                    public static bool TryParse(string candidate, bool caseSensitive, IFormatProvider provider, out Target result)
+                    {
+                        result = default!;
+                        return false;
+                    }
+                }
+            }
+            """;
 
         return TestHelper.Verify(source + Environment.NewLine + OptionSource);
     }
@@ -103,36 +110,37 @@ namespace Funcky.Extensions
     [Fact]
     public Task GeneratesAllOverloadsForAGivenMethod()
     {
-        const string source = @"
-#nullable enable
+        const string source = """
+            #nullable enable
 
-using System;
-using System.Diagnostics.Contracts;
-using Funcky.Internal;
-using Funcky.Monads;
+            using System;
+            using System.Diagnostics.Contracts;
+            using Funcky.Internal;
+            using Funcky.Monads;
 
-namespace Funcky.Extensions
-{
-    [OrNoneFromTryPattern(typeof(Target), nameof(Target.TryParse))]
-    public static partial class ParseExtensions
-    {
-    }
+            namespace Funcky.Extensions
+            {
+                [OrNoneFromTryPattern(typeof(Target), nameof(Target.TryParse))]
+                public static partial class ParseExtensions
+                {
+                }
 
-    public sealed class Target
-    {
-        public static bool TryParse(string candidate, out Target result)
-        {
-            result = default!;
-            return false;
-        }
+                public sealed class Target
+                {
+                    public static bool TryParse(string candidate, out Target result)
+                    {
+                        result = default!;
+                        return false;
+                    }
 
-        public static bool TryParse(string candidate, bool caseSensitive, out Target result)
-        {
-            result = default!;
-            return false;
-        }
-    }
-}";
+                    public static bool TryParse(string candidate, bool caseSensitive, out Target result)
+                    {
+                        result = default!;
+                        return false;
+                    }
+                }
+            }
+            """;
 
         return TestHelper.Verify(source + Environment.NewLine + OptionSource);
     }
@@ -140,37 +148,38 @@ namespace Funcky.Extensions
     [Fact]
     public Task GenerateMultipleMethodsInASingleClass()
     {
-        const string source = @"
-#nullable enable
+        const string source = """
+            #nullable enable
 
-using System;
-using System.Diagnostics.Contracts;
-using Funcky.Internal;
-using Funcky.Monads;
+            using System;
+            using System.Diagnostics.Contracts;
+            using Funcky.Internal;
+            using Funcky.Monads;
 
-namespace Funcky.Extensions
-{
-    [OrNoneFromTryPattern(typeof(Target), nameof(Target.TryParse))]
-    [OrNoneFromTryPattern(typeof(Target), nameof(Target.TryParseExact))]
-    public static partial class ParseExtensions
-    {
-    }
+            namespace Funcky.Extensions
+            {
+                [OrNoneFromTryPattern(typeof(Target), nameof(Target.TryParse))]
+                [OrNoneFromTryPattern(typeof(Target), nameof(Target.TryParseExact))]
+                public static partial class ParseExtensions
+                {
+                }
 
-    public sealed class Target
-    {
-        public static bool TryParse(string candidate, out Target result)
-        {
-            result = default!;
-            return false;
-        }
+                public sealed class Target
+                {
+                    public static bool TryParse(string candidate, out Target result)
+                    {
+                        result = default!;
+                        return false;
+                    }
 
-        public static bool TryParseExact(string candidate, out Target result)
-        {
-            result = default!;
-            return false;
-        }
-    }
-}";
+                    public static bool TryParseExact(string candidate, out Target result)
+                    {
+                        result = default!;
+                        return false;
+                    }
+                }
+            }
+            """;
 
         return TestHelper.Verify(source + Environment.NewLine + OptionSource);
     }
