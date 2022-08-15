c26a3d533d90e4d497d6da6f6b70a253766ea79e
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-08-15 21:52:58 +0200

    Use raw string literal to store attribute source

--- Funcky.SourceGenerator.Test/OrNoneGeneratorSnapshotTests.GenerateMultipleMethodsInASingleClass.01.verified.cs
+++ Funcky.SourceGenerator.Test/OrNoneGeneratorSnapshotTests.GenerateMultipleMethodsInASingleClass.01.verified.cs
@@ -1,16 +1,14 @@
 ﻿//HintName: OrNoneFromTryPatternAttribute.g.cs
-using System;
-
 namespace Funcky.Internal
 {
     [global::System.Diagnostics.Conditional("COMPILE_TIME_ONLY")]
     [global::System.AttributeUsage(global::System.AttributeTargets.Class, AllowMultiple = true)]
-    internal class OrNoneFromTryPatternAttribute : Attribute
+    internal class OrNoneFromTryPatternAttribute : global::System.Attribute
     {
-        public OrNoneFromTryPatternAttribute(Type type, string method)
+        public OrNoneFromTryPatternAttribute(global::System.Type type, string method)
             => (Type, Method) = (type, method);
 
-        public Type Type { get; }
+        public global::System.Type Type { get; }
 
         public string Method { get; }
     }
