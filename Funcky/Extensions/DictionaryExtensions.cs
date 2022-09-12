b627eddc15a8023d80a5d6dfc02ece593a60507c
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-09-12 17:35:35 +0200

    Drop `notnull` constraint for newer target frameworks
    Co-authored-by: Thomas Bruderer <apophis@apophis.ch>

--- Funcky/Extensions/DictionaryExtensions.cs
+++ Funcky/Extensions/DictionaryExtensions.cs
@@ -4,7 +4,11 @@ public static partial class DictionaryExtensions
 {
     [Pure]
     public static Option<TValue> GetValueOrNone<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+#if NETCOREAPP3_1
+        // TKey was constraint to notnull when nullability annotations were originally added. It was later dropped again.
+        // See: https://github.com/dotnet/runtime/issues/31401
         where TKey : notnull
+#endif
         where TValue : notnull
         => dictionary.TryGetValue(key, out var result)
             ? result
@@ -12,7 +16,11 @@ public static partial class DictionaryExtensions
 
     [Pure]
     public static Option<TValue> GetValueOrNone<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary, TKey readOnlyKey)
+#if NETCOREAPP3_1
+        // TKey was constraint to notnull when nullability annotations were originally added. It was later dropped again.
+        // See: https://github.com/dotnet/runtime/issues/31401
         where TKey : notnull
+#endif
         where TValue : notnull
         => dictionary.TryGetValue(readOnlyKey, out var result)
             ? result
