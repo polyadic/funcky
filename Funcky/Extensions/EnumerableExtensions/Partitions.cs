cfb5e9cb0af3a9eba230ca1437b56e55e47257d2
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-09-12 16:01:13 +0200

    Rename parameter names for Partitions
    Co-authored-by: Thomas Bruderer <apophis@apophis.ch>

--- Funcky/Extensions/EnumerableExtensions/Partitions.cs
+++ Funcky/Extensions/EnumerableExtensions/Partitions.cs
@@ -2,17 +2,17 @@ namespace Funcky.Extensions;
 
 public static class Partitions
 {
-    public static Partitions<TItem> Create<TItem>(IReadOnlyList<TItem> trues, IReadOnlyList<TItem> falses)
-        => new(trues, falses);
+    public static Partitions<TItem> Create<TItem>(IReadOnlyList<TItem> @true, IReadOnlyList<TItem> @false)
+        => new(@true, @false);
 }
 
 public readonly struct Partitions<TItem>
 {
-    public Partitions(IReadOnlyList<TItem> trues, IReadOnlyList<TItem> falses) => (True, False) = (trues, falses);
+    public Partitions(IReadOnlyList<TItem> @true, IReadOnlyList<TItem> @false) => (True, False) = (@true, @false);
 
     public IReadOnlyList<TItem> True { get; }
 
     public IReadOnlyList<TItem> False { get; }
 
-    public void Deconstruct(out IReadOnlyList<TItem> trues, out IReadOnlyList<TItem> falses) => (trues, falses) = (True, False);
+    public void Deconstruct(out IReadOnlyList<TItem> @true, out IReadOnlyList<TItem> @false) => (@true, @false) = (True, False);
 }
