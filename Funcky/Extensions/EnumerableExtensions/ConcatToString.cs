2c7aa1194dea950a19b42e4b74c8794f4d774c0e
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-09-12 16:01:24 +0200

    Rename to TSource
    Co-authored-by: Thomas Bruderer <apophis@apophis.ch>

--- Funcky/Extensions/EnumerableExtensions/ConcatToString.cs
+++ Funcky/Extensions/EnumerableExtensions/ConcatToString.cs
@@ -5,9 +5,9 @@ public static partial class EnumerableExtensions
     /// <summary>
     /// Concatenates the elements of the given sequence to a single string.
     /// </summary>
-    /// <typeparam name="T">Type of the elements in <paramref name="source"/> sequence.</typeparam>
+    /// <typeparam name="TSource">Type of the elements in <paramref name="source"/> sequence.</typeparam>
     /// <returns>Concatenated string.</returns>
     [Pure]
-    public static string ConcatToString<T>(this IEnumerable<T> source)
+    public static string ConcatToString<TSource>(this IEnumerable<TSource> source)
         => string.Concat(source);
 }
