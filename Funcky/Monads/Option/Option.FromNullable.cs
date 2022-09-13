3141f07817c640547bd1545f002210ac5949d0a0
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-09-13 12:12:33 +0200

    Rename T -> TItem
    Co-authored-by: Thomas Bruderer <apophis@apophis.ch>

--- Funcky/Monads/Option/Option.FromNullable.cs
+++ Funcky/Monads/Option/Option.FromNullable.cs
@@ -6,15 +6,15 @@ public static partial class Option
     /// Creates an <see cref="Option{T}"/> from a nullable value.
     /// </summary>
     [Pure]
-    public static Option<T> FromNullable<T>(T? item)
-        where T : class
-        => item is null ? Option<T>.None : Some(item);
+    public static Option<TItem> FromNullable<TItem>(TItem? item)
+        where TItem : class
+        => item is null ? Option<TItem>.None : Some(item);
 
     /// <summary>
     /// Creates an <see cref="Option{T}"/> from a nullable value.
     /// </summary>
     [Pure]
-    public static Option<T> FromNullable<T>(T? item)
-        where T : struct
-        => item.HasValue ? Some(item.Value) : Option<T>.None;
+    public static Option<TItem> FromNullable<TItem>(TItem? item)
+        where TItem : struct
+        => item.HasValue ? Some(item.Value) : Option<TItem>.None;
 }
