47f0ed2fbb71c11fb6cf1bf4c7a36dfbcb87693f
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-09-13 14:58:26 +0200

    Add cancellation token to `NoneAsync`
    Co-authored-by: Thomas Bruderer <apophis@apophis.ch>

--- Funcky.Async/Extensions/AsyncEnumerableExtensions/None.cs
+++ Funcky.Async/Extensions/AsyncEnumerableExtensions/None.cs
@@ -4,21 +4,21 @@ public static partial class AsyncEnumerableExtensions
 {
     /// <summary>Determines whether a sequence contains no elements.</summary>
     [Pure]
-    public static async ValueTask<bool> NoneAsync<TSource>(this IAsyncEnumerable<TSource> source)
-        => !await source.AnyAsync().ConfigureAwait(false);
+    public static async ValueTask<bool> NoneAsync<TSource>(this IAsyncEnumerable<TSource> source, CancellationToken cancellationToken = default)
+        => !await source.AnyAsync(cancellationToken).ConfigureAwait(false);
 
     /// <summary>Determines whether no element exists or satisfies a condition.</summary>
     [Pure]
-    public static async ValueTask<bool> NoneAsync<TSource>(this IAsyncEnumerable<TSource> source, Func<TSource, bool> predicate)
-        => !await source.AnyAsync(predicate).ConfigureAwait(false);
+    public static async ValueTask<bool> NoneAsync<TSource>(this IAsyncEnumerable<TSource> source, Func<TSource, bool> predicate, CancellationToken cancellationToken = default)
+        => !await source.AnyAsync(predicate, cancellationToken).ConfigureAwait(false);
 
-    /// <inheritdoc cref="NoneAsync{TSource}(IAsyncEnumerable{TSource}, Func{TSource,bool})"/>
+    /// <inheritdoc cref="NoneAsync{TSource}(IAsyncEnumerable{TSource}, Func{TSource,bool}, CancellationToken)"/>
     [Pure]
-    public static async ValueTask<bool> NoneAwaitAsync<TSource>(this IAsyncEnumerable<TSource> source, Func<TSource, ValueTask<bool>> predicate)
-        => !await source.AnyAwaitAsync(predicate).ConfigureAwait(false);
+    public static async ValueTask<bool> NoneAwaitAsync<TSource>(this IAsyncEnumerable<TSource> source, Func<TSource, ValueTask<bool>> predicate, CancellationToken cancellationToken = default)
+        => !await source.AnyAwaitAsync(predicate, cancellationToken).ConfigureAwait(false);
 
-    /// <inheritdoc cref="NoneAsync{TSource}(IAsyncEnumerable{TSource}, Func{TSource,bool})"/>
+    /// <inheritdoc cref="NoneAsync{TSource}(IAsyncEnumerable{TSource}, Func{TSource,bool}, CancellationToken)"/>
     [Pure]
-    public static async ValueTask<bool> NoneAwaitWithCancellationAsync<TSource>(this IAsyncEnumerable<TSource> source, Func<TSource, CancellationToken, ValueTask<bool>> predicate)
-        => !await source.AnyAwaitWithCancellationAsync(predicate).ConfigureAwait(false);
+    public static async ValueTask<bool> NoneAwaitWithCancellationAsync<TSource>(this IAsyncEnumerable<TSource> source, Func<TSource, CancellationToken, ValueTask<bool>> predicate, CancellationToken cancellationToken = default)
+        => !await source.AnyAwaitWithCancellationAsync(predicate, cancellationToken).ConfigureAwait(false);
 }
