45157ee709440d45c473d6940533aaeebbf57655
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-09-13 08:16:39 +0200

    Cleanup converter and fix trimming annotations

--- Funcky/Monads/Option/OptionJsonConverter.cs
+++ Funcky/Monads/Option/OptionJsonConverter.cs
@@ -4,57 +4,39 @@ using System.Text.Json.Serialization;
 
 namespace Funcky.Monads;
 
-/// <remarks>A JSON converter that serializes <see cref="Option{TItem}.None"/> as <c>null</c>.</remarks>
+/// <summary>A JSON converter for <see cref="Option{TItem}"/> that serializes <see cref="Option.Some{TItem}"/> transparently and <see cref="Option{TItem}.None"/> as <c>null</c>.</summary>
 public sealed class OptionJsonConverter : JsonConverterFactory
 {
     public override bool CanConvert(Type typeToConvert)
         => typeToConvert.IsGenericType &&
            typeToConvert.GetGenericTypeDefinition() == typeof(Option<>);
 
+    [RequiresUnreferencedCode("JSON serialization and deserialization might require types that cannot be statically analyzed.")]
+    [UnconditionalSuppressMessage("Trimming", "IL2046:\'RequiresUnreferencedCodeAttribute\' annotations must match across all interface implementations or overrides.")]
     public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
     {
-        var converterType = typeof(OptionJsonConverter<>).MakeGenericType(typeToConvert.GetGenericArguments().Single());
-        return (JsonConverter)Activator.CreateInstance(converterType, options)!;
+        var itemType = typeToConvert.GetGenericArguments().Single();
+        var converterType = typeof(OptionJsonConverter<>).MakeGenericType(itemType);
+        var itemConverter = options.GetConverter(itemType);
+        return (JsonConverter)Activator.CreateInstance(converterType, itemConverter)!;
     }
 }
 
 internal sealed class OptionJsonConverter<TItem> : JsonConverter<Option<TItem>>
     where TItem : notnull
 {
-    private const string UnreferencedCodeMessage = "JSON serialization and deserialization might require types that cannot be statically analyzed.";
-
     private readonly JsonConverter<TItem> _itemConverter;
 
-    [RequiresUnreferencedCode(UnreferencedCodeMessage)]
-    public OptionJsonConverter(JsonSerializerOptions options)
-    {
-        _itemConverter = (JsonConverter<TItem>)options.GetConverter(typeof(TItem));
-    }
+    public OptionJsonConverter(JsonConverter<TItem> itemConverter)
+        => _itemConverter = itemConverter;
 
-    [RequiresUnreferencedCode(UnreferencedCodeMessage)]
-    [UnconditionalSuppressMessage("Trimming", "IL2046", Justification = "JsonSerializer is annotated with RequiresUnreferencedCode")]
     public override Option<TItem> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         => reader.TokenType == JsonTokenType.Null
             ? Option<TItem>.None
-            : _itemConverter is not null
-                ? Option.Some(_itemConverter.Read(ref reader, typeof(TItem), options)!)
-                : JsonSerializer.Deserialize<TItem>(ref reader, options)!;
+            : _itemConverter.Read(ref reader, typeof(TItem), options)!;
 
-    [RequiresUnreferencedCode(UnreferencedCodeMessage)]
-    [UnconditionalSuppressMessage("Trimming", "IL2046", Justification = "JsonSerializer is annotated with RequiresUnreferencedCode")]
-    [SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1134:Attributes should not share line", Justification = "Lambda attribute is fine on the same line")]
     public override void Write(Utf8JsonWriter writer, Option<TItem> value, JsonSerializerOptions options)
         => value.Switch(
             none: writer.WriteNullValue,
-            some: [RequiresUnreferencedCode(UnreferencedCodeMessage)] (item) =>
-            {
-                if (_itemConverter is not null)
-                {
-                    _itemConverter.Write(writer, item, options);
-                }
-                else
-                {
-                    JsonSerializer.Serialize(writer, item, options);
-                }
-            });
+            some: item => _itemConverter.Write(writer, item, options));
 }
