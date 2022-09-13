f4cf6dc53ef1d1e2364f6aeecda115d90575b316
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-09-13 14:51:32 +0200

    Rename T -> TSource
    Co-authored-by: Thomas Bruderer <apophis@apophis.ch>

--- Funcky.Async/Extensions/AsyncEnumerableExtensions/JoinToString.cs
+++ Funcky.Async/Extensions/AsyncEnumerableExtensions/JoinToString.cs
@@ -7,23 +7,23 @@ public static partial class AsyncEnumerableExtensions
     /// <summary>
     /// Concatenates the elements of the given sequence, using the specified separator between each element or member.
     /// </summary>
-    /// <typeparam name="T">Type of the elements in <paramref name="source"/> sequence.</typeparam>
+    /// <typeparam name="TSource">Type of the elements in <paramref name="source"/> sequence.</typeparam>
     /// <param name="source">A sequence of items to be joined in a string.</param>
     /// <param name="separator">A single character to separate the individual elements.</param>
     /// <returns>Joined string with separators between the elements.</returns>
     [Pure]
-    public static Task<string> JoinToStringAsync<T>(this IAsyncEnumerable<T> source, char separator)
+    public static Task<string> JoinToStringAsync<TSource>(this IAsyncEnumerable<TSource> source, char separator)
         => JoinToStringInternal(separator.ToString(), source);
 
     /// <summary>
     /// Concatenates the elements of the given sequence, using the specified separator between each element or member.
     /// </summary>
-    /// <typeparam name="T">Type of the elements in <paramref name="source"/> sequence.</typeparam>
+    /// <typeparam name="TSource">Type of the elements in <paramref name="source"/> sequence.</typeparam>
     /// <param name="source">A sequence of items to be joined in a string.</param>
     /// <param name="separator">A string to separate the individual elements.</param>
     /// <returns>Joined string with separators between the elements.</returns>
     [Pure]
-    public static Task<string> JoinToStringAsync<T>(this IAsyncEnumerable<T> source, string separator)
+    public static Task<string> JoinToStringAsync<TSource>(this IAsyncEnumerable<TSource> source, string separator)
         => JoinToStringInternal(separator, source);
 
     private static async Task<string> JoinToStringInternal<T>(string separator, IAsyncEnumerable<T> values)
