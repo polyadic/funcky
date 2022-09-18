bf426785422126b96850463d184d153f9009e317
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-09-18 18:48:07 +0200

    Add `ElementAtOrNone(Index)` overload for `IEnumerable<T>`

--- Funcky/Extensions/EnumerableExtensions/ElementAtOrNone.cs
+++ Funcky/Extensions/EnumerableExtensions/ElementAtOrNone.cs
@@ -15,4 +15,20 @@ public static partial class EnumerableExtensions
         => source
             .Select(Option.Some)
             .ElementAtOrDefault(index);
+
+#if ELEMENT_AT_INDEX
+    /// <summary>
+    /// Returns the element at a specified index in a sequence or an <see cref="Option{T}.None" /> value if the index is out of range.
+    /// </summary>
+    /// <typeparam name="TSource">The type of element contained by the sequence.</typeparam>
+    /// <param name="source">The sequence to find an element in.</param>
+    /// <param name="index">The index for the element to retrieve.</param>
+    /// <returns>The item at the specified index, or <see cref="Option{T}.None" /> if the index is not found.</returns>
+    [Pure]
+    public static Option<TSource> ElementAtOrNone<TSource>(this IEnumerable<TSource> source, Index index)
+        where TSource : notnull
+        => source
+            .Select(Option.Some)
+            .ElementAtOrDefault(index);
+#endif
 }
