c19dd52dfa7e0dae13e5142c4a3699133708205f
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-09-18 18:50:22 +0200

    Add `ElementAtOrNone(Index)` overload for `IAsyncEnumerable<T>`

--- Funcky.Async/Extensions/AsyncEnumerableExtensions/ElementAtOrNone.cs
+++ Funcky.Async/Extensions/AsyncEnumerableExtensions/ElementAtOrNone.cs
@@ -14,4 +14,48 @@ public static partial class AsyncEnumerableExtensions
     public static async ValueTask<Option<TSource>> ElementAtOrNoneAsync<TSource>(this IAsyncEnumerable<TSource> source, int index, CancellationToken cancellationToken = default)
         where TSource : notnull
         => await source.Select(Option.Some).ElementAtOrDefaultAsync(index, cancellationToken).ConfigureAwait(false);
+
+#if INDEX_TYPE
+    [Pure]
+    public static ValueTask<Option<TSource>> ElementAtOrNoneAsync<TSource>(this IAsyncEnumerable<TSource> source, Index index, CancellationToken cancellationToken = default)
+        where TSource : notnull
+        => index.IsFromEnd
+            ? ElementAtOrNoneFromEnd(source, index.Value, cancellationToken)
+            : source.ElementAtOrNoneAsync(index.Value, cancellationToken);
+
+    // Adopted from: https://github.com/dotnet/runtime/blob/ebba1d4acb7abea5ba15e1f7f69d1d1311465d16/src/libraries/System.Linq/src/System/Linq/ElementAt.cs#L152-L183
+    private static async ValueTask<Option<TSource>> ElementAtOrNoneFromEnd<TSource>(IAsyncEnumerable<TSource> source, int indexFromEnd, CancellationToken cancellationToken)
+        where TSource : notnull
+    {
+        if (indexFromEnd > 0)
+        {
+#pragma warning disable CA2007
+            await using var enumerator = source.ConfigureAwait(false).WithCancellation(cancellationToken).GetAsyncEnumerator();
+#pragma warning restore CA2007
+
+            if (await enumerator.MoveNextAsync())
+            {
+                var queue = new Queue<TSource>();
+                queue.Enqueue(enumerator.Current);
+
+                while (await enumerator.MoveNextAsync())
+                {
+                    if (queue.Count == indexFromEnd)
+                    {
+                        queue.Dequeue();
+                    }
+
+                    queue.Enqueue(enumerator.Current);
+                }
+
+                if (queue.Count == indexFromEnd)
+                {
+                    return queue.Dequeue();
+                }
+            }
+        }
+
+        return Option<TSource>.None;
+    }
+#endif
 }
