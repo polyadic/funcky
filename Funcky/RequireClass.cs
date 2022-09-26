54d6f63c5b51778f575cb888bcfa8014bf68cb45
Thomas Bruderer <apophis@apophis.ch>
2022-09-26 17:46:00 +0200

    Seems like this suppression is no longer needed.

--- Funcky/RequireClass.cs
+++ Funcky/RequireClass.cs
@@ -1,10 +1,8 @@
 using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
 using static System.ComponentModel.EditorBrowsableState;
 
 namespace Funcky;
 
-[SuppressMessage("ApiDesign", "RS0041", Justification = "This is just a helper type")]
 [EditorBrowsable(Never)]
 public sealed record RequireClass<T>
     where T : class;
