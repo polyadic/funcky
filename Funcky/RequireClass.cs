b4414a086ecf9c41e29678e0a1217d71e4705c8a
Thomas Bruderer <apophis@apophis.ch>
2022-09-26 17:43:01 +0200

    Hide RequireClass and RequireStruct in the IDE

--- Funcky/RequireClass.cs
+++ Funcky/RequireClass.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
+using static System.ComponentModel.EditorBrowsableState;
 
 namespace Funcky;
 
 [SuppressMessage("ApiDesign", "RS0041", Justification = "This is just a helper type")]
+[EditorBrowsable(Never)]
 public sealed record RequireClass<T>
     where T : class;
