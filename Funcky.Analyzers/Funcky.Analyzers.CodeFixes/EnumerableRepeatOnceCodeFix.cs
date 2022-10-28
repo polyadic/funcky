db4f4471d0527029a3e2033dcf3dfd73ef96b93f
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-10-28 10:38:13 +0200

    Allow arbitrary argument order in repeat once analyzer

--- Funcky.Analyzers/Funcky.Analyzers.CodeFixes/EnumerableRepeatOnceCodeFix.cs
+++ Funcky.Analyzers/Funcky.Analyzers.CodeFixes/EnumerableRepeatOnceCodeFix.cs
@@ -8,6 +8,7 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Editing;
 using Microsoft.CodeAnalysis.Simplification;
 using static Funcky.Analyzers.CodeFixResources;
+using static Funcky.Analyzers.EnumerableRepeatOnceAnalyzer;
 using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace Funcky.Analyzers;
@@ -19,7 +20,7 @@ public sealed class EnumerableRepeatOnceCodeFix : CodeFixProvider
     private const string Return = "Return";
 
     public override ImmutableArray<string> FixableDiagnosticIds
-        => ImmutableArray.Create(EnumerableRepeatOnceAnalyzer.DiagnosticId);
+        => ImmutableArray.Create(DiagnosticId);
 
     public override FixAllProvider GetFixAllProvider()
         => WellKnownFixAllProviders.BatchFixer;
@@ -27,38 +28,38 @@ public sealed class EnumerableRepeatOnceCodeFix : CodeFixProvider
     public override async Task RegisterCodeFixesAsync(CodeFixContext context)
     {
         var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
-        var diagnosticSpan = context.Diagnostics.First().Location.SourceSpan;
+        var diagnostic = GetDiagnostic(context);
+        var diagnosticSpan = diagnostic.Location.SourceSpan;
 
-        if (root?.FindToken(diagnosticSpan.Start).Parent?.AncestorsAndSelf().OfType<InvocationExpressionSyntax>().First() is { } declaration)
+        if (root?.FindToken(diagnosticSpan.Start).Parent?.AncestorsAndSelf().OfType<InvocationExpressionSyntax>().First() is { } declaration
+            && diagnostic.Properties.TryGetValue(ValueParameterIndexProperty, out var valueParameterIndexProperty)
+            && int.TryParse(valueParameterIndexProperty, out var valueParameterIndex))
         {
-            context.RegisterCodeFix(CreateFix(context, declaration), GetDiagnostic(context));
+            context.RegisterCodeFix(CreateFix(context, declaration, valueParameterIndex), diagnostic);
         }
     }
 
     private static Diagnostic GetDiagnostic(CodeFixContext context)
         => context.Diagnostics.First();
 
-    private static CodeAction CreateFix(CodeFixContext context, InvocationExpressionSyntax declaration)
+    private static CodeAction CreateFix(CodeFixContext context, InvocationExpressionSyntax declaration, int valueParameterIndex)
         => CodeAction.Create(
             EnumerableRepeatOnceCodeFixTitle,
-            CreateSequenceReturnAsync(context.Document, declaration),
+            CreateSequenceReturnAsync(context.Document, declaration, valueParameterIndex),
             nameof(EnumerableRepeatOnceCodeFixTitle));
 
-    private static Func<CancellationToken, Task<Document>> CreateSequenceReturnAsync(Document document, InvocationExpressionSyntax declaration)
+    private static Func<CancellationToken, Task<Document>> CreateSequenceReturnAsync(Document document, InvocationExpressionSyntax declaration, int valueParameterIndex)
         => async cancellationToken
             =>
             {
                 var editor = await DocumentEditor.CreateAsync(document, cancellationToken).ConfigureAwait(false);
-                editor.ReplaceNode(declaration, CreateSequenceReturnRoot(ExtractFirstArgument(declaration), editor.SemanticModel, editor.Generator));
+                editor.ReplaceNode(declaration, CreateSequenceReturnRoot(declaration.ArgumentList.Arguments[valueParameterIndex], editor.SemanticModel, editor.Generator));
                 return editor.GetChangedDocument();
             };
 
-    private static ArgumentSyntax ExtractFirstArgument(InvocationExpressionSyntax invocationExpression)
-        => invocationExpression.ArgumentList.Arguments[Argument.First];
-
     private static SyntaxNode CreateSequenceReturnRoot(ArgumentSyntax firstArgument, SemanticModel model, SyntaxGenerator generator)
         => SyntaxSequenceReturn(model, generator)
-            .WithArgumentList(ArgumentList(SingletonSeparatedList(firstArgument))
+            .WithArgumentList(ArgumentList(SingletonSeparatedList(firstArgument.WithNameColon(null)))
             .WithCloseParenToken(Token(SyntaxKind.CloseParenToken)))
             .NormalizeWhitespace();
 
