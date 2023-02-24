13f2bf8213768b388ce69ab035999145a9aa859e
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2023-02-24 18:41:15 +0100

    Implement `Retry` with exception-throwing producers

---
+++ Funcky/Functional/RetryWithException.cs
@@ -0,0 +1,28 @@
+using Funcky.RetryPolicies;
+
+namespace Funcky;
+
+public static partial class Functional
+{
+    /// <summary>Retries a producer according as long as an exception matching the
+    /// <paramref name="shouldRetry"/> predicate is thrown.
+    /// When all retries are exhausted, the exception is propagated to the caller.</summary>
+    /// <remarks>Note that this function uses <see cref="Thread.Sleep(TimeSpan)"/> for the delay.
+    /// Consider using <c>RetryAsync</c> from the <c>Funcky.Async</c> package instead.</remarks>
+    public static TResult Retry<TResult>(Func<TResult> producer, Func<Exception, bool> shouldRetry, IRetryPolicy retryPolicy)
+    {
+        var retryCount = 1;
+        while (true)
+        {
+            try
+            {
+                return producer();
+            }
+            catch (Exception exception) when (shouldRetry(exception) && retryCount < retryPolicy.MaxRetries)
+            {
+                Thread.Sleep(retryPolicy.Delay(retryCount));
+                retryCount++;
+            }
+        }
+    }
+}
