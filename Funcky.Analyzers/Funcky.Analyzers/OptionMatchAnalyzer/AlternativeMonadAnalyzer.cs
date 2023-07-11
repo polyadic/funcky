6f7e04543df1bd567aed86aab86c6a6fb5d1c0c0
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2023-07-11 11:14:22 +0200

    Parse attribute

--- Funcky.Analyzers/Funcky.Analyzers/OptionMatchAnalyzer/AlternativeMonadAnalyzer.cs
+++ Funcky.Analyzers/Funcky.Analyzers/OptionMatchAnalyzer/AlternativeMonadAnalyzer.cs
@@ -36,29 +36,38 @@ public sealed partial class AlternativeMonadAnalyzer : DiagnosticAnalyzer
     {
         var operation = (IInvocationOperation)context.Operation;
 
-        if (IsMatchInvocation(operation, symbols, out var receiverType)
+        if (IsMatchInvocation(operation, symbols, out var receiverType, out var alternativeMonad)
             && AnalyzeMatchInvocation(
                 operation,
                 symbols,
                 receiverType,
-                noneArgument: operation.GetArgumentForParameterAtIndex(0),
-                someArgument: operation.GetArgumentForParameterAtIndex(1)) is { } diagnostic)
+                noneArgument: operation.GetArgumentForParameterAtIndex(ErrorStateArgumentIndex(alternativeMonad)),
+                someArgument: operation.GetArgumentForParameterAtIndex(SuccessStateArgumentIndex(alternativeMonad))) is { } diagnostic)
         {
             context.ReportDiagnostic(diagnostic);
         }
     }
 
+    private static int ErrorStateArgumentIndex(AlternativeMonad alternativeMonad)
+        => alternativeMonad.MatchHasSuccessStateFirst ? 1 : 0;
+
+    private static int SuccessStateArgumentIndex(AlternativeMonad alternativeMonad)
+        => alternativeMonad.MatchHasSuccessStateFirst ? 0 : 1;
+
     private static bool IsMatchInvocation(
         IInvocationOperation invocation,
         CompilationSymbols symbols,
-        [NotNullWhen(true)] out INamedTypeSymbol? matchReceiverType)
+        [NotNullWhen(true)] out INamedTypeSymbol? matchReceiverType,
+        [NotNullWhen(true)] out AlternativeMonad? matchAlternativeMonad)
     {
         matchReceiverType = null;
+        matchAlternativeMonad = null;
         return invocation.TargetMethod.ReceiverType is INamedTypeSymbol receiverType
            && invocation.TargetMethod.Name == MatchMethodName
            && invocation.Arguments.Length == 2
-           && receiverType.GetAttributes().Any(attribute => SymbolEqualityComparer.Default.Equals(attribute.AttributeClass, symbols.AlternativeMonadAttributeType))
-           && (matchReceiverType = receiverType) is var _;
+           && ParseAlternativeMonad(receiverType, symbols.AlternativeMonadAttributeType) is { } alternativeMonad
+           && (matchReceiverType = receiverType) is var _
+           && (matchAlternativeMonad = alternativeMonad) is var _;
     }
 
     private static Diagnostic? AnalyzeMatchInvocation(
@@ -107,5 +116,18 @@ public sealed partial class AlternativeMonadAnalyzer : DiagnosticAnalyzer
         => compilation.GetOptionExtensionsType() is { } optionExtensionsType
             && optionExtensionsType.GetMembers().Any(static member => member is IMethodSymbol { Name: OptionToNullableMethodName });
 
+    private static AlternativeMonad? ParseAlternativeMonad(INamedTypeSymbol type, INamedTypeSymbol alternativeMonadAttributeType)
+        => type.GetAttributes()
+            .Where(a => SymbolEqualityComparer.Default.Equals(a.AttributeClass, alternativeMonadAttributeType))
+            .Select(ParseAlternativeMonadAttribute)
+            .SingleOrDefault();
+
+    private static AlternativeMonad ParseAlternativeMonadAttribute(AttributeData attribute)
+        => new(
+            MatchHasSuccessStateFirst: (bool)(attribute.NamedArguments.Single(arg => arg.Key == "MatchHasSuccessStateFirst").Value.Value ?? throw new InvalidOperationException()),
+            ReturnAlias: (string)(attribute.NamedArguments.Single(arg => arg.Key == "ReturnAlias").Value.Value ?? throw new InvalidOperationException()));
+
     private sealed record CompilationSymbols(INamedTypeSymbol AlternativeMonadAttributeType, bool ToNullableExtensionIsAvailable);
+
+    private sealed record AlternativeMonad(bool MatchHasSuccessStateFirst, string ReturnAlias);
 }
