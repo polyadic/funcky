34bad5c937349b194161cedb4e25b0764628fbd3
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2023-07-12 11:18:59 +0200

    Remove no longer needed factory

--- Funcky.Analyzers/Funcky.Analyzers/AlternativeMonadAnalyzer/AlternativeMonadType.cs
+++ Funcky.Analyzers/Funcky.Analyzers/AlternativeMonadAnalyzer/AlternativeMonadType.cs
@@ -24,16 +24,4 @@ internal sealed class AlternativeMonadType
     public int SuccessStateArgumentIndex => _matchHasSuccessStateFirst ? 0 : 1;
 
     public bool HasGetOrElse => _hasGetOrElse.Value;
-
-    public static AlternativeMonadType? Create(INamedTypeSymbol type, INamedTypeSymbol alternativeMonadAttributeType)
-        => type.GetAttributes()
-            .Where(a => SymbolEqualityComparer.Default.Equals(a.AttributeClass, alternativeMonadAttributeType))
-            .Select(Create(type.ConstructedFrom))
-            .SingleOrDefault();
-
-    private static Func<AttributeData, AlternativeMonadType> Create(INamedTypeSymbol type)
-        => attribute => new(
-            type: type,
-            matchHasSuccessStateFirst: (bool)(attribute.NamedArguments.SingleOrDefault(arg => arg.Key == "MatchHasSuccessStateFirst").Value.Value ?? false),
-            returnAlias: (string)(attribute.NamedArguments.Single(arg => arg.Key == "ReturnAlias").Value.Value ?? throw new InvalidOperationException()));
 }
