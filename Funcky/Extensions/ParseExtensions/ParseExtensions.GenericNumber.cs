c4d790f687551bfda36eb15615b06ba23ee39656
Thomas Bruderer <apophis@apophis.ch>
2023-09-04 11:32:02 +0200

    adressing review: * improve naming * implement ParseOrNone for IParseable

--- Funcky/Extensions/ParseExtensions/ParseExtensions.GenericNumber.cs
+++ Funcky/Extensions/ParseExtensions/ParseExtensions.GenericNumber.cs
@@ -5,13 +5,13 @@ using System.Numerics;
 namespace Funcky.Extensions;
 public static partial class ParseExtensions
 {
-    public static Option<TNumber> ParseGenericNumber<TNumber>(this string value, NumberStyles style, IFormatProvider? provider)
+    public static Option<TNumber> ParseNumberOrNone<TNumber>(this string value, NumberStyles style, IFormatProvider? provider)
         where TNumber : INumberBase<TNumber>
         => TNumber.TryParse(value, style, provider, out var result)
             ? result
             : Option<TNumber>.None;
 
-    public static Option<TNumber> ParseGenericNumber<TNumber>(this ReadOnlySpan<char> value, NumberStyles style, IFormatProvider? provider)
+    public static Option<TNumber> ParseNumberOrNone<TNumber>(this ReadOnlySpan<char> value, NumberStyles style, IFormatProvider? provider)
         where TNumber : INumberBase<TNumber>
         => TNumber.TryParse(value, style, provider, out var result)
             ? result
--- Funcky/Extensions/ParseExtensions/ParseExtensions.GenericParseable.cs
+++ Funcky/Extensions/ParseExtensions/ParseExtensions.GenericParseable.cs
@@ -3,8 +3,14 @@ namespace Funcky.Extensions;
 
 public static partial class ParseExtensions
 {
-    public static Option<TParseable> ParseGenericParsable<TParseable>(this ReadOnlySpan<char> value, IFormatProvider? provider)
+    public static Option<TParseable> ParseOrNone<TParseable>(this ReadOnlySpan<char> value, IFormatProvider? provider)
         where TParseable : ISpanParsable<TParseable>
+        => TParseable.TryParse(value, provider, out var result)
+            ? result
+            : Option<TParseable>.None;
+
+    public static Option<TParseable> ParseOrNone<TParseable>(this string? value, IFormatProvider? provider)
+        where TParseable : IParsable<TParseable>
         => TParseable.TryParse(value, provider, out var result)
             ? result
             : Option<TParseable>.None;
