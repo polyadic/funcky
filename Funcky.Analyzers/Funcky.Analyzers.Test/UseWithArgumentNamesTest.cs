3c62e154e7647efca923058a601b70e5388a351b
Tau <4602612+bash@users.noreply.github.com>
2023-09-24 23:11:14 +0200

    Inline and simplify analyzer test files

--- Funcky.Analyzers/Funcky.Analyzers.Test/UseWithArgumentNamesTest.cs
+++ Funcky.Analyzers/Funcky.Analyzers.Test/UseWithArgumentNamesTest.cs
@@ -17,40 +17,120 @@ public sealed class UseWithArgumentNamesTest
     [Fact]
     public async Task ArgumentsThatAlreadyUseArgumentNamesGetNoDiagnostic()
     {
-        var inputCode = await File.ReadAllTextAsync("TestCode/ValidUseWithArgumentNames.input");
-        await VerifyCS.VerifyAnalyzerAsync(inputCode);
+        var inputCode =
+            """
+            using Funcky.CodeAnalysis;
+
+            class Test
+            {
+                private void Syntax()
+                {
+                    Method(x: 10, y: 20);
+                }
+
+                [UseWithArgumentNames]
+                private void Method(int x, int y) { }
+            }
+            """;
+        await VerifyCS.VerifyAnalyzerAsync(inputCode + AttributeSource);
     }
 
     [Fact]
     public async Task ArgumentsForCallsToMethodsWithoutAttributeGetNoDiagnostic()
     {
-        var inputCode = await File.ReadAllTextAsync("TestCode/ValidUseWithArgumentNamesNoAttribute.input");
-        await VerifyCS.VerifyAnalyzerAsync(inputCode);
+        var inputCode =
+            """
+            using Funcky.CodeAnalysis;
+
+            class Test
+            {
+                private void Syntax()
+                {
+                    Method(10, 20);
+                }
+
+                private void Method(int x, int y) { }
+            }
+            """;
+        await VerifyCS.VerifyAnalyzerAsync(inputCode + AttributeSource);
     }
 
     [Fact]
     public async Task UsagesOfMethodsAnnotatedWithShouldUseNamedArgumentsAttributeGetWarningAndAreFixed()
     {
+        const string inputCode =
+            """
+            using Funcky.CodeAnalysis;
+
+            class Test
+            {
+                private void Syntax()
+                {
+                    Method(x: 10, 20);
+                    Method(10, 20);
+                    Method(
+                        10, 20);
+                    Method(
+                        10,
+                        20);
+                    MethodWithKeywordAsArgument(10);
+                }
+
+                [UseWithArgumentNames]
+                private void Method(int x, int y) { }
+
+                [UseWithArgumentNames]
+                private void MethodWithKeywordAsArgument(int @int) { }
+            }
+            """;
+
+        const string fixedCode =
+            """
+            using Funcky.CodeAnalysis;
+
+            class Test
+            {
+                private void Syntax()
+                {
+                    Method(x: 10, y: 20);
+                    Method(x: 10, y: 20);
+                    Method(
+                        x: 10, y: 20);
+                    Method(
+                        x: 10,
+                        y: 20);
+                    MethodWithKeywordAsArgument(@int: 10);
+                }
+
+                [UseWithArgumentNames]
+                private void Method(int x, int y) { }
+
+                [UseWithArgumentNames]
+                private void MethodWithKeywordAsArgument(int @int) { }
+            }
+            """;
+
         var expectedDiagnostics = new[]
         {
-            VerifyCS.Diagnostic().WithSpan(11, 27, 11, 29).WithArguments("y"),
-            VerifyCS.Diagnostic().WithSpan(12, 20, 12, 22).WithArguments("x"),
-            VerifyCS.Diagnostic().WithSpan(12, 24, 12, 26).WithArguments("y"),
-            VerifyCS.Diagnostic().WithSpan(14, 17, 14, 19).WithArguments("x"),
-            VerifyCS.Diagnostic().WithSpan(14, 21, 14, 23).WithArguments("y"),
-            VerifyCS.Diagnostic().WithSpan(16, 17, 16, 19).WithArguments("x"),
-            VerifyCS.Diagnostic().WithSpan(17, 17, 17, 19).WithArguments("y"),
-            VerifyCS.Diagnostic().WithSpan(18, 41, 18, 43).WithArguments("int"),
+            VerifyCS.Diagnostic().WithSpan(7, 23, 7, 25).WithArguments("y"),
+            VerifyCS.Diagnostic().WithSpan(8, 16, 8, 18).WithArguments("x"),
+            VerifyCS.Diagnostic().WithSpan(8, 20, 8, 22).WithArguments("y"),
+            VerifyCS.Diagnostic().WithSpan(10, 13, 10, 15).WithArguments("x"),
+            VerifyCS.Diagnostic().WithSpan(10, 17, 10, 19).WithArguments("y"),
+            VerifyCS.Diagnostic().WithSpan(12, 13, 12, 15).WithArguments("x"),
+            VerifyCS.Diagnostic().WithSpan(13, 13, 13, 15).WithArguments("y"),
+            VerifyCS.Diagnostic().WithSpan(14, 37, 14, 39).WithArguments("int"),
         };
 
-        await VerifyWithSourceExample.VerifyDiagnosticAndCodeFix<UseWithArgumentNamesAnalyzer, AddArgumentNameCodeFix>(expectedDiagnostics, "UseWithArgumentNames");
+        await VerifyCS.VerifyAnalyzerAsync(inputCode + AttributeSource, expectedDiagnostics);
+        await VerifyCS.VerifyCodeFixAsync(inputCode + AttributeSource, expectedDiagnostics, fixedCode + AttributeSource);
     }
 
     [Fact]
     public async Task IgnoresCallsToMethodsInsideExpressionTrees()
     {
         var inputCode =
-            $$"""
+            """
             using System;
             using System.Linq.Expressions;
             using Funcky.CodeAnalysis;
@@ -65,9 +145,7 @@ public sealed class UseWithArgumentNamesTest
                 [UseWithArgumentNames]
                 private void Method(int x, int y) { }
             }
-
-            {{AttributeSource}}
             """;
-        await VerifyCS.VerifyAnalyzerAsync(inputCode);
+        await VerifyCS.VerifyAnalyzerAsync(inputCode + AttributeSource);
     }
 }
