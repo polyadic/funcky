423edb0dfe24ff01ec3feb30afb73c1c97a392db
Tau <4602612+bash@users.noreply.github.com>
2023-09-25 15:50:44 +0200

    Make functions static by providing the context as arguments

--- Funcky.Analyzers/Funcky.Analyzers/SyntaxNodeExtensions.cs
+++ Funcky.Analyzers/Funcky.Analyzers/SyntaxNodeExtensions.cs
@@ -18,39 +18,35 @@ internal static class SyntaxNodeExtensions
     {
         return expressionTypeOpt is not null
             && node is not null
-            && node.AncestorsAndSelf().Any(current => IsExpressionTree(current, expressionTypeOpt, semanticModel, cancellationToken));
+            && node.AncestorsAndSelf().Any(current => IsExpressionTree(new IsExpressionTreeContext(current, expressionTypeOpt, semanticModel, cancellationToken)));
 
-        static bool IsExpressionTree(
-            SyntaxNode current,
-            INamedTypeSymbol expressionType,
-            SemanticModel semanticModel,
-            CancellationToken cancellationToken)
+        static bool IsExpressionTree(IsExpressionTreeContext context)
         {
-            return current switch {
-                _ when current.IsAnyLambda() => LambdaIsExpressionTree(),
-                SelectOrGroupClauseSyntax or OrderingSyntax => QueryExpressionIsExpressionTree(),
-                QueryClauseSyntax queryClause => QueryClauseIsExpressionTree(queryClause),
+            return context.Syntax switch {
+                var node when node.IsAnyLambda() => LambdaIsExpressionTree(context),
+                SelectOrGroupClauseSyntax or OrderingSyntax => QueryExpressionIsExpressionTree(context),
+                QueryClauseSyntax queryClause => QueryClauseIsExpressionTree(context, queryClause),
                 _ => false,
             };
+        }
 
-            bool LambdaIsExpressionTree()
-            {
-                var typeInfo = semanticModel.GetTypeInfo(current, cancellationToken);
-                return SymbolEqualityComparer.Default.Equals(expressionType, typeInfo.ConvertedType?.OriginalDefinition);
-            }
+        static bool LambdaIsExpressionTree(IsExpressionTreeContext context)
+        {
+            var typeInfo = context.SemanticModel.GetTypeInfo(context.Syntax, context.CancellationToken);
+            return SymbolEqualityComparer.Default.Equals(context.ExpressionType, typeInfo.ConvertedType?.OriginalDefinition);
+        }
 
-            bool QueryExpressionIsExpressionTree()
-            {
-                var info = semanticModel.GetSymbolInfo(current, cancellationToken);
-                return TakesExpressionTree(info, expressionType);
-            }
+        static bool QueryExpressionIsExpressionTree(IsExpressionTreeContext context)
+        {
+            var info = context.SemanticModel.GetSymbolInfo(context.Syntax, context.CancellationToken);
+            return TakesExpressionTree(info, context.ExpressionType);
+        }
 
-            bool QueryClauseIsExpressionTree(QueryClauseSyntax queryClause)
-            {
-                var info = semanticModel.GetQueryClauseInfo(queryClause, cancellationToken);
-                return TakesExpressionTree(info.CastInfo, expressionType)
-                    || TakesExpressionTree(info.OperationInfo, expressionType);
-            }
+        static bool QueryClauseIsExpressionTree(IsExpressionTreeContext context, QueryClauseSyntax queryClause)
+        {
+            var info = context.SemanticModel.GetQueryClauseInfo(queryClause, context.CancellationToken);
+            return TakesExpressionTree(info.CastInfo, context.ExpressionType)
+                || TakesExpressionTree(info.OperationInfo, context.ExpressionType);
         }
 
         static bool TakesExpressionTree(SymbolInfo info, INamedTypeSymbol expressionType)
@@ -71,4 +67,10 @@ internal static class SyntaxNodeExtensions
     // https://github.com/dotnet/roslyn/blob/232f7afa4966411958759c880de3a1765bdb28a0/src/Workspaces/SharedUtilitiesAndExtensions/Compiler/CSharp/Extensions/SyntaxNodeExtensions.cs#L925
     internal static bool IsAnyLambda([NotNullWhen(returnValue: true)] this SyntaxNode? node)
         => node?.Kind() is SyntaxKind.ParenthesizedLambdaExpression or SyntaxKind.SimpleLambdaExpression;
+
+    private sealed record IsExpressionTreeContext(
+        SyntaxNode Syntax,
+        INamedTypeSymbol ExpressionType,
+        SemanticModel SemanticModel,
+        CancellationToken CancellationToken);
 }
