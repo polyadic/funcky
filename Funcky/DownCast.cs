61d56dc61326bb69b8f07aa379c77f85c566a25d
Tau Gärtli <git@tau.garden>
2025-01-15 11:07:10 +0100

    Document DownCast methods

--- Funcky/DownCast.cs
+++ Funcky/DownCast.cs
@@ -3,14 +3,20 @@ namespace Funcky;
 public static class DownCast<TResult>
     where TResult : class
 {
+    /// <summary>Downcasts the <c>Some</c> value inside the <c>Option</c> to <typeparamref name="TResult"/>.
+    /// If the cast fails, <c>None</c> is returned.</summary>
     public static Option<TResult> From<TItem>(Option<TItem> option)
         where TItem : class
         => option.SelectMany(OptionDownCast);
 
+    /// <summary>Downcasts the <c>Ok</c> value inside the <c>Result</c> to <typeparamref name="TResult"/>.
+    /// If the cast fails, an <c>Error</c> value containing a <see cref="InvalidCastException"/> is returned.</summary>
     public static Result<TResult> From<TItem>(Result<TItem> result)
         where TItem : class
         => result.SelectMany(ResultDownCast);
 
+    /// <summary>Downcasts the <c>Left</c> value inside the <c>Either</c> to <typeparamref name="TResult"/>.
+    /// If the cast fails, the result from calling <paramref name="failedCast"/> is returned.</summary>
     public static Either<TLeft, TResult> From<TLeft, TRight>(Either<TLeft, TRight> either, Func<TLeft> failedCast)
         where TRight : class
         where TLeft : notnull
