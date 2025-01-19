b1e8ceb1c97c5af0698962b4e095ad7ec68329e6
Tau Gärtli <git@tau.garden>
2025-01-19 21:50:42 +0100

    Add more Try* API wrappers

--- Funcky/Extensions/ParseExtensions/ParseExtensions.IpPrimitives.cs
+++ Funcky/Extensions/ParseExtensions/ParseExtensions.IpPrimitives.cs
@@ -7,4 +7,7 @@ namespace Funcky.Extensions;
 #if IP_END_POINT_TRY_PARSE_SUPPORTED
 [OrNoneFromTryPattern(typeof(IPEndPoint), nameof(IPEndPoint.TryParse))]
 #endif
+#if IP_NETWORK
+[OrNoneFromTryPattern(typeof(IPNetwork), nameof(IPNetwork.TryParse))]
+#endif
 public static partial class ParseExtensions;
