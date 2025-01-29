728a247b89a8e94438a4ccda43c3f6f3e594faa4
Thomas Bruderer <apophis@apophis.ch>
2025-01-29 09:13:59 +0100

    Add RemoveOrNone to Dictionary extensions

--- Funcky.Test/Extensions/DictionaryExtensionTest.cs
+++ Funcky.Test/Extensions/DictionaryExtensionTest.cs
@@ -22,4 +22,28 @@ public sealed class DictionaryExtensionTest
         var dictionary = new Dictionary<string, string> { ["some"] = "value" };
         _ = dictionary.GetValueOrNone("some");
     }
+
+#if REMOVE_EXTENSION
+    [Fact]
+    public void RemoveOrNoneOfAnExistingItemRemovesItemFromDictionaryAndReturnsTheValue()
+    {
+        IDictionary<string, string> dictionary = new Dictionary<string, string> { ["some"] = "value" };
+
+        var value = dictionary.RemoveOrNone("some");
+
+        Assert.Empty(dictionary);
+        FunctionalAssert.Some("value", value);
+    }
+
+    [Fact]
+    public void RemoveOrNoneOfANonExistingItemReturnsNoneAndLeavesTheDictionaryUnchanegd()
+    {
+        IDictionary<string, string> dictionary = new Dictionary<string, string> { ["some"] = "value" };
+
+        var value = dictionary.RemoveOrNone("none");
+
+        Assert.NotEmpty(dictionary);
+        FunctionalAssert.None(value);
+    }
+#endif
 }
