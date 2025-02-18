50d3b074588ccd9a488fa6a5c9a4e052c88bc214
Tau Gärtli <git@tau.garden>
2025-02-18 21:14:37 +0100

    Remove extension `this` from obsolete methods

--- Funcky/Obsolete/ImmutableListExtensions/IndexOfOrNone.cs
+++ Funcky/Obsolete/ImmutableListExtensions/IndexOfOrNone.cs
@@ -10,51 +10,51 @@ public static partial class ImmutableListExtensions
 {
     [EditorBrowsable(EditorBrowsableState.Never)]
     [Obsolete($"Use {nameof(ListExtensions)}.{nameof(IndexOfOrNone)} instead.")]
-    public static Option<int> IndexOfOrNone<TItem>(this IImmutableList<TItem> list, TItem item, int startIndex, int count, IEqualityComparer<TItem>? equalityComparer)
+    public static Option<int> IndexOfOrNone<TItem>(IImmutableList<TItem> list, TItem item, int startIndex, int count, IEqualityComparer<TItem>? equalityComparer)
         => MapNotFoundToNone(list.IndexOf(item, startIndex, count, equalityComparer));
 
     [EditorBrowsable(EditorBrowsableState.Never)]
     [Obsolete($"Use {nameof(ListExtensions)}.{nameof(IndexOfOrNone)} instead.")]
-    public static Option<int> IndexOfOrNone<TItem>(this IImmutableList<TItem> list, TItem item)
+    public static Option<int> IndexOfOrNone<TItem>(IImmutableList<TItem> list, TItem item)
         => MapNotFoundToNone(list.IndexOf(item));
 
     [EditorBrowsable(EditorBrowsableState.Never)]
     [Obsolete($"Use {nameof(ListExtensions)}.{nameof(IndexOfOrNone)} instead.")]
-    public static Option<int> IndexOfOrNone<TItem>(this IImmutableList<TItem> list, TItem item, IEqualityComparer<TItem>? equalityComparer)
+    public static Option<int> IndexOfOrNone<TItem>(IImmutableList<TItem> list, TItem item, IEqualityComparer<TItem>? equalityComparer)
         => MapNotFoundToNone(list.IndexOf(item, equalityComparer));
 
     [EditorBrowsable(EditorBrowsableState.Never)]
     [Obsolete($"Use {nameof(ListExtensions)}.{nameof(IndexOfOrNone)} instead.")]
-    public static Option<int> IndexOfOrNone<TItem>(this IImmutableList<TItem> list, TItem item, int startIndex)
+    public static Option<int> IndexOfOrNone<TItem>(IImmutableList<TItem> list, TItem item, int startIndex)
         => MapNotFoundToNone(list.IndexOf(item, startIndex));
 
     [EditorBrowsable(EditorBrowsableState.Never)]
     [Obsolete($"Use {nameof(ListExtensions)}.{nameof(IndexOfOrNone)} instead.")]
-    public static Option<int> IndexOfOrNone<TItem>(this IImmutableList<TItem> list, TItem item, int startIndex, int count)
+    public static Option<int> IndexOfOrNone<TItem>(IImmutableList<TItem> list, TItem item, int startIndex, int count)
         => MapNotFoundToNone(list.IndexOf(item, startIndex, count));
 
     [EditorBrowsable(EditorBrowsableState.Never)]
     [Obsolete($"Use {nameof(ListExtensions)}.{nameof(LastIndexOfOrNone)} instead.")]
-    public static Option<int> LastIndexOfOrNone<TItem>(this IImmutableList<TItem> list, TItem item, int startIndex, int count, IEqualityComparer<TItem>? equalityComparer)
+    public static Option<int> LastIndexOfOrNone<TItem>(IImmutableList<TItem> list, TItem item, int startIndex, int count, IEqualityComparer<TItem>? equalityComparer)
         => MapNotFoundToNone(list.LastIndexOf(item, startIndex, count, equalityComparer));
 
     [EditorBrowsable(EditorBrowsableState.Never)]
     [Obsolete($"Use {nameof(ListExtensions)}.{nameof(LastIndexOfOrNone)} instead.")]
-    public static Option<int> LastIndexOfOrNone<TItem>(this IImmutableList<TItem> list, TItem item)
+    public static Option<int> LastIndexOfOrNone<TItem>(IImmutableList<TItem> list, TItem item)
         => MapNotFoundToNone(list.LastIndexOf(item));
 
     [EditorBrowsable(EditorBrowsableState.Never)]
     [Obsolete($"Use {nameof(ListExtensions)}.{nameof(LastIndexOfOrNone)} instead.")]
-    public static Option<int> LastIndexOfOrNone<TItem>(this IImmutableList<TItem> list, TItem item, IEqualityComparer<TItem>? equalityComparer)
+    public static Option<int> LastIndexOfOrNone<TItem>(IImmutableList<TItem> list, TItem item, IEqualityComparer<TItem>? equalityComparer)
         => MapNotFoundToNone(list.LastIndexOf(item, equalityComparer));
 
     [EditorBrowsable(EditorBrowsableState.Never)]
     [Obsolete($"Use {nameof(ListExtensions)}.{nameof(LastIndexOfOrNone)} instead.")]
-    public static Option<int> LastIndexOfOrNone<TItem>(this IImmutableList<TItem> list, TItem item, int startIndex)
+    public static Option<int> LastIndexOfOrNone<TItem>(IImmutableList<TItem> list, TItem item, int startIndex)
         => MapNotFoundToNone(list.LastIndexOf(item, startIndex));
 
     [EditorBrowsable(EditorBrowsableState.Never)]
     [Obsolete($"Use {nameof(ListExtensions)}.{nameof(LastIndexOfOrNone)} instead.")]
-    public static Option<int> LastIndexOfOrNone<TItem>(this IImmutableList<TItem> list, TItem item, int startIndex, int count)
+    public static Option<int> LastIndexOfOrNone<TItem>(IImmutableList<TItem> list, TItem item, int startIndex, int count)
         => MapNotFoundToNone(list.LastIndexOf(item, startIndex, count));
 }
