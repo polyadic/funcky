159093b9975f12f360066f8cda70e5f9114719df
Tau Gärtli <git@tau.garden>
2025-03-09 13:25:18 +0100

    Add tests for new overload

--- Funcky.Async.Test/Extensions/AsyncEnumerableExtensions/WhereSelectTest.cs
+++ Funcky.Async.Test/Extensions/AsyncEnumerableExtensions/WhereSelectTest.cs
@@ -1,3 +1,5 @@
+using static Funcky.Discard;
+
 namespace Funcky.Async.Test.Extensions.AsyncEnumerableExtensions;
 
 public sealed class WhereSelectTest
@@ -8,6 +10,16 @@ public sealed class WhereSelectTest
         var expectedSquares = new[] { 0, 4, 16, 36, 64 };
         var squares = await Ι(10).WhereSelect(SquareEvenNumbers).ToListAsync();
         Assert.Equal(expectedSquares, squares);
+    }
+
+    [Fact]
+    public async Task WhereSelectReceivesTheSourceElementsIndex()
+    {
+        const int count = 6;
+        var expectedSequence = Enumerable.Range(0, count: count);
+        var units = AsyncSequence.Cycle(__).Take(count);
+        var indexes = units.WhereSelect((_, index) => Option.Some(index));
+        Assert.Equal(expectedSequence, await indexes.ToListAsync());
     }
 
     private static Option<int> SquareEvenNumbers(int n)
