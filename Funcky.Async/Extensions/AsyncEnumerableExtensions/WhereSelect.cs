f175221a6bc2daf56a63224b168c0daba53786aa
Tau Gärtli <git@tau.garden>
2025-03-09 13:19:20 +0100

    Add WhereSelect with index overloads

--- Funcky.Async/Extensions/AsyncEnumerableExtensions/WhereSelect.cs
+++ Funcky.Async/Extensions/AsyncEnumerableExtensions/WhereSelect.cs
@@ -3,7 +3,7 @@ namespace Funcky.Extensions;
 public static partial class AsyncEnumerableExtensions
 {
     /// <summary>
-    /// Filters out all the empty values from an <c>IEnumerable&lt;Option&lt;T&gt;&gt;</c> and therefore returns an <see cref="IEnumerable{T}"/>.
+    /// Filters out all the empty values from an <see cref="IAsyncEnumerable{T}"><![CDATA[IAsyncEnumerable<Option<T>>]]></see> and therefore returns an <see cref="IAsyncEnumerable{T}"/>.
     /// </summary>
     [Pure]
     public static IAsyncEnumerable<TSource> WhereSelect<TSource>(this IAsyncEnumerable<Option<TSource>> source)
@@ -19,15 +19,37 @@ public static partial class AsyncEnumerableExtensions
         where TResult : notnull
         => source.Select(selector).SelectMany(ToAsyncEnumerable);
 
-    /// <inheritdoc cref="WhereSelect{TSource,TOutput}"/>
+    /// <summary>
+    /// Projects and filters an <see cref="IAsyncEnumerable{T}"/> at the same time.
+    /// This is done by filtering out any empty <see cref="Option{T}"/> values returned by the <paramref name="selector"/>.
+    /// The index of each source element is used in the projected form of that element.
+    /// </summary>
+    [Pure]
+    public static IAsyncEnumerable<TResult> WhereSelect<TSource, TResult>(this IAsyncEnumerable<TSource> source, Func<TSource, int, Option<TResult>> selector)
+        where TResult : notnull
+        => source.Select(selector).SelectMany(ToAsyncEnumerable);
+
+    /// <inheritdoc cref="WhereSelect{TSource,TResult}(IAsyncEnumerable{TSource},Func{TSource,Option{TResult}})"/>
     [Pure]
     public static IAsyncEnumerable<TResult> WhereSelectAwait<TSource, TResult>(this IAsyncEnumerable<TSource> source, Func<TSource, ValueTask<Option<TResult>>> selector)
         where TResult : notnull
         => source.SelectAwait(selector).SelectMany(ToAsyncEnumerable);
 
-    /// <inheritdoc cref="WhereSelect{TSource,TOutput}"/>
+    /// <inheritdoc cref="WhereSelect{TSource,TResult}(IAsyncEnumerable{TSource},Func{TSource,int,Option{TResult}})"/>
+    [Pure]
+    public static IAsyncEnumerable<TResult> WhereSelectAwait<TSource, TResult>(this IAsyncEnumerable<TSource> source, Func<TSource, int, ValueTask<Option<TResult>>> selector)
+        where TResult : notnull
+        => source.SelectAwait(selector).SelectMany(ToAsyncEnumerable);
+
+    /// <inheritdoc cref="WhereSelect{TSource,TResult}(IAsyncEnumerable{TSource},Func{TSource,Option{TResult}})"/>
     [Pure]
     public static IAsyncEnumerable<TResult> WhereSelectAwaitWithCancellation<TSource, TResult>(this IAsyncEnumerable<TSource> source, Func<TSource, CancellationToken, ValueTask<Option<TResult>>> selector)
+        where TResult : notnull
+        => source.SelectAwaitWithCancellation(selector).SelectMany(ToAsyncEnumerable);
+
+    /// <inheritdoc cref="WhereSelect{TSource,TResult}(IAsyncEnumerable{TSource},Func{TSource,int,Option{TResult}})"/>
+    [Pure]
+    public static IAsyncEnumerable<TResult> WhereSelectAwaitWithCancellation<TSource, TResult>(this IAsyncEnumerable<TSource> source, Func<TSource, int, CancellationToken, ValueTask<Option<TResult>>> selector)
         where TResult : notnull
         => source.SelectAwaitWithCancellation(selector).SelectMany(ToAsyncEnumerable);
 
